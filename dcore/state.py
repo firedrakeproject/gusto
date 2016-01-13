ee0771dba0c677cd7c3f4f0f91a4ce8aef1b1294
Colin Cotter <colin.cotter@imperial.ac.uk>
2016-01-13 17:36:20 +0000

    Started an example file for dcimp_3-1 with BDFM1.
    Did a bit of lint-ing

---
+++ dcore/state.py
@@ -0,0 +1,79 @@
+class State(object):
+    """
+    Build a model state to keep the variables in, and specify parameters.
+
+    :arg mesh: The :class:`ExtrudedMesh` to use.
+    :arg vertical_degree: integer, the degree for spaces in the vertical 
+    (specifies the degree for the pressure space, other spaces are inferred)
+    defaults to 1.
+    :arg horizontal_degree: integer, the degree for spaces in the horizontal 
+    (specifies the degree for the pressure space, other spaces are inferred)
+    defaults to 1.
+    :arg family: string, specifies the velocity space family to use. 
+    Options:
+    "RT": The Raviart-Thomas family (default, recommended for quads)
+    "BDM": The BDM family
+    "BDFM": The BDFM family
+    :arg dt: the timestep
+    :arg g: the acceleration due to gravity
+    """
+
+    def __init__(mesh, vertical_degree = 1, horizontal_degree = 1,
+                 family = "RT",
+                 dt = 1.0,
+                 g = 9.81):
+        
+        #The mesh
+        self.mesh = mesh
+
+        #parameters
+        self.dt = dt
+        self.g = g
+        
+        #Build the spaces
+        self._build_spaces(mesh, vertical_degree,
+                          horizontal_degree, family)
+        
+        #Allocate state
+        self._allocate_state()
+
+    def initialise_state_from_expressions(u_expr, rho_expr, theta_expr):
+        """
+        Initialise state variables from expressions.
+        :arg u_expr: This expression will be projected to initial u.
+        :arg rho_expr: This expression will be interpolated to initial rho.
+        :arg theta_expr: This expression will be interpolated to initial theta.
+        """
+
+        raise(NotImplementedError)
+
+    def _build_spaces(self, mesh, vertical_degree, horizontal_degree, family):
+        """
+        Build:
+        velocity space self.V2,
+        pressure space self.V3,
+        temperature space self.Vt,
+        mixed function space self.W = (V2,V3,Vt)
+        """
+
+        #build spaces V2, V3, Vt
+        raise(NotImplementedError)
+
+        self.V2 = V2
+        self.V3 = V3
+        self.Vt = Vt
+        self.W = MixedFunctionSpace((V2, V3, Vt))
+
+    def _allocate_state(self):
+        """
+        Construct Functions to store the state variables.
+        """
+
+        self.xn = Function(W)
+        self.x_init = Function(W)
+        self.xstar = Function(W)
+        self.xp = Function(W)
+        self.xnp1 = Function(W)
+        self.xrhs = Function(W)
+        self.dy = Function(W)
+
--- dcore/timeloop.py
+++ dcore/timeloop.py
@@ -13,11 +13,8 @@ class Timestepper(object):
         self.advection_list = advection_list
 
 
-    def run(self):
+    def run(self, t, dt, tmax):
         state = self.state
-        t = state.t0
-        dt = state.dt
-        tmax = state.tmax
         
         state.xn.assign(state.x_init)
 
@@ -26,16 +23,16 @@ class Timestepper(object):
 
         while(t<tmax - 0.5*dt):
             t += dt 
-            self.apply_forcing(state.xn,state.xstar)
+            self.apply_forcing(state.xn, state.xstar)
             state.xnp1.assign(state.xn)
             
             for(k in range(state.maxk)):
                 self.set_ubar()  #computes state.ubar from state.xn and state.xnp1
                 for advection, index in self.advection_list:
-                    advection.apply(xstar_fields[index],xp_fields[index]) #advects a field from xstar and puts result in xp
+                    advection.apply(xstar_fields[index], xp_fields[index]) #advects a field from xstar and puts result in xp
                 for(i in range(state.maxi)):
                     state.xrhs.assign(0.) #xrhs is the residual which goes in the linear solve
-                    self.apply_forcing(state.xp,state.xrhs)
+                    self.apply_forcing(state.xp, state.xrhs)
                     state.xrhs -= state.xnp1
                     self.linear_system.solve() # solves linear system and places result in state.dy
                     state.xnp1 += state.dy
