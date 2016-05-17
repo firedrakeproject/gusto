8acaf390fd2981a11f22ed82d20aafed09ba9674
Jemma Shipton <jemmashipton@JSMac.local>
2016-05-17 18:19:06 +0100

    base diffusion class and do nothing method

--- dcore/__init__.py
+++ dcore/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from dcore.configuration import *
 from dcore.advection import *   # noqa
+from dcore.diffusion import *   # noqa
 from dcore.diagnostics import *     # noqa
 from dcore.forcing import *     # noqa
 from dcore.linear_solvers import *  # noqa
---
+++ dcore/diffusion.py
@@ -0,0 +1,32 @@
+
+class Diffusion(object):    """
+    Base class for diffusion schemes for dcore.
+
+    :arg state: :class:`.State` object.
+    """
+    __metaclass__ = ABCMeta
+
+    def __init__(self, state):
+        self.state = state
+
+    @abstractmethod
+    def apply(self, x, x_out):
+        """
+        Function takes x as input, computes F(x) and returns x_out
+        as output.
+
+        :arg x: :class:`.Function` object, the input Function.
+        :arg x_out: :class:`.Function` object, the output Function.
+        """
+        pass
+
+
+class NoDiffusion(Diffusion):
+    """
+    An non-diffusion scheme that does nothing.
+    """
+
+    def apply(self, x_in, x_out):
+
+        x_out.assign(x_in)
+
