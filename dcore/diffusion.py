0c6e4d8e3da04e55f137e878ca51bbaf134eaccf
Jemma Shipton <jemmashipton@Unknown-54-26-96-d1-67-03.home>
2016-05-19 11:21:14 +0100

    documentation for IP diffusion method

--- dcore/diffusion.py
+++ dcore/diffusion.py
@@ -29,6 +29,20 @@ class Diffusion(object):
 
 
 class InteriorPenulty(Diffusion):
+    """
+    Interior penulty diffusion method
+    
+    :arg state: :class:`.State` object.
+    :arg V: Function space of diffused field
+    :arg direction: list containing directions in which function space 
+    is discontinuous: 1 corresponds to vertical, 2 to horizontal.
+    :arg params: dictionary containing the interior penulty parameters
+    mu and kappa where mu is the penulty weighting function, proportional 
+    to 1/dx (if not supplied this is set to a default value based on the 
+    max area of a cell), and kappa is the diffusion parameter which must 
+    be specified.
+
+    """
 
     def __init__(self, state, V, direction=[1,2], params=None):
         super(InteriorPenulty, self).__init__(state)
