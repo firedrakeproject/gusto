6735370d946dc47e5891c1b0728096a1ebed2c00
Jemma Shipton <jshipton@ma-jshipton.ma.ic.ac.uk>
2016-06-14 12:01:29 +0100

    no need to test dumping in latlon coords in both sw tests

--- tests/test_sw_linear_triangle.py
+++ tests/test_sw_linear_triangle.py
@@ -20,7 +20,7 @@ def setup_sw(dirname):
 
     fieldlist = ['u', 'D']
     timestepping = TimesteppingParameters(dt=3600.)
-    output = OutputParameters(dirname=dirname+"/sw_linear_w2", steady_state_dump_err={'u':True,'D':True}, dumpfreq=12, dumplist_latlon=['D'])
+    output = OutputParameters(dirname=dirname+"/sw_linear_w2", steady_state_dump_err={'u':True,'D':True}, dumpfreq=12)
     parameters = ShallowWaterParameters(H=H)
     diagnostics = Diagnostics(*fieldlist)
 
