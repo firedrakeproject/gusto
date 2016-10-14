ee2e849e066a786e6e70f97236b0eeb74afee547
Jemma Shipton <j.shipton@imperial.ac.uk>
2016-10-14 17:55:57 +0100

    The shallow water tests were failing because the _apply_bc method assumed an extruded mesh. I have added a check for this - not sure if there is a better way to avoid calling the method at all for shallow water, as it doesn't do anything...

--- gusto/timeloop.py
+++ gusto/timeloop.py
@@ -50,14 +50,15 @@ class Timestepper(object):
         """
         unp1 = self.state.xnp1.split()[0]
 
-        dim = unp1.ufl_element().value_shape()[0]
-        bc = ("0.0",)*dim
-        M = unp1.function_space()
-        bcs = [DirichletBC(M, Expression(bc), "bottom"),
-               DirichletBC(M, Expression(bc), "top")]
+        if unp1.function_space().extruded:
+            dim = unp1.ufl_element().value_shape()[0]
+            bc = ("0.0",)*dim
+            M = unp1.function_space()
+            bcs = [DirichletBC(M, Expression(bc), "bottom"),
+                   DirichletBC(M, Expression(bc), "top")]
 
-        for bc in bcs:
-            bc.apply(unp1)
+            for bc in bcs:
+                bc.apply(unp1)
 
     def run(self, t, tmax):
         state = self.state
