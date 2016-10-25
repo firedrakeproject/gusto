43f0ccb3650f5c49097bdf497e65b3ff5231a6e8
Jemma Shipton <jemmashipton@dyn1186-88.wlan.ic.ac.uk>
2016-10-25 15:42:02 +0100

    oops, should have been part of earlier restucturing commit

--- gusto/__init__.py
+++ gusto/__init__.py
@@ -3,6 +3,7 @@ from gusto.configuration import *   # noqa
 from gusto.advection import *       # noqa
 from gusto.diagnostics import *     # noqa
 from gusto.diffusion import *     # noqa
+from gusto.equation import *     # noqa
 from gusto.forcing import *         # noqa
 from gusto.linear_solvers import *  # noqa
 from gusto.state import *           # noqa
