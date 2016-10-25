849c4aeafcfe9597e3d6a834755582a53e51103a
Jemma Shipton <jemmashipton@dyn1186-88.wlan.ic.ac.uk>
2016-10-25 15:40:24 +0100

    no need to make field_dict twice. Also, add check for pytest in directory name so that pytest stops complaining that directory already exisits

--- gusto/state.py
+++ gusto/state.py
@@ -54,7 +54,10 @@ class State(object):
         self.Omega = Omega
         self.mu = mu
         self.timestepping = timestepping
-        self.output = output
+        if output is None:
+            raise RuntimeError("You must provide a directory name for dumping results")
+        else:
+            self.output = output
         self.parameters = parameters
         if fieldlist is None:
             raise RuntimeError("You must provide a fieldlist containing the names of the prognostic fields")
@@ -96,9 +99,8 @@ class State(object):
             mesh_ll = get_latlon_mesh(self.mesh)
 
         funcs = self.xn.split()
-        field_dict = {name: func for (name, func) in zip(self.fieldlist, funcs)}
         to_dump = []
-        for name, f in field_dict.iteritems():
+        for name, f in self.field_dict.iteritems():
             if name in self.output.dumplist:
                 to_dump.append(f)
             f.rename(name=name)
@@ -110,7 +112,7 @@ class State(object):
         for name, f, f_init in zip(self.fieldlist, funcs, self.x_init.split()):
             if steady_state_dump_err[name]:
                 err = Function(f.function_space(), name=name+'err').assign(f-f_init)
-                field_dict[name+"err"] = err
+                self.field_dict[name+"err"] = err
                 self.diagnostics.register(name+"err")
                 to_dump.append(err)
 
@@ -118,18 +120,18 @@ class State(object):
         meanfields.update(self.output.meanfields)
         for name, meanfield in meanfields.iteritems():
             if meanfield is not None:
-                field = field_dict[name]
+                field = self.field_dict[name]
                 diff = Function(
                     field.function_space(),
                     name=field.name()+"_perturbation").assign(field - meanfield)
                 self.diagnostics.register(name+"perturbation")
-                field_dict[name+"perturbation"] = diff
+                self.field_dict[name+"perturbation"] = diff
                 to_dump.append(diff)
 
         # make functions on latlon mesh, as specified by dumplist_latlon
         to_dump_latlon = []
         for name in self.output.dumplist_latlon:
-            f = field_dict[name]
+            f = self.field_dict[name]
             f_ll = Function(functionspaceimpl.WithGeometry(f.function_space(), mesh_ll), val=f.topological, name=name+'_ll')
             field_dict_ll[name] = f_ll
             to_dump_latlon.append(f_ll)
@@ -137,7 +139,7 @@ class State(object):
         self.dumpdir = path.join("results", self.output.dirname)
         outfile = path.join(self.dumpdir, "field_output.pvd")
         if self.dumpfile is None:
-            if self.mesh.comm.rank == 0 and path.exists(self.dumpdir):
+            if self.mesh.comm.rank == 0 and not "pytest" in self.output.dirname and path.exists(self.dumpdir):
                 exit("results directory '%s' already exists" % self.dumpdir)
             self.dumpcount = itertools.count()
             self.dumpfile = File(outfile, project_output=self.output.project_fields, comm=self.mesh.comm)
@@ -157,7 +159,7 @@ class State(object):
                 self.dumpfile_latlon.write(*to_dump_latlon)
 
             for name in self.diagnostics.fields:
-                data = self.diagnostics.l2(field_dict[name])
+                data = self.diagnostics.l2(self.field_dict[name])
                 self.diagnostic_data[name]["l2"].append(data)
 
     def diagnostic_dump(self):
