ff0040633d2f2a01a3dbb706d15fc8cfaa035e04
Jemma Shipton <jemmashipton@dyn1186-88.wlan.ic.ac.uk>
2016-10-25 15:40:58 +0100

    advection timestepper

--- gusto/timeloop.py
+++ gusto/timeloop.py
@@ -1,9 +1,44 @@
 from __future__ import absolute_import
+from abc import ABCMeta, abstractmethod
 from pyop2.profiling import timed_stage
 from gusto.state import IncompressibleState
+from firedrake import Function
 
 
-class Timestepper(object):
+class BaseTimestepper(object):
+    """
+    Base timestepping class for Gusto
+
+    :arg state: a :class:`.State` object
+    :arg advection_dict a dictionary with entries fieldname: scheme, where
+        fieldname is the name of the field to be advection and scheme is an
+        :class:`.AdvectionScheme` object
+    """
+    __metaclass__ = ABCMeta
+
+    def __init__(self, state, advection_dict):
+
+        self.state = state
+        self.advection_dict = advection_dict
+
+    def _set_ubar(self):
+        """
+        Update ubar in the advection methods.
+        """
+
+        state = self.state
+        un = state.xn.split()[0]
+        unp1 = state.xnp1.split()[0]
+
+        for field, advection in self.advection_dict.iteritems():
+            advection.ubar.assign(un + state.timestepping.alpha*(unp1-un))
+
+    @abstractmethod
+    def run(self):
+        pass
+
+
+class Timestepper(BaseTimestepper):
     """
     Build a timestepper to implement an "auxiliary semi-Lagrangian" timestepping
     scheme for the dynamical core.
@@ -18,8 +53,7 @@ class Timestepper(object):
 
     def __init__(self, state, advection_dict, linear_solver, forcing, diffusion_dict=None):
 
-        self.state = state
-        self.advection_dict = advection_dict
+        super(Timestepper, self).__init__(state, advection_dict)
         self.linear_solver = linear_solver
         self.forcing = forcing
         self.diffusion_dict = {}
@@ -30,18 +64,6 @@ class Timestepper(object):
             self.incompressible = True
         else:
             self.incompressible = False
-
-    def _set_ubar(self):
-        """
-        Update ubar in the advection methods.
-        """
-
-        state = self.state
-        un = state.xn.split()[0]
-        unp1 = state.xnp1.split()[0]
-
-        for field, advection in self.advection_dict.iteritems():
-            advection.ubar.assign(un + state.timestepping.alpha*(unp1-un))
 
     def run(self, t, tmax):
         state = self.state
@@ -104,3 +126,49 @@ class Timestepper(object):
                 state.dump()
 
         state.diagnostic_dump()
+
+
+class AdvectionTimestepper(BaseTimestepper):
+
+    def run(self, t, tmax, x_end=None):
+        state = self.state
+
+        state.xn.assign(state.x_init)
+
+        xn_fields = state.field_dict
+        xnp1_fields = {name: func for (name, func) in
+                       zip(state.fieldlist, state.xnp1.split())}
+        for name, func in state.field_dict.iteritems():
+            if name not in state.fieldlist and name in self.advection_dict.keys():
+                xnp1_fields[name] = Function(func.function_space())
+
+        dt = state.timestepping.dt
+        state.xnp1.assign(state.xn)
+        for name, func in state.field_dict.iteritems():
+            if name not in state.fieldlist:
+                xnp1_fields[name].assign(xn_fields[name])
+
+        state.dump()
+
+        while t < tmax + 0.5*dt:
+            if state.output.Verbose:
+                print "STEP", t, dt
+
+            t += dt
+
+            self._set_ubar()  # computes state.ubar from state.xn and state.xnp1
+            for field, advection in self.advection_dict.iteritems():
+                # advects a field from xn and puts result in xnp1
+                advection.apply(xn_fields[field], xnp1_fields[field])
+
+            state.xn.assign(state.xnp1)
+            for name, func in state.field_dict.iteritems():
+                if name not in state.fieldlist and name in self.advection_dict.keys():
+                    xn_fields[name].assign(xnp1_fields[name])
+
+            state.dump()
+
+        state.diagnostic_dump()
+
+        if x_end is not None:
+            return {field: state.field_dict[field] for field in x_end}
