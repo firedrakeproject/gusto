1f3e14694f3225e7de58086813db8da429586011
Jemma Shipton <jshipton@ma-jshipton.ma.ic.ac.uk>
2016-11-06 20:35:12 +0000

    smoother grid transformation for mountain

--- examples/nh_mountain.py
+++ examples/nh_mountain.py
@@ -16,7 +16,9 @@ x = Function(Vc).interpolate(as_vector([coord[0],coord[1]]))
 H = Constant(H)
 a = Constant(1000.)
 xc = Constant(L/2.)
-new_coords = Function(Vc).interpolate(as_vector([x[0], x[1]+(H-x[1])*a**2/(H*((x[0]-xc)**2+a**2))]))
+#new_coords = Function(Vc).interpolate(as_vector([x[0], x[1]+(H-x[1])*a**2/(H*((x[0]-xc)**2+a**2))]))
+xexpr = Expression(("x[0]","x[1] < zh ? x[1]+pow(cos(0.5*pi*x[1]/zh),6)*h*pow(a,2)/(pow(x[0]-xc,2)+pow(a,2)) : x[1]"), zh=5000., h=1., a=a, xc=xc)
+new_coords = Function(Vc).interpolate(xexpr)
 mesh = Mesh(new_coords)
 
 # Space for initialising velocity
@@ -34,7 +36,7 @@ mu_top = Expression("x[1] <= zc ? 0.0 : mubar*pow(sin((pi/2.)*(x[1]-zc)/(H-zc)),
 mu = Function(W_DG).interpolate(mu_top)
 fieldlist = ['u', 'rho', 'theta']
 timestepping = TimesteppingParameters(dt=dt)
-output = OutputParameters(dirname='nh_mountain', dumpfreq=18, dumplist=['u'])
+output = OutputParameters(dirname='nh_mountain_smootherz', dumpfreq=18, dumplist=['u'])
 parameters = CompressibleParameters(g=9.80665, cp=1004.)
 diagnostics = Diagnostics(*fieldlist)
 diagnostic_fields = [CourantNumber(), VerticalVelocity()]
@@ -118,11 +120,17 @@ state.set_reference_profiles(rho_b, theta_b)
 state.output.meanfields = {'rho':state.rhobar, 'theta':state.thetabar}
 
 # Set up advection schemes
-Vtdg = FunctionSpace(mesh, "DG", 1)
+ueqn = MomentumEquation(state, state.V[0], vector_invariant="EulerPoincare")
+rhoeqn = AdvectionEquation(state, state.V[1], continuity=True)
+supg = True
+if supg:
+    thetaeqn = AdvectionEquation(state, state.V[2], supg={"dg_directions":[0]}, continuity=False)
+else:
+    thetaeqn = AdvectionEquation(state, state.V[2], embedded_dg_space="Default", continuity=False)
 advection_dict = {}
-advection_dict["u"] = EulerPoincareForm(state, state.V[0])
-advection_dict["rho"] = DGAdvection(state, state.V[1], continuity=True)
-advection_dict["theta"] = SUPGAdvection(state, state.V[2], direction=[1])
+advection_dict["u"] = ImplicitMidpoint(state, u0, ueqn)
+advection_dict["rho"] = SSPRK3(state, rho0, rhoeqn)
+advection_dict["theta"] = SSPRK3(state, theta0, thetaeqn)
 
 # Set up linear solver
 schur_params = {'pc_type': 'fieldsplit',
