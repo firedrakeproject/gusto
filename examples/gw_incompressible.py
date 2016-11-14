79e5509a1b040fbf1693726b38a7ebd0d5c7d857
Jemma Shipton <j.shipton@imperial.ac.uk>
2016-11-14 17:19:42 +0000

    fix failing test

--- examples/gw_incompressible.py
+++ examples/gw_incompressible.py
@@ -135,16 +135,15 @@ state.output.meanfields = {'b':state.bbar}
 ##############################################################################
 # advection_dict is a dictionary containing field_name: advection class
 ueqn = MomentumEquation(state, state.V[0], vector_invariant="EulerPoincare")
-beqn = AdvectionEquation(state, state.V[1], continuity=False)
 supg = True
 if supg:
-    thetaeqn = AdvectionEquation(state, state.V[2],
-                                 supg={"dg_directions":[0]},
-                                 continuity=False)
+    beqn = AdvectionEquation(state, state.V[2],
+                             supg={"dg_directions":[0]},
+                             continuity=False)
 else:
-    thetaeqn = AdvectionEquation(state, state.V[2],
-                                 embedded_dg_space="Default",
-                                 continuity=False)
+    beqn = AdvectionEquation(state, state.V[2],
+                             embedded_dg_space="Default",
+                             continuity=False)
 advection_dict = {}
 advection_dict["u"] = ImplicitMidpoint(state, u0, ueqn)
 advection_dict["b"] = SSPRK3(state, b0, beqn)
