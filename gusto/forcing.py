ff4477f5f2cb9b1860bd53090c2e8f1a49751970
Jemma Shipton <j.shipton@imperial.ac.uk>
2017-01-12 15:49:37 +0000

    defining perp in state

--- gusto/forcing.py
+++ gusto/forcing.py
@@ -3,7 +3,7 @@ from abc import ABCMeta, abstractmethod
 from firedrake import Function, split, TrialFunction, TestFunction, \
     FacetNormal, inner, dx, cross, div, jump, avg, dS_v, \
     DirichletBC, LinearVariationalProblem, LinearVariationalSolver, \
-    CellNormal, dot, dS, Constant, warning, Expression, as_vector
+    dot, dS, Constant, warning, Expression, as_vector
 
 
 class Forcing(object):
@@ -371,14 +371,9 @@ class ShallowWaterForcing(Forcing):
         w = TestFunction(Vu)
         self.uF = Function(Vu)
 
-        if state.on_sphere:
-            outward_normals = CellNormal(state.mesh)
-            perp = lambda u: cross(outward_normals, u)
-        else:
-            perp = lambda u: as_vector([-u[1], u[0]])
         a = inner(w, F)*dx
         L = (
-            (-f*inner(w, perp(u0)) + g*div(w)*D0)*dx
+            (-f*inner(w, state.perp(u0)) + g*div(w)*D0)*dx
             - g*inner(jump(w, n), un('+')*D0('+') - un('-')*D0('-'))*dS)
 
         if self.euler_poincare:
