506806c6ae40f9cc7a0c85d16046877bc026facc
Jemma Shipton <j.shipton@imperial.ac.uk>
2017-02-07 14:05:40 +0000

    revised test for hydrostatic initialisation

--- tests/test_gw_incompressible.py
+++ tests/test_gw_incompressible.py
@@ -4,16 +4,6 @@ from firedrake import SpatialCoordinate, \
     sin, pi
 
 
-def max(f):
-    fmax = op2.Global(1, [-1000], dtype=float)
-    op2.par_loop(op2.Kernel("""void maxify(double *a, double *b)
-    {
-    a[0] = a[0] < fabs(b[0]) ? fabs(b[0]) : a[0];
-    }""", "maxify"),
-                 f.dof_dset.set, fmax(op2.MAX), f.dat(op2.READ))
-    return fmax.data[0]
-
-
 def setup_gw(dirname):
     nlayers = 10  # horizontal layers
     columns = 30  # number of columns
@@ -25,9 +15,6 @@ def setup_gw(dirname):
     H = 1.0e4  # Height position of the model top
     mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
 
-    # Space for initialising velocity
-    W_VectorCG1 = VectorFunctionSpace(mesh, "CG", 1)
-
     # vertical coordinate and normal
     x, y, z = SpatialCoordinate(mesh)
     k = Constant([0, 0, 1])
@@ -35,9 +22,7 @@ def setup_gw(dirname):
     fieldlist = ['u', 'p', 'b']
     timestepping = TimesteppingParameters(dt=dt)
     output = OutputParameters(dirname=dirname+"/gw_incompressible", dumplist=['u'], dumpfreq=5)
-    diagnostics = Diagnostics(*fieldlist)
     parameters = CompressibleParameters(geopotential=False)
-    diagnostic_fields = [CourantNumber()]
 
     state = IncompressibleState(mesh, vertical_degree=1, horizontal_degree=1,
                                 family="RTCF",
@@ -45,110 +30,40 @@ def setup_gw(dirname):
                                 timestepping=timestepping,
                                 output=output,
                                 parameters=parameters,
-                                diagnostics=diagnostics,
                                 fieldlist=fieldlist,
-                                diagnostic_fields=diagnostic_fields,
                                 on_sphere=False)
 
     # Initial conditions
     u0, p0, b0 = Function(state.V[0]), Function(state.V[1]), Function(state.V[2])
-
-    # Thermodynamic constants required for setting initial conditions
-    # and reference profiles
-    N = parameters.N
 
     # z.grad(bref) = N**2
     N = parameters.N
     bref = z*(N**2)
 
     b_b = Function(state.V[2]).interpolate(bref)
-
-    a = Constant(5.0e3)
-    deltab = Constant(1.0e-2)
-    H = Constant(H)
-    L = Constant(L)
-    b_pert = deltab*sin(pi*z/H)/(1 + (x - L/2)**2/a**2)
-    b0.interpolate(b_b + b_pert)
+    b0.interpolate(b_b)
     incompressible_hydrostatic_balance(state, b0, p0)
-    uinit = Function(W_VectorCG1).interpolate(as_vector([20.0,0.0,0.0]))
-    u0.project(uinit)
-
     state.initialise([u0, p0, b0])
-    state.set_reference_profiles(b_b)
-    state.output.meanfields = {'b':state.bbar}
-
-    # Set up advection schemes
-    ueqn = EulerPoincare(state, state.V[0])
-    beqn = EmbeddedDGAdvection(state, state.V[2], equation_form="advective")
-    advection_dict = {}
-    advection_dict["u"] = ThetaMethod(state, u0, ueqn)
-    advection_dict["b"] = SSPRK3(state, b0, beqn)
-
-    # Set up linear solver
-    params = {'ksp_type':'gmres',
-              'pc_type':'fieldsplit',
-              'pc_fieldsplit_type':'additive',
-              'fieldsplit_0_pc_type':'lu',
-              'fieldsplit_1_pc_type':'lu',
-              'fieldsplit_0_ksp_type':'preonly',
-              'fieldsplit_1_ksp_type':'preonly'}
-    linear_solver = IncompressibleSolver(state, L, params=params)
 
     # Set up forcing
     forcing = IncompressibleForcing(state)
 
-    # build time stepper
-    stepper = Timestepper(state, advection_dict, linear_solver,
-                          forcing)
-
-    return b0, p0, mesh, state, stepper, 5*dt
+    return state, forcing
 
 
 def run_gw_incompressible(dirname):
 
-    b0, p0, mesh, state, stepper, tmax = setup_gw(dirname)
-    stepper.run(t=0, tmax=tmax)
-
-    # get pressure gradient
-    V0 = state.V[0]
-    g = TrialFunction(V0)
-    wg = TestFunction(V0)
-
-    n = FacetNormal(mesh)
-
-    a = inner(wg,g)*dx
-    L = -div(wg)*p0*dx + inner(wg,n)*p0*ds_tb
-    pgrad = Function(V0)
-    solve(a == L, pgrad)
-
-    # get difference between b0 and dp0/dz
-    V1 = state.V[1]
-    phi = TestFunction(V1)
-    m = TrialFunction(V1)
-
-    a = phi*m*dx
-    L = phi*(b0-pgrad[2])*dx
-    diff = Function(V1)
-    solve(a == L, diff)
-
-    # get v component of the velocity
-    u = state.field_dict['u']
-    v = u[1]
-
-    tri = TrialFunction(V1)
-    tes = TestFunction(V1)
-
-    ax = inner(tes,tri)*dx
-    Lx = inner(tes,v)*dx
-    vproj = Function(V1)
-    solve(ax == Lx, vproj)
-
-    return diff, vproj
+    state, forcing = setup_gw(dirname)
+    dt = state.timestepping.dt
+    forcing.apply(dt, state.xn, state.xn, state.xn)
+    u = state.xn.split()[0]
+    w = Function(state.V[1]).interpolate(u[2])
+    return w
 
 
 def test_gw(tmpdir):
 
     dirname = str(tmpdir)
-    diff, v = run_gw_incompressible(dirname)
-    assert max(diff) < 0.05
-    assert max(v) < 1.e-10
+    w = run_gw_incompressible(dirname)
+    print w.dat.data.min(), w.dat.data.max()
+    assert max(abs(w.dat.data.min()), w.dat.data.max()) < 3e-8
