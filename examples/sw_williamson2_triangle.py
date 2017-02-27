1506812b5be6ddf823757b9e19f7653143e098e0
Colin Cotter <colin.cotter@imperial.ac.uk>
2017-02-27 14:06:45 -0500

    adapted for vector integration

--- examples/sw_williamson2_triangle.py
+++ examples/sw_williamson2_triangle.py
@@ -62,7 +62,8 @@ for ref_level, dt in ref_dt.items():
     state.initialise([('u', u0),
                       ('D', D0)])
 
-    ueqn = EulerPoincare(state, u0.function_space())
+    #ueqn = EulerPoincare(state, u0.function_space())
+    ueqn = AdvectionEquation(state, u0.function_space(), vector_manifold=True)
     Deqn = AdvectionEquation(state, D0.function_space(), equation_form="continuity")
     advected_fields = []
     advected_fields.append(("u", ThetaMethod(state, u0, ueqn)))
