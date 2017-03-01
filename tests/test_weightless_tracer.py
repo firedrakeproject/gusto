b0d5f607b0d78505a59aec8a3cc614f9aabc297b
tommbendall <tmb15@imperial.ac.uk>
2017-03-01 17:08:19 +0000

    correcting lint

--- tests/test_weightless_tracer.py
+++ tests/test_weightless_tracer.py
@@ -1,9 +1,7 @@
 from gusto import *
 from firedrake import PeriodicIntervalMesh, ExtrudedMesh, Expression, \
-    SpatialCoordinate, Constant, as_vector
-from math import pi
+    SpatialCoordinate, Constant
 import json
-import pytest
 
 
 def setup_tracer(dirname):
@@ -16,24 +14,23 @@ def setup_tracer(dirname):
 
     # make mesh
     m = PeriodicIntervalMesh(ncolumns, L)
-    mesh = ExtrudedMesh(m, layers = nlayers, layer_height = H / nlayers)
-
+    mesh = ExtrudedMesh(m, layers=nlayers, layer_height=(H / nlayers))
 
     fieldlist = ['u', 'rho', 'theta']
-    timestepping = TimesteppingParameters(dt = 10.0, maxk = 4, maxi = 1)
+    timestepping = TimesteppingParameters(dt=10.0, maxk=4, maxi=1)
     output = OutputParameters(dirname=dirname+"/tracer",
-                              dumpfreq = 1,
-                              dumplist = ['u'],
-                              perturbation_fields = ['theta', 'rho'],
-                              difference_fields = [['theta', 'tracer']])
+                              dumpfreq=1,
+                              dumplist=['u'],
+                              perturbation_fields=['theta', 'rho'],
+                              difference_fields=[['theta', 'tracer']])
     parameters = CompressibleParameters()
 
-    state = State(mesh, vertical_degree = 1, horizontal_degree = 1,
+    state = State(mesh, vertical_degree=1, horizontal_degree=1,
                   family="CG",
-                  timestepping = timestepping,
-                  output = output,
-                  parameters = parameters,
-                  fieldlist = fieldlist)
+                  timestepping=timestepping,
+                  output=output,
+                  parameters=parameters,
+                  fieldlist=fieldlist)
 
     # declare initial fields
     u0 = state.fields("u")
@@ -56,29 +53,31 @@ def setup_tracer(dirname):
     rho_b = Function(Vr)
 
     # Calculate initial rho
-    compressible_hydrostatic_balance(state, theta_b, rho_b, solve_for_rho = True)
+    compressible_hydrostatic_balance(state, theta_b, rho_b,
+                                     solve_for_rho=True)
 
     # set up perturbation to theta
     x = SpatialCoordinate(mesh)
-    theta_pert = Function(Vt).interpolate(Expression("sqrt(pow(x[0]-xc,2)+pow(x[1]-zc,2))" +
-                                                     "> rc ? 0.0 : 0.25*(1. + cos((pi/rc)*" +
-                                                     "(sqrt(pow((x[0]-xc),2)+pow((x[1]-zc),2)))))",
-                                                     xc=500., zc=350., rc=250.))
+    theta_pert = Function(Vt).interpolate(
+        Expression("sqrt(pow(x[0]-xc,2)+pow(x[1]-zc,2))" +
+                   "> rc ? 0.0 : 0.25*(1. + cos((pi/rc)*" +
+                   "(sqrt(pow((x[0]-xc),2)+pow((x[1]-zc),2)))))",
+                   xc=500., zc=350., rc=250.))
 
     theta0.interpolate(theta_b + theta_pert)
     rho0.interpolate(rho_b)
     tracer0.interpolate(theta0)
 
-    state.initialise({'u': u0, 'rho': rho0, 'theta': theta0, 'tracer' : tracer0})
+    state.initialise({'u': u0, 'rho': rho0, 'theta': theta0,
+                      'tracer': tracer0})
     state.set_reference_profiles({'rho': rho_b, 'theta': theta_b})
 
     # set up advection schemes
     ueqn = EulerPoincare(state, Vu)
-    rhoeqn = AdvectionEquation(state, Vr, equation_form = "continuity")
+    rhoeqn = AdvectionEquation(state, Vr, equation_form="continuity")
     thetaeqn = SUPGAdvection(state, Vt,
-                             supg_params = {"dg_direction":"horizontal"},
-                             equation_form = "advective")
-    
+                             supg_params={"dg_direction":"horizontal"},
+                             equation_form="advective")
 
     # build advection dictionary
     advection_dict = {}
@@ -87,33 +86,32 @@ def setup_tracer(dirname):
     advection_dict["theta"] = SSPRK3(state, theta0, thetaeqn)
     advection_dict["tracer"] = SSPRK3(state, tracer0, thetaeqn)
 
-
     # Set up linear solver
     schur_params = {'pc_type': 'fieldsplit',
-                'pc_fieldsplit_type': 'schur',
-                'ksp_type': 'gmres',
-                'ksp_monitor_true_residual': True,
-                'ksp_max_it': 100,
-                'ksp_gmres_restart': 50,
-                'pc_fieldsplit_schur_fact_type': 'FULL',
-                'pc_fieldsplit_schur_precondition': 'selfp',
-                'fieldsplit_0_ksp_type': 'richardson',
-                'fieldsplit_0_ksp_max_it': 5,
-                'fieldsplit_0_pc_type': 'bjacobi',
-                'fieldsplit_0_sub_pc_type': 'ilu',
-                'fieldsplit_1_ksp_type': 'richardson',
-                'fieldsplit_1_ksp_max_it': 5,
-                "fieldsplit_1_ksp_monitor_true_residual": True,
-                'fieldsplit_1_pc_type': 'gamg',
-                'fieldsplit_1_pc_gamg_sym_graph': True,
-                'fieldsplit_1_mg_levels_ksp_type': 'chebyshev',
-                'fieldsplit_1_mg_levels_ksp_chebyshev_estimate_eigenvalues': True,
-                'fieldsplit_1_mg_levels_ksp_chebyshev_estimate_eigenvalues_random': True,
-                'fieldsplit_1_mg_levels_ksp_max_it': 5,
-                'fieldsplit_1_mg_levels_pc_type': 'bjacobi',
-                'fieldsplit_1_mg_levels_sub_pc_type': 'ilu'}
+                    'pc_fieldsplit_type': 'schur',
+                    'ksp_type': 'gmres',
+                    'ksp_monitor_true_residual': True,
+                    'ksp_max_it': 100,
+                    'ksp_gmres_restart': 50,
+                    'pc_fieldsplit_schur_fact_type': 'FULL',
+                    'pc_fieldsplit_schur_precondition': 'selfp',
+                    'fieldsplit_0_ksp_type': 'richardson',
+                    'fieldsplit_0_ksp_max_it': 5,
+                    'fieldsplit_0_pc_type': 'bjacobi',
+                    'fieldsplit_0_sub_pc_type': 'ilu',
+                    'fieldsplit_1_ksp_type': 'richardson',
+                    'fieldsplit_1_ksp_max_it': 5,
+                    'fieldsplit_1_ksp_monitor_true_residual': True,
+                    'fieldsplit_1_pc_type': 'gamg',
+                    'fieldsplit_1_pc_gamg_sym_graph': True,
+                    'fieldsplit_1_mg_levels_ksp_type': 'chebyshev',
+                    'fieldsplit_1_mg_levels_ksp_chebyshev_estimate_eigenvalues': True,
+                    'fieldsplit_1_mg_levels_ksp_chebyshev_estimate_eigenvalues_random': True,
+                    'fieldsplit_1_mg_levels_ksp_max_it': 5,
+                    'fieldsplit_1_mg_levels_pc_type': 'bjacobi',
+                    'fieldsplit_1_mg_levels_sub_pc_type': 'ilu'}
 
-    linear_solver = CompressibleSolver(state, params = schur_params)
+    linear_solver = CompressibleSolver(state, params=schur_params)
 
     compressible_forcing = CompressibleForcing(state)
 
@@ -122,7 +120,6 @@ def setup_tracer(dirname):
                           compressible_forcing)
 
     return stepper, 100.0
-
 
 def run_tracer(dirname):
 
@@ -136,7 +133,7 @@ def test_tracer_setup(tmpdir):
     with open(path.join(dirname, "tracer/diagnostics.json"), "r") as f:
         data = json.load(f)
     print data.keys()
-    
+
     diffl2 = data["theta_minus_tracer"]["l2"][-1] / data["theta"]["l2"][0]
 
     assert abs(diffl2) < 1e-5
