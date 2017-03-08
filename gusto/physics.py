3c4c2416c6cb68068637c37af85095f1f6b3409a
tommbendall <tmb15@imperial.ac.uk>
2017-03-08 17:04:17 +0000

    insert the horrendous number of moist parameters

--- gusto/physics.py
+++ gusto/physics.py
@@ -31,8 +31,21 @@ class Condensation(Physics):
         super(Condensation, self).__init__(state)
 
         # define some parameters as attributes
+        dt = state.timestepping.dt
+        cp = state.parameters.cp
+        cv = state.parameters.cv
+        c_pv = state.parameters.c_pv
+        c_pl = state.parameters.c_pl
+        c_vv = state.parameters.c_vv
+        R_v = state.parameters.R_v
+        L_v0 = state.parameters.L_v0
+        T_0 = state.parameters.T_0
+        w_sat1 = state.parameters.w_sat1
+        w_sat2 = state.parameters.w_sat2
+        w_sat3 = state.parameters.w_sat3
+        w_sat4 = state.parameters.w_sat4
 
     def apply(self):
-        self.water_v.assign( ... )
-        self.water_c.assign( ... )
-        self.theta.assign( ... )
+        self.water_v.assign(self.water_v_new)
+        self.water_c.assign(self.water_c_new)
+        self.theta.assign(self.theta_new) 
