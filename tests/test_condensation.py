d34ada5fac5f05953b982f84f16f172339a5c2c7
tommbendall <tmb15@imperial.ac.uk>
2017-03-15 12:51:58 +0000

    lint corrections again

--- tests/test_condensation.py
+++ tests/test_condensation.py
@@ -1,12 +1,13 @@
 from gusto import *
 from firedrake import as_vector, Constant, sin, PeriodicIntervalMesh, \
-    SpatialCoordinate, ExtrudedMesh, Expression \
+    SpatialCoordinate, ExtrudedMesh, Expression
 import json
 from math import pi
 
 # This setup creates a bubble of water vapour that is advected
 # by a prescribed velocity. The test passes if the integral
 # of the water mixing ratio is conserved.
+
 
 def setup_condens(dirname):
 
