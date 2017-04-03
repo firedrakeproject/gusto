e99ebcaf6d4a111f45c324e6b98d3365a96fef56
Paul Burns <p.burns2@exeter.ac.uk>
2017-04-03 09:37:19 +0100

    Numerical parameter choices guided by Courant number (and physical problem) to optimise for speed. Code added to initialise b_pert in different ways.

--- examples/boussinesq_2d_lab.py
+++ examples/boussinesq_2d_lab.py
@@ -1,10 +1,13 @@
 from gusto import *
 from firedrake import PeriodicIntervalMesh, ExtrudedMesh, \
-    sin, SpatialCoordinate
+    sin, exp, pi, SpatialCoordinate
 import numpy as np
+import sympy as sp
+from sympy.stats import Normal
 import sys
 
-dt = 0.05
+dt = 0.1
+
 if '--running-tests' in sys.argv:
     tmax = dt
     # avoid using mumps on Travis
@@ -16,7 +19,7 @@ if '--running-tests' in sys.argv:
                             'fieldsplit_0_ksp_type':'preonly',
                             'fieldsplit_1_ksp_type':'preonly'}
 else:
-    tmax = 3600.
+    tmax = 3600*6.
     # use default linear solver parameters (i.e. mumps)
     linear_solver_params = None
 
@@ -24,12 +27,12 @@ else:
 # set up mesh
 ##############################################################################
 # Construct 1d periodic base mesh for idealised lab experiment of Park et al. (1994)
-columns = 100  # number of columns
+columns = 35  # number of columns
 L = 0.2
 m = PeriodicIntervalMesh(columns, L)
 
 # build 2D mesh by extruding the base mesh
-nlayers = 225  # horizontal layers
+nlayers = 80  # horizontal layers
 H = 0.45  # Height position of the model top
 mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
 
@@ -54,19 +57,20 @@ timestepping = TimesteppingParameters(dt=dt)
 # class containing output parameters
 # all values not explicitly set here use the default values provided
 # and documented in configuration.py
-output = OutputParameters(dirname='boussinesq_2d_lab', dumpfreq=10, dumplist=['u', 'p', 'b'], perturbation_fields=['b'])
+output = OutputParameters(dirname='boussinesq_2d_lab', dumpfreq=50, dumplist=['u', 'p', 'b'], perturbation_fields=['b'])
 
 # class containing physical parameters
 # all values not explicitly set here use the default values provided
 # and documented in configuration.py
-parameters = CompressibleParameters(p_0=106141.3045, N=1.957)
+rho_0 = Constant(1090.95075)
+parameters = CompressibleParameters(N=1.957, p_0=106141.3045)
 
 # Physical parameters adjusted for idealised lab experiment of Park et al. (1994):
 # The value of the background buoyancy frequency N is that for their run number 18, which has clear stair-step features.
 # p_0 was set by assuming an initially hydrostatic fluid and a reference density rho_0=1090.95075 kg m^(-3).
 # The reference density was found by estimating drho/dz from Fig. 7a of Park et al. (1994), converting to SI units,
 # and then using the N^2 value above.
-
+# p_0=106141.3045
 
 # class for diagnostics
 # fields passed to this class will have basic diagnostics computed
@@ -101,28 +105,38 @@ b0 = state.fields("b")
 # from the parameters class. x[1]=z and comes from x=SpatialCoordinate(mesh)
 x = SpatialCoordinate(mesh)
 N = parameters.N
-bref = x[1]*(N**2)
+bref = N**2*(x[1]-H)
 # interpolate the expression to the function
 Vb = b0.function_space()
 b_b = Function(Vb).interpolate(bref)
 
 # Define constants for bouyancy perturbation:
-g = Constant(9.810616)
-rho_0 = Constant(1090.95075)
-A_x1 = Constant(0) 			# Initial amplitude of internal waves in x-direction (e.g., try setting to zero initially)
-A_z1 = Constant(g/rho_0 * 100./3) 	# Initial amplitude of internal waves in z-direction
-lmda_x1 = Constant(1.3/100)		# Horizontal wavelength of internal waves
-lmda_z1 = Constant(1.3/100)		# Vertical wavelength of internal waves
-k1 = Constant(2*np.pi/lmda_x1)		# Horizontal wavenumber of internal waves
-m1 = Constant(2*np.pi/lmda_z1)		# Vertical wavenumber of internal waves
-
+g = parameters.g
+A_x1 = Constant(0) 				# Initial amplitude of internal waves in x-direction (e.g., try setting to zero initially)
+A_z1 = Constant(g/rho_0 * 100./3)	 	# Initial amplitude of internal waves in z-direction
+lmda_x1 = Constant(1.3/100)			# Horizontal wavelength of internal waves
+#lmda_z1 = Constant(1.3/100)			# Vertical wavelength of internal waves
+lmda_z1 = Constant(20./100)			# Vertical wavelength of internal waves
+k1 = Constant(2*np.pi/lmda_x1)			# Horizontal wavenumber of internal waves
+m1 = Constant(2*np.pi/lmda_z1)			# Vertical wavenumber of internal waves
 
 # Define bouyancy perturbation to represent background soup of internal waves in idealised lab scenario of Park et al.
-b_pert = A_x1*sin(k1*x[0]) + A_z1*sin(m1*x[1])
+#b_pert = A_x1*sin(k1*x[0]) + A_z1/2.*sin(m1*x[1])
+#sigma = 0.01
+#b_pert = A_z1*exp( -( x[1] - H/2 )**2 / (2*sigma**2) )
+
+r = Function(b0.function_space()).assign(Constant(0.0))
+r.dat.data[:] += np.random.uniform(low=-1., high=1., size=r.dof_dset.size)
+b_pert = r*A_z1/2.
+
+#b_pert = sp.Piecewise( (0, x[1] < H/2-0.01), (0, x[1] > H/2+0.01), (A_z1, H/2-0.01 >= x[1] <= H/2+0.01, True) ) - doesn't work
+#b_pert = sp.integrate( A_z1 * DiracDelta(x[1]-H/2), (x[1],0,H) ) - doesn't work
+
 # interpolate the expression to the function
 b0.interpolate(b_b + b_pert)
 
-incompressible_hydrostatic_balance(state, b_b, p0)
+
+incompressible_hydrostatic_balance(state, b_b, p0, top=False)
 
 # pass these initial conditions to the state.initialise method
 state.initialise({"u": u0, "p": p0, "b": b0})
@@ -155,6 +169,25 @@ linear_solver = IncompressibleSolver(state, L, params=linear_solver_params)
 # Set up forcing
 ##############################################################################
 forcing = IncompressibleForcing(state)
+
+
+##############################################################################
+#Set up diffusion scheme
+##############################################################################
+# mu is a numerical parameter
+# kappa is the diffusion constant
+#Vu = u0.function_space()
+#Vb = b0.function_space()
+#delta = ?
+
+#bcs = [DirichletBC(Vu, 0.0, "bottom"), DirichletBC(Vu, 0.0, "top")]
+
+#diffusion_dict = {"u": InteriorPenalty(state, Vu, kappa=Constant(75.),
+#                                           mu=Constant(10./delta), bcs=bcs),
+#                      "b": InteriorPenalty(state, Vb, kappa=Constant(75.),
+#                                               mu=Constant(10./delta))}
+
+
 
 ##############################################################################
 # build time stepper
