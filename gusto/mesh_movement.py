e6cae6c5085dfcc6fc0eb1ee152618062abb642b
Jemma Shipton <j.shipton@imperial.ac.uk>
2017-05-03 23:21:53 +0100

    remove extra divide by dt

--- gusto/mesh_movement.py
+++ gusto/mesh_movement.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from firedrake import inner, sqrt, acos
 
 
-def spherical_logarithm(X0, X1, v, dt):
+def spherical_logarithm(X0, X1, v):
     """
     Find vector function v such that X1 = exp(v)X0 on
     a sphere of radius R, centre the origin.
@@ -13,4 +13,4 @@ def spherical_logarithm(X0, X1, v, dt):
     R = sqrt(inner(X0, X0))
     normv = sqrt(inner(v, v))
     theta = acos(inner(X0, X1)/R**2)
-    v.interpolate(theta*R*v/dt/normv)
+    v.interpolate(theta*R*v/normv)
