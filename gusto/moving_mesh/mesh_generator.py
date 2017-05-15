cec5eab3a11ea68f95f8e628b3c03526aec6ebcd
Andrew McRae <a.t.t.mcrae@bath.ac.uk>
2017-05-15 13:54:02 +0100

    fix docstring

--- gusto/moving_mesh/mesh_generator.py
+++ gusto/moving_mesh/mesh_generator.py
@@ -5,8 +5,6 @@ from abc import ABCMeta, abstractmethod
 class MeshGenerator(object):
     """
     Base class for a mesh generator for a moving mesh method.
-
-    :arg mesh: mesh for underlying simulation
     """
     __metaclass__ = ABCMeta
 
