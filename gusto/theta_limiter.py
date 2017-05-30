25ec1808a05e749991cff308cc53c992b29e7e7c
tommbendall <tmb15@imperial.ac.uk>
2017-05-30 11:13:46 +0100

    restructuring of theta limiter code

--- gusto/theta_limiter.py
+++ gusto/theta_limiter.py
@@ -8,6 +8,40 @@ from firedrake.slope_limiter.limiter import Limiter
 from firedrake.slope_limiter.vertex_based_limiter import VertexBasedLimiter
 __all__ = ("ThetaLimiter",)
 
+_copy_to_vertex_space_loop = """
+theta_hat[0][0] = theta[0][0];
+theta_hat[1][0] = theta[1][0];
+theta_hat[2][0] = theta[3][0];
+theta_hat[3][0] = theta[4][0];
+"""
+_copy_from_vertex_space_loop = """
+theta[0][0] = theta_hat[0][0];
+theta[1][0] = theta_hat[1][0];
+theta[3][0] = theta_hat[2][0];
+theta[4][0] = theta_hat[3][0];
+"""
+
+_check_midpoint_values_loop = """
+if (theta[2][0] > fmax(theta[0][0], theta[1][0]))
+    theta[2][0] = 0.5 * (theta[0][0] + theta[1][0]);
+else if (theta[2][0] < fmin(theta[0][0], theta[1][0]))
+    theta[2][0] = 0.5 * (theta[0][0] + theta[1][0]);
+if (theta[5][0] > fmax(theta[3][0], theta[4][0]))
+    theta[5][0] = 0.5 * (theta[3][0] + theta[4][0]);
+else if (theta[5][0] < fmin(theta[3][0], theta[4][0]))
+    theta[5][0] = 0.5 * (theta[3][0] + theta[4][0]);
+"""
+
+_weight_kernel = """
+for (int i=0; i<weight.dofs; ++i) {
+    weight[i][0] += 1.0;
+    }"""
+
+_average_kernel = """
+for (int i=0; i<vrec.dofs; ++i) {
+        vrec[i][0] += v_b[i][0]/weight[i][0];
+        }"""
+
 
 class ThetaLimiter(Limiter):
     """
@@ -30,47 +64,16 @@ class ThetaLimiter(Limiter):
         self.Q1DG = FunctionSpace(self.Vt.mesh(), 'DG', 1) # space with only vertex DOF
         self.vertex_limiter = VertexBasedLimiter(self.Q1DG)
         self.theta_hat = Function(self.Q1DG) # theta function with only vertex DOF
-
-        self._copy_to_vertex_space_loop = """
-theta_hat[0][0] = theta[0][0];
-theta_hat[1][0] = theta[1][0];
-theta_hat[2][0] = theta[3][0];
-theta_hat[3][0] = theta[4][0];
-"""
-        self._copy_from_vertex_space_loop = """
-theta[0][0] = theta_hat[0][0];
-theta[1][0] = theta_hat[1][0];
-theta[3][0] = theta_hat[2][0];
-theta[4][0] = theta_hat[3][0];
-"""
-
-        self._check_midpoint_values_loop = """
-if (theta[2][0] > fmax(theta[0][0], theta[1][0]))
-    theta[2][0] = 0.5 * (theta[0][0] + theta[1][0]);
-else if (theta[2][0] < fmin(theta[0][0], theta[1][0]))
-    theta[2][0] = 0.5 * (theta[0][0] + theta[1][0]);
-if (theta[5][0] > fmax(theta[3][0], theta[4][0]))
-    theta[5][0] = 0.5 * (theta[3][0] + theta[4][0]);
-else if (theta[5][0] < fmin(theta[3][0], theta[4][0]))
-    theta[5][0] = 0.5 * (theta[3][0] + theta[4][0]);
-"""
-
-        self._weight_kernel = """
-for (int i=0; i<weight.dofs; ++i) {
-    weight[i][0] += 1.0;
-    }"""
-
-        self._average_kernel = """
-for (int i=0; i<vrec.dofs; ++i) {
-        vrec[i][0] += v_b[i][0]/weight[i][0];
-        }"""
-
+        self.w = Function(self.Vt)
+        self.result = Function(self.Vt)
+        par_loop(_weight_kernel, dx, {"weight": (self.w, INC)})
+        
     def copy_vertex_values(self, field):
         """
         Copies the vertex values from temperature space to
         Q1DG space which only has vertices.
         """
-        par_loop(self._copy_to_vertex_space_loop, dx,
+        par_loop(_copy_to_vertex_space_loop, dx,
                  {"theta": (field, READ),
                   "theta_hat": (self.theta_hat, RW)})
 
@@ -79,9 +82,9 @@ for (int i=0; i<vrec.dofs; ++i) {
         Copies the vertex values back from the Q1DG space to
         the original temperature space.
         """
-        par_loop(self._copy_from_vertex_space_loop, dx,
+        par_loop(_copy_from_vertex_space_loop, dx,
                  {"theta": (field, RW),
-                  "theta_hat": (self.theta_hat, RW)})
+                  "theta_hat": (self.theta_hat, READ)})
 
     def check_midpoint_values(self, field):
         """
@@ -89,7 +92,7 @@ for (int i=0; i<vrec.dofs; ++i) {
         and more than the minimum values. Amends them to the average
         if they are not.
         """
-        par_loop(self._check_midpoint_values_loop, dx,
+        par_loop(_check_midpoint_values_loop, dx,
                  {"theta": (field, RW)})
 
     def remap_to_embedded_space(self, field):
@@ -97,14 +100,11 @@ for (int i=0; i<vrec.dofs; ++i) {
         Not entirely sure yet. Remap to embedded space?
         """
 
-        w = Function(self.Vt)
-        result = Function(self.Vt)
-        par_loop(self._weight_kernel, dx, {"weight": (w, INC)})
-        par_loop(self._average_kernel, dx, {"vrec": (result, INC),
+        self.result.assign(0.)
+        par_loop(_average_kernel, dx, {"vrec": (self.result, INC),
                                             "v_b": (field, READ),
-                                            "weight": (w, READ)})
-
-        return result
+                                            "weight": (self.w, READ)})
+        field.assign(self.result)
         
     def compute_bounds(self, field):
         """
@@ -123,8 +123,8 @@ for (int i=0; i<vrec.dofs; ++i) {
         assert field.function_space() == self.Vt, \
             'Given field does not belong to this objects function space'
 
-        self.copy_vertex_values(field)
-        self.vertex_limiter.apply(self.theta_hat)
-        self.copy_vertex_values_back(field)
-        self.check_midpoint_values(field)
-        field.assign(self.remap_to_embedded_space(field))
+        #self.copy_vertex_values(field)
+        #self.vertex_limiter.apply(self.theta_hat)
+        #self.copy_vertex_values_back(field)
+        #self.check_midpoint_values(field)
+        #self.remap_to_embedded_space(field)
