d3f7e1fb9714d87d3f1f7d8f78be3fbb8981f488
tommbendall <tmb15@imperial.ac.uk>
2017-06-22 17:33:03 +0100

    removal of theta

--- tests/test_rho_limiter.py
+++ tests/test_rho_limiter.py
@@ -24,10 +24,10 @@ def setup_rho_limiter(dirname):
 
     fieldlist = ['u', 'rho', 'theta']
     timestepping = TimesteppingParameters(dt=1.0, maxk=4, maxi=1)
-    output = OutputParameters(dirname=dirname+"/theta_limiter",
+    output = OutputParameters(dirname=dirname+"/rho_limiter",
                               dumpfreq=5,
                               dumplist=['u'],
-                              perturbation_fields=['theta', 'rho'])
+                              perturbation_fields=['rho'])
     parameters = CompressibleParameters()
 
     state = State(mesh, vertical_degree=1, horizontal_degree=1,
@@ -40,35 +40,27 @@ def setup_rho_limiter(dirname):
     # declare initial fields
     u0 = state.fields("u")
     rho0 = state.fields("rho")
-    theta0 = state.fields("theta")
 
     # spaces
     Vpsi = FunctionSpace(mesh, "CG", 2)
-    Vt = theta0.function_space()
     Vr = rho0.function_space()
 
     # make a gradperp
     gradperp = lambda u: as_vector([-u.dx(1), u.dx(0)])
 
     # Isentropic background state
-    Tsurf = 300.
-    thetab = Constant(Tsurf)
-
-    theta_b = Function(Vt).interpolate(thetab)
-    rho_b = Function(Vr)
-
-    # Calculate initial rho
-    # compressible_hydrostatic_balance(state, theta_b, rho_b,
-    #                                  solve_for_rho=True)
+    rho_surf = 1.
+    rhob = Constant(rho_surf)
+    rho_b = Function(Vr).interpolate(rhob)
 
     # set up bubble
     xc = 200.
     zc = 200.
     rc = 100.
-    theta_pert = Function(Vt).interpolate(conditional(sqrt((x - xc) ** 2.0) < rc,
-                                                      conditional(sqrt((z - zc) ** 2.0) < rc,
-                                                                  Constant(2.0),
-                                                                  Constant(0.0)), Constant(0.0)))
+    rho_pert = Function(Vt).interpolate(conditional(sqrt((x - xc) ** 2.0) < rc,
+                                                    conditional(sqrt((z - zc) ** 2.0) < rc,
+                                                                Constant(0.2),
+                                                                Constant(0.0)), Constant(0.0)))
 
     # set up velocity field
     u_max = Constant(1.0)
@@ -77,21 +69,19 @@ def setup_rho_limiter(dirname):
 
     psi0 = Function(Vpsi).interpolate(psi_expr)
     u0.project(gradperp(psi0))
-    theta0.interpolate(theta_b + theta_pert)
-    rho0.interpolate(rho_b)
+    rho0.interpolate(rho_b + rho_pert)
 
-    state.initialise({'u': u0, 'rho': rho0, 'theta': theta0})
-    state.set_reference_profiles({'rho': rho_b, 'theta': theta_b})
+    state.initialise({'u': u0, 'rho': rho0})
+    state.set_reference_profiles({'rho': rho_b})
 
     # set up advection schemes
     rhoeqn = AdvectionEquation(state, Vr, equation_form="continuity")
-    thetaeqn = EmbeddedDGAdvection(state, Vt, equation_form="advective")
+#    thetaeqn = EmbeddedDGAdvection(state, Vt, equation_form="advective")
 
     # build advection dictionary
     advection_dict = {}
     advection_dict["u"] = NoAdvection(state, u0, None)
     advection_dict["rho"] = SSPRK3(state, rho0, rhoeqn)
-    advection_dict["theta"] = SSPRK3(state, theta0, thetaeqn, limiter=ThetaLimiter(thetaeqn.space))
 
     # build time stepper
     stepper = AdvectionTimestepper(state, advection_dict)
