d07a325a8514aa299706464e80948c50350c54ea
tommbendall <tmb15@imperial.ac.uk>
2017-06-22 17:29:25 +0100

    first commit with new rho limiter files

---
+++ gusto/rho_limiter.py
@@ -0,0 +1,109 @@
+from __future__ import absolute_import, print_function, division
+from firedrake import dx, assemble, LinearSolver
+from firedrake.function import Function
+from firedrake.functionspace import FunctionSpace
+from firedrake.parloops import par_loop, READ, RW, INC
+from firedrake.ufl_expr import TrialFunction, TestFunction
+from firedrake.slope_limiter.limiter import Limiter
+from firedrake.slope_limiter.vertex_based_limiter import VertexBasedLimiter
+__all__ = ("RhoLimiter",)
+
+_copy_to_vertex_space_loop = """
+theta_hat[0][0] = theta[0][0];
+theta_hat[1][0] = theta[1][0];
+theta_hat[2][0] = theta[3][0];
+theta_hat[3][0] = theta[4][0];
+"""
+_copy_from_vertex_space_loop = """
+theta[0][0] = theta_hat[0][0];
+theta[1][0] = theta_hat[1][0];
+theta[3][0] = theta_hat[2][0];
+theta[4][0] = theta_hat[3][0];
+"""
+
+_weight_kernel = """
+for (int i=0; i<weight.dofs; ++i) {
+    weight[i][0] += 1.0;
+    }"""
+
+_average_kernel = """
+for (int i=0; i<vrec.dofs; ++i) {
+        vrec[i][0] += v_b[i][0]/weight[i][0];
+        }"""
+
+
+class RhoLimiter(Limiter):
+    """
+    A vertex based limiter for P1DG fields.
+
+    This limiter implements the vertex-based limiting scheme described in
+    Dmitri Kuzmin, "A vertex-based hierarchical slope limiter for p-adaptive
+    discontinuous Galerkin methods". J. Comp. Appl. Maths (2010)
+    http://dx.doi.org/10.1016/j.cam.2009.05.028
+    """
+
+    def __init__(self, space):
+        """
+        Initialise limiter
+
+        :param space : FunctionSpace instance
+        """
+
+        self.Vt = space
+        self.Q1DG = FunctionSpace(self.Vt.mesh(), 'DG', 1) # space with only vertex DOF
+        self.vertex_limiter = VertexBasedLimiter(self.Q1DG)
+        self.theta_hat = Function(self.Q1DG) # theta function with only vertex DOF
+        self.w = Function(self.Vt)
+        self.result = Function(self.Vt)
+        par_loop(_weight_kernel, dx, {"weight": (self.w, INC)})
+        
+    def copy_vertex_values(self, field):
+        """
+        Copies the vertex values from temperature space to
+        Q1DG space which only has vertices.
+        """
+        par_loop(_copy_to_vertex_space_loop, dx,
+                 {"theta": (field, READ),
+                  "theta_hat": (self.theta_hat, RW)})
+
+    def copy_vertex_values_back(self, field):
+        """
+        Copies the vertex values back from the Q1DG space to
+        the original temperature space.
+        """
+        par_loop(_copy_from_vertex_space_loop, dx,
+                 {"theta": (field, RW),
+                  "theta_hat": (self.theta_hat, READ)})
+
+    def remap_to_embedded_space(self, field):
+        """
+        Not entirely sure yet. Remap to embedded space?
+        """
+
+        self.result.assign(0.)
+        par_loop(_average_kernel, dx, {"vrec": (self.result, INC),
+                                            "v_b": (field, READ),
+                                            "weight": (self.w, READ)})
+        field.assign(self.result)
+        
+    def compute_bounds(self, field):
+        """
+        Blank
+        """
+
+    def apply_limiter(self, field):
+        """
+        Blank
+        """
+
+    def apply(self, field):
+        """
+        Re-computes centroids and applies limiter to given field
+        """
+        assert field.function_space() == self.Vt, \
+            'Given field does not belong to this objects function space'
+
+        self.copy_vertex_values(field)
+        self.vertex_limiter.apply(self.theta_hat)
+        self.copy_vertex_values_back(field)
+        #self.remap_to_embedded_space(field)
---
+++ tests/test_rho_limiter.py
@@ -0,0 +1,111 @@
+from gusto import *
+from firedrake import as_vector, Constant, sin, PeriodicIntervalMesh, \
+    SpatialCoordinate, ExtrudedMesh, Expression
+from gusto.theta_limiter import RhoLimiter
+import json
+from math import pi
+
+# This setup creates a sharp bubble of warm air in a vertical slice
+# This bubble is then advected by a prescribed advection scheme
+
+
+def setup_rho_limiter(dirname):
+
+    # declare grid shape, with length L and height H
+    L = 1000.
+    H = 400.
+    nlayers = int(H / 20.)
+    ncolumns = int(L / 20.)
+
+    # make mesh
+    m = PeriodicIntervalMesh(ncolumns, L)
+    mesh = ExtrudedMesh(m, layers=nlayers, layer_height=(H / nlayers))
+    x,z = SpatialCoordinate(mesh)
+
+    fieldlist = ['u', 'rho', 'theta']
+    timestepping = TimesteppingParameters(dt=1.0, maxk=4, maxi=1)
+    output = OutputParameters(dirname=dirname+"/theta_limiter",
+                              dumpfreq=5,
+                              dumplist=['u'],
+                              perturbation_fields=['theta', 'rho'])
+    parameters = CompressibleParameters()
+
+    state = State(mesh, vertical_degree=1, horizontal_degree=1,
+                  family="CG",
+                  timestepping=timestepping,
+                  output=output,
+                  parameters=parameters,
+                  fieldlist=fieldlist)
+
+    # declare initial fields
+    u0 = state.fields("u")
+    rho0 = state.fields("rho")
+    theta0 = state.fields("theta")
+
+    # spaces
+    Vpsi = FunctionSpace(mesh, "CG", 2)
+    Vt = theta0.function_space()
+    Vr = rho0.function_space()
+
+    # make a gradperp
+    gradperp = lambda u: as_vector([-u.dx(1), u.dx(0)])
+
+    # Isentropic background state
+    Tsurf = 300.
+    thetab = Constant(Tsurf)
+
+    theta_b = Function(Vt).interpolate(thetab)
+    rho_b = Function(Vr)
+
+    # Calculate initial rho
+    # compressible_hydrostatic_balance(state, theta_b, rho_b,
+    #                                  solve_for_rho=True)
+
+    # set up bubble
+    xc = 200.
+    zc = 200.
+    rc = 100.
+    theta_pert = Function(Vt).interpolate(conditional(sqrt((x - xc) ** 2.0) < rc,
+                                                      conditional(sqrt((z - zc) ** 2.0) < rc,
+                                                                  Constant(2.0),
+                                                                  Constant(0.0)), Constant(0.0)))
+
+    # set up velocity field
+    u_max = Constant(1.0)
+
+    psi_expr = - u_max * z
+
+    psi0 = Function(Vpsi).interpolate(psi_expr)
+    u0.project(gradperp(psi0))
+    theta0.interpolate(theta_b + theta_pert)
+    rho0.interpolate(rho_b)
+
+    state.initialise({'u': u0, 'rho': rho0, 'theta': theta0})
+    state.set_reference_profiles({'rho': rho_b, 'theta': theta_b})
+
+    # set up advection schemes
+    rhoeqn = AdvectionEquation(state, Vr, equation_form="continuity")
+    thetaeqn = EmbeddedDGAdvection(state, Vt, equation_form="advective")
+
+    # build advection dictionary
+    advection_dict = {}
+    advection_dict["u"] = NoAdvection(state, u0, None)
+    advection_dict["rho"] = SSPRK3(state, rho0, rhoeqn)
+    advection_dict["theta"] = SSPRK3(state, theta0, thetaeqn, limiter=ThetaLimiter(thetaeqn.space))
+
+    # build time stepper
+    stepper = AdvectionTimestepper(state, advection_dict)
+
+    return stepper, 500.0
+
+
+def run_rho_limiter(dirname):
+
+    stepper, tmax = setup_rho_limiter(dirname)
+    stepper.run(t=0, tmax=tmax)
+
+
+def test_rho_limiter_setup(tmpdir):
+
+    dirname = str(tmpdir)
+    run_rho_limiter(dirname)
