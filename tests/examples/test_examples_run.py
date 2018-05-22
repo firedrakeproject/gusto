1222220a86eef1eb5cf6db4226ca9b8a97e5cda4
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-05-22 22:41:50 +0100

    Test hybridized compressible examples

--- tests/examples/test_examples_run.py
+++ tests/examples/test_examples_run.py
@@ -8,6 +8,19 @@ import sys
 cwd = abspath(dirname(__file__))
 examples_dir = join(cwd, "..", "..", "examples")
 
+# Examples which use the compressible solvers
+compressible_examples = ["compressible_eady.py",
+                         "dense_bubble.py",
+                         "dry_bf_bubble.py",
+                         "h_mountain.py",
+                         "moist_bf_bubble.py",
+                         "nh_mountain.py",
+                         "rising_bubble.py",
+                         "sk_hydrostatic.py",
+                         "sk_linear_advection.py",
+                         "sk_nonlinear.py",
+                         "tracer.py"]
+
 
 @pytest.fixture(params=glob.glob("%s/*.py" % examples_dir),
                 ids=lambda x: basename(x))
@@ -15,8 +28,23 @@ def example_file(request):
     return abspath(request.param)
 
 
+@pytest.fixture(params=compressible_examples)
+def compressible_example_file(request):
+    example = request.param
+    path = "%s/%s" % (examples_dir, example)
+    return abspath(path)
+
+
 def test_example_runs(example_file, tmpdir, monkeypatch):
     # This ensures that the test writes output in a temporary
     # directory, rather than where pytest was run from.
     monkeypatch.chdir(tmpdir)
     subprocess.check_call([sys.executable, example_file, "--running-tests"])
+
+
+def test_hybridized_compressible_examples(compressible_example_file, tmpdir, monkeypatch):
+    # This ensures that the test writes output in a temporary
+    # directory, rather than where pytest was run from.
+    monkeypatch.chdir(tmpdir)
+    subprocess.check_call([sys.executable, compressible_example_file,
+                           "--running-tests --hybridization"])
