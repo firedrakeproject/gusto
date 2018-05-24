c71d3abbc2bbaf05c08fdafc366758e1f9c3769c
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-05-24 11:50:49 +0100

    Edit examples testing

--- tests/examples/test_examples_run.py
+++ tests/examples/test_examples_run.py
@@ -28,11 +28,12 @@ def example_file(request):
     return abspath(request.param)
 
 
-@pytest.fixture(params=compressible_examples)
+@pytest.fixture(params=[ex for glb in [glob.glob("%s/%s" % (examples_dir, exmp))
+                                       for exmp in compressible_examples]
+                        for ex in glb],
+                ids=lambda x: basename(x))
 def compressible_example_file(request):
-    example = request.param
-    path = "%s/%s" % (examples_dir, example)
-    return abspath(path)
+    return abspath(request.param)
 
 
 def test_example_runs(example_file, tmpdir, monkeypatch):
@@ -47,4 +48,4 @@ def test_hybridized_compressible_examples(compressible_example_file, tmpdir, mon
     # directory, rather than where pytest was run from.
     monkeypatch.chdir(tmpdir)
     subprocess.check_call([sys.executable, compressible_example_file,
-                           "--running-tests --hybridization"])
+                           "--running-tests", "--hybridization"])
