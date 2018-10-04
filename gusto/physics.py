9d01688fea77e56adf2096b04d12e394db101712
Tom Bendall <t.bendall15@imperial.ac.uk>
2018-10-04 17:30:26 +0100

    add liquid water to condensation scheme and start making option for sedimentation

--- gusto/physics.py
+++ gusto/physics.py
@@ -46,6 +46,11 @@ class Condensation(Physics):
         self.water_v = state.fields('water_v')
         self.water_c = state.fields('water_c')
         rho = state.fields('rho')
+        try:
+            rain = state.fields('rain')
+            water_l = self.water_c + rain
+        except:
+            water_l = self.water_c
 
         # declare function space
         Vt = self.theta.function_space()
@@ -73,8 +78,8 @@ class Condensation(Physics):
         p = thermodynamics.p(state.parameters, Pi)
         L_v = thermodynamics.Lv(state.parameters, T)
         R_m = R_d + R_v * self.water_v
-        c_pml = cp + c_pv * self.water_v + c_pl * self.water_c
-        c_vml = cv + c_vv * self.water_v + c_pl * self.water_c
+        c_pml = cp + c_pv * self.water_v + c_pl * water_l
+        c_vml = cv + c_vv * self.water_v + c_pl * water_l
 
         # use Teten's formula to calculate w_sat
         w_sat = thermodynamics.r_sat(state.parameters, T, p)
@@ -83,22 +88,20 @@ class Condensation(Physics):
         dot_r_cond = ((self.water_v - w_sat) /
                       (dt * (1.0 + ((L_v ** 2.0 * w_sat) /
                                     (cp * R_v * T ** 2.0)))))
-        dot_r_cond = self.water_v - w_sat
 
         # make cond_rate function, that needs to be the same for all updates in one time step
-        self.cond_rate = Function(Vt)
-        self.cond_rate = state.fields('cond_rate', Vt)
+        cond_rate = Function(Vt)
 
         # adjust cond rate so negative concentrations don't occur
         self.lim_cond_rate = Interpolator(conditional(dot_r_cond < 0,
                                                       max_value(dot_r_cond, - self.water_c / dt),
-                                                      min_value(dot_r_cond, self.water_v / dt)), self.cond_rate)
+                                                      min_value(dot_r_cond, self.water_v / dt)), cond_rate)
 
         # tell the prognostic fields what to update to
-        self.water_v_new = Interpolator(self.water_v - dt * self.cond_rate, Vt)
-        self.water_c_new = Interpolator(self.water_c + dt * self.cond_rate, Vt)
+        self.water_v_new = Interpolator(self.water_v - dt * cond_rate, Vt)
+        self.water_c_new = Interpolator(self.water_c + dt * cond_rate, Vt)
         self.theta_new = Interpolator(self.theta *
-                                      (1.0 + dt * self.cond_rate *
+                                      (1.0 + dt * cond_rate *
                                        (cv * L_v / (c_vml * cp * T) -
                                         R_v * cv * c_pml / (R_m * cp * c_vml))), Vt)
 
@@ -116,13 +119,21 @@ class Fallout(Physics):
     The fallout process of hydrometeors.
 
     :arg state :class: `.State.` object.
+    :arg moments: an integer denoting which rainfall scheme to use.
+                  Corresponds to the number of moments of the raindrop
+                  distribution to be advected. Valid options:
+                  0 -- rainfall all at terminal velocity 5 m/s.
+                  1 -- droplet size depends upon density. Advect the mean
+                  of the droplet size distribution.
     """
 
     def __init__(self, state):
-        super(Fallout, self).__init__(state)
+        super(Fallout, self).__init__(state, moments)
 
         self.state = state
+        self.moments = moments
         self.rain = state.fields('rain')
+        self.v = Function(state.fields('u').function_space())
 
         # function spaces
         Vt = self.rain.function_space()
@@ -130,15 +141,28 @@ class Fallout(Physics):
 
         # introduce sedimentation rate
         # for now assume all rain falls at terminal velocity
-        terminal_velocity = 10  # in m/s
-        self.v = state.fields("rainfall_velocity", Vu)
-        self.v.project(as_vector([0, -terminal_velocity]))
+        
+
+        if moments == 0:
+            # all rain falls at terminal velocity
+            terminal_velocity = Constant(5)  # in m/s
+            self.v.project(as_vector([0, -terminal_velocity]))
+        elif moments == 1:
+            rho = state.fields('rho')
+            v_expression = rho
+            raise NotImplementedError('sorry!')
+        else:
+            raise NotImplementedError('Currently we only have implementations for 0th and 1st moments of rainfall')
+
+        if moments > 1:
+            self.determine_v = Projector(as_vector([0, -v_expression]), self.v)
 
         # sedimentation will happen using a full advection method
         advection_equation = EmbeddedDGAdvection(state, Vt, equation_form="advective", outflow=True)
         self.advection_method = SSPRK3(state, self.rain, advection_equation)
 
     def apply(self):
-        for k in range(self.state.timestepping.maxk):
-            self.advection_method.update_ubar(self.v, self.v, 0)
-            self.advection_method.apply(self.rain, self.rain)
+        if self.moments > 0:
+            self.determine_v.project()
+        self.advection_method.update_ubar(self.v, self.v, 0)
+        self.advection_method.apply(self.rain, self.rain)
