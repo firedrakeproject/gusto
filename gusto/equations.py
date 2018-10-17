047fec91958fe3342380012c26847f10d547adaf
jshipton <jshipton@ic.ac.uk>
2018-10-17 14:09:15 +0100

    __call__ method for Label and start of Term class. Test that labelling a form returns a Term with the expected label and form property.

--- gusto/equations.py
+++ gusto/equations.py
@@ -1,5 +1,31 @@
 
+class Term(object):
+    """
+    Term class, containing a form and its labels.
+
+    :arg form: the form for this term
+    """
+
+    labels = {}
+
+    def __init__(self, form):
+        self.__setattr__("form", form)
+
+
 class Label(object):
+    """
+    Class provinding labelling functionality for Gusto terms and equations
+
+    :arg label: str giving the name of the label.
+    :arg value: str providing the value of the label. Defaults to True.
+    """
 
     def __init__(self, label, value=True):
+        self.label = label
         self.__setattr__(label, value)
+
+    def __call__(self, q):
+        if not isinstance(q, Term):
+            q = Term(q)
+        q.labels[self.label] = getattr(self, self.label)
+        return q
--- tests/test_labelling.py
+++ tests/test_labelling.py
@@ -1,8 +1,37 @@
+import pytest
+from firedrake import TestFunction, Function, FunctionSpace, UnitSquareMesh, dx
 from gusto import *
 
 
-def test_label():
+@pytest.fixture
+def mass():
     mass = Label("mass")
-    assert(mass.mass)
+    return mass
+
+
+@pytest.fixture
+def implicit():
     implicit = Label("time", "implicit")
+    return implicit
+
+
+@pytest.fixture
+def mass_form():
+    mesh = UnitSquareMesh(2, 2)
+    V = FunctionSpace(mesh, "CG", 1)
+    phi = TestFunction(V)
+    f = Function(V)
+    mass_form = phi*f*dx
+    return mass_form
+
+
+def test_label(mass, implicit):
+    assert(mass.mass)
     assert(implicit.time == "implicit")
+
+
+def test_label_form(mass, mass_form):
+    mass_term = mass(mass_form)
+    assert(isinstance(mass_term, Term))
+    assert(mass_term.labels["mass"])
+    assert(mass_term.form == mass_form)
