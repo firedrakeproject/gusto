588b96731db4a18648ccabcafa8344545266a697
David Ham <david.ham@imperial.ac.uk>
2018-10-18 14:57:47 +0100

    Draft of labelling functionality

--- gusto/equations.py
+++ gusto/equations.py
@@ -1,32 +1,72 @@
+import ufl
 
 class Term(object):
     """
     Term class, containing a form and its labels.
 
     :arg form: the form for this term
+    :arg label_dict: dictionary of key-value pairs corresponding to current form labels.
     """
+    __slots__ = ["form", "labels"]
 
-    def __init__(self, form):
-        self.__setattr__("form", form)
+    def __init__(self, form, label_dict=None):
+        self.form = form
+        self.labels = label_dict or {}
+
+    def has_label(self, *labels):
+        if len(labels) == 1:
+            return labels[0].label in self.labels
+        else:
+            return tuple(self.has_label(l) for l in labels)
 
     def __add__(self, other):
-        if isinstance(other, Term):
+        if other is None:
+            return self
+        elif isinstance(other, Term):
             return Equation(self, other)
         elif isinstance(other, Equation):
             return Equation(self, *other.terms)
+        else:
+            return NotImplemented
 
 
 class Equation(object):
+    __slots__ = ["terms"]
 
     def __init__(self, *terms):
-        self.terms = list(terms)
+        if len(terms) == 1 and isinstance (terms[0], Equation):
+            self.terms = terms[0].terms
+        else:
+            self.terms = list(terms)
 
     def __add__(self, other):
-        if isinstance(other, Equation):
-            self.terms.extend(other.terms)
-        else:
-            self.terms.append(other)
-        return self
+        if type(other) not in {Term, Equation}:
+            return NotImplemented
+        elif other is None:
+            return self
+        return Equation(*self, *other)
+
+    def __iter__(self):
+        return iter(self.terms)
+
+    def __len__(self):
+        return len(self.terms)
+
+    def __getitem__(self, key):
+        return self.terms[key]
+
+    def label_map(self, term_filter, map_function):
+        """Return a new equation in which terms for which
+        `term_filter` is `True` are transformed by
+        `map_function`. Terms for which `term_filter` is false are
+        included unaltered."""
+
+        return Equation(functools.reduce(lambda x, y: x + y,
+                                         (map_function(t) if term_filter(t) else t
+                                          for t in self.terms)))
+    @property
+    def form(self):
+        return functools.reduce(lambda x, y: x + y, (t.form for t in self.terms))
 
 
 class Label(object):
@@ -36,13 +76,33 @@ class Label(object):
     :arg label: str giving the name of the label.
     :arg value: str providing the value of the label. Defaults to True.
     """
+    __slots__ = ["label", "value"]
 
     def __init__(self, label, value=True):
         self.label = label
-        self.__setattr__(label, value)
+        self.value = value
 
-    def __call__(self, q):
-        if not isinstance(q, Term):
-            q = Term(q)
-        q.__setattr__(self.label, getattr(self, self.label))
-        return q
+    def __call__(self, target):
+        if isinstance(target, Equation):
+            return Equation(*(self(t) for t in target.terms))
+        elif isinstance(target, ufl.Form):
+            return Equation(Term(target, {self.label: self.value}))
+        elif isinstance(target, Term):
+            return Term(target.form, target.labels.copy().update({self.label: self.value}))
+        else:
+            raise ValueError("Unable to label %s" % target)
+
+    def remove(self, target):
+        """Remove any :form:`Label` with this `label` from
+        `target`. If called on an :class:`Equation`, act termwise."""
+        if isinstance(target, Equation):
+            return Equation(*(self.remove(t) for t in target.terms))
+        elif isinstance(target, Term):
+            try:
+                d = target.labels.copy()
+                d.pop(self.label)
+                return Term(target.form, d)
+            except KeyError:
+                return target
+        else:
+            raise ValueError("Unable to unlabel %s" % target)
--- tests/test_labelling.py
+++ tests/test_labelling.py
@@ -36,43 +36,37 @@ def eq(mass_term):
     eq = mass_term + mass_term
     return eq
 
-
-def test_label(mass, implicit):
-    assert(mass.mass)
-    assert(implicit.time == "implicit")
-
-
 def test_label_form(mass_term, mass_form):
-    assert(isinstance(mass_term, Term))
-    assert(mass_term.mass)
-    assert(mass_term.form == mass_form)
+    assert(isinstance(mass_term, Equation))
+    assert(mass_term[0].has_label(mass))
+    assert(mass_term[0].form == mass_form)
 
 
-def test_label_term(mass_term, implicit):
+def test_label_term(mass_term, implicit, mass):
     mass_term = implicit(mass_term)
-    assert(isinstance(mass_term, Term))
-    assert(mass_term.mass)
-    assert(mass_term.time == "implicit")
+    assert(isinstance(mass_term, Equation))
+    assert(mass_term[0].has_label(mass))
+    assert(mass_term[0].labels[time.label] == "implicit")
     explicit = Label("time", "explicit")
     mass_term = explicit(mass_term)
-    assert(mass_term.time == "explicit")
+    assert(mass_term[0].labels[time.label] == "explicit")
 
 
 def test_add_term(eq):
     assert(isinstance(eq, Equation))
-    assert(len(eq.terms) == 2)
+    assert(len(eq) == 2)
 
 
 def test_add_equation(eq):
     eq += eq
     assert(isinstance(eq, Equation))
-    assert(len(eq.terms) == 4)
+    assert(len(eq) == 4)
 
 
 def test_add_term_and_equation(mass_term, eq):
     eq += mass_term
     assert(isinstance(eq, Equation))
-    assert(len(eq.terms) == 3)
+    assert(len(eq) == 3)
     a = mass_term + eq
     assert(isinstance(a, Equation))
-    assert(len(a.terms) == 4)
+    assert(len(a) == 4)
