24fbe22378b9e27ef5f6844e6c4bbdddb9464283
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-10-24 16:43:14 +0100

    Clean up compressible pressure solver test

--- tests/test_vertical_hybridization.py
+++ tests/test_vertical_hybridization.py
@@ -2,89 +2,6 @@ from gusto import *
 from firedrake import *
 
 
-def run_incompressible_balance_test(dirname):
-    nlayers = 10  # horizontal layers
-    columns = 30  # number of columns
-    L = 1.e5
-    m = PeriodicRectangleMesh(columns, 1, L, 1.e4, quadrilateral=True)
-    dt = 6.0
-
-    # build volume mesh
-    H = 1.0e4  # Height position of the model top
-    mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
-
-    fieldlist = ['u', 'p', 'b']
-    timestepping = TimesteppingParameters(dt=dt)
-    output = OutputParameters(dirname=dirname+"/test_incompressible", dumplist=['u'], dumpfreq=5)
-    parameters = CompressibleParameters()
-
-    state = State(mesh, vertical_degree=1, horizontal_degree=1,
-                  family="RTCF",
-                  timestepping=timestepping,
-                  output=output,
-                  parameters=parameters,
-                  fieldlist=fieldlist)
-
-    b0 = state.fields("b")
-
-    # z.grad(bref) = N**2
-    x, y, z = SpatialCoordinate(mesh)
-    N = parameters.N
-    bref = z*(N**2)
-    b_b = Function(b0.function_space()).interpolate(bref)
-    b0.interpolate(b_b)
-
-    # get F
-    Vv = state.spaces("Vv")
-    v = TrialFunction(Vv)
-    w = TestFunction(Vv)
-    bcs = [DirichletBC(Vv, 0.0, "bottom")]
-
-    af = inner(w, v)*dx
-    Lf = inner(state.k, w)*b0*dx
-    F = Function(Vv)
-
-    solve(af == Lf, F, bcs=bcs)
-
-    # Define mixed function space
-    VDG = state.spaces("DG")
-    WV = Vv*VDG
-
-    # Set up mixed problem
-    v, pprime = TrialFunctions(WV)
-    w, phi = TestFunctions(WV)
-
-    bcs = [DirichletBC(WV[0], 0.0, "bottom")]
-
-    a = (
-        inner(w, v) + div(w)*pprime + div(v)*phi
-    )*dx
-    L = phi*div(F)*dx
-    w_hybrid = Function(WV)
-    w_ref = Function(WV)
-
-    hybrid_params = {
-        'ksp_type': 'preonly',
-        'pc_type': 'python',
-        'mat_type': 'matfree',
-        'pc_python_type': 'gusto.VerticalHybridizationPC',
-        'vert_hybridization': {'ksp_type': 'preonly',
-                               'pc_type': 'lu',
-                               'pc_factor_mat_solver_type': 'mumps'}
-    }
-    solve(a == L, w_hybrid, bcs=bcs, solver_parameters=hybrid_params)
-    _, pprime_hybrid = w_hybrid.split()
-
-    ref_params = {'ksp_type': 'preonly',
-                  'mat_type': 'aij',
-                  'pc_type': 'lu',
-                  'pc_factor_mat_solver_type': 'mumps'}
-    solve(a == L, w_ref, bcs=bcs, solver_parameters=ref_params)
-    _, pprime_ref = w_ref.split()
-
-    return errornorm(pprime_ref, pprime_hybrid, norm_type="L2")
-
-
 def run_compressible_balance_test(dirname):
     dt = 1.
     deltax = 400
@@ -162,22 +79,6 @@ def run_compressible_balance_test(dirname):
     _, pi_ref = w_ref.split()
 
     return errornorm(pi_ref, pi_hybrid, norm_type="L2")
-
-
-# def test_incompressible_balance_hybrid(tmpdir):
-#     """Solves the incompressible hydrostatic equation
-#     for a pressure variable using two solver configurations:
-
-#     (1): Hybridization with LU on the multiplier solve;
-#     (2): Direct LU on the full system.
-
-#     The error between the two fields should be small.
-#     """
-
-#     dirname = str(tmpdir)
-#     error = run_incompressible_balance_test(dirname)
-
-#     assert error < 1.0e-8
 
 
 def test_compressible_balance_hybrid(tmpdir):
