0a540990fc032ee666b498c702389f2ff2c27bbc
Thomas Gibson <t.gibson15@imperial.ac.uk>
2019-01-23 11:45:38 +0000

    Remove unused imports

--- gusto/linear_solvers.py
+++ gusto/linear_solvers.py
@@ -3,7 +3,7 @@ from firedrake import (split, LinearVariationalProblem, Constant,
                        TestFunction, TrialFunction, lhs, rhs, DirichletBC, FacetNormal,
                        div, dx, jump, avg, dS_v, dS_h, ds_v, ds_t, ds_b, inner, dot, grad,
                        Function, VectorSpaceBasis, BrokenElement, FunctionSpace, MixedFunctionSpace,
-                       assemble, LinearSolver, Tensor, AssembledVector)
+                       assemble, LinearSolver)
 from firedrake.petsc import flatten_parameters
 from firedrake.parloops import par_loop, READ, INC
 from pyop2.profiling import timed_function, timed_region
