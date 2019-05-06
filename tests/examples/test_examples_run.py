572f2a0ce5c3373fdd7c01bd4b9644b5b52c9916
Thomas Gibson <t.gibson15@imperial.ac.uk>
2019-05-06 12:59:37 -0400

    Unmark h mountain test

--- tests/examples/test_examples_run.py
+++ tests/examples/test_examples_run.py
@@ -9,8 +9,8 @@ cwd = abspath(dirname(__file__))
 examples_dir = join(cwd, "..", "..", "examples")
 example_files = glob.glob("%s/*.py" % examples_dir)
 
-@pytest.fixture(params=[pytest.param(f, marks=pytest.mark.xfail(reason="unreliable")) if f.endswith("h_mountain.py") else f for f in example_files],
-                ids=lambda x: basename(x))
+
+@pytest.fixture(params=[f for f in example_files], ids=lambda x: basename(x))
 def example_file(request):
     return abspath(request.param)
 
