4aec32a0e538bce4d583f71bdc74349710cfb1df
Tom Bendall <t.bendall15@imperial.ac.uk>
2019-05-22 16:48:02 +0100

    add the recovered diffusion method

--- gusto/diffusion.py
+++ gusto/diffusion.py
@@ -71,3 +71,37 @@ class InteriorPenalty(Diffusion):
         self.phi1.assign(x_in)
         self.solver.solve()
         x_out.assign(self.phi1)
+
+
+class RecoveredDiffusion(Diffusion):
+    """
+    A diffusion strategy for the lowest order spaces, allowing us to recover to a
+    higher order space, where diffusion will be performed.
+
+    :arg diffusion_scheme: an existing Diffusion class.
+    :arg recovered_options: a RecoveredOptions object.
+    """
+
+    def __init__(self, state, diffusion_scheme, V0, recovered_options):
+        super(RecoveredDiffusion, self).__init__(state)
+
+        self.diffusion = diffusion_scheme
+
+        self.x_in = Function(V0)
+        x_rec = Function(recovered_options.recovered_space)
+        self.x_dg = Function(recovered_options.embedding_space)
+        x_brok = Function(recovered_options.broken_space)
+
+        self.x_rec_projector = Recoverer(self.x_in, x_rec, VDG=recovered_options.embedding_space, boundary_method=recovered_options.boundary_method)
+        self.x_brok_projector = Projector(x_rec, x_brok)
+        self.xdg_interpolator = Interpolator(self.x_in + x_rec - x_brok, self.x_dg)
+        self.project_back = Projector(self.x_dg, self.x_in)
+
+    def apply(self, x_in, x_out):
+        self.x_in.assign(x_in)
+        self.x_rec_projector.project()
+        self.x_brok_projector.project()
+        self.xdg_interpolator.interpolate()
+        self.diffusion.apply(self.x_dg, self.x_dg)
+        self.project_back.project()
+        x_out.assign(self.x_projected)
