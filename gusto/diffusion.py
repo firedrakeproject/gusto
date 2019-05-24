b61ac89fba3453a345c93d13553374001fd9d26e
Tom Bendall <t.bendall15@imperial.ac.uk>
2019-05-24 11:47:36 +0100

    new vector recovered diffusion to apply different bcs to different components

--- gusto/diffusion.py
+++ gusto/diffusion.py
@@ -3,7 +3,7 @@ from firedrake import (TestFunction, TrialFunction, Function,
                        inner, outer, grad, avg, dx, dS_h, dS_v,
                        FacetNormal, LinearVariationalProblem,
                        LinearVariationalSolver, action, Interpolator,
-                       Projector)
+                       Projector, FunctionSpace, as_vector)
 from gusto.recovery import Recoverer
 
 
@@ -80,30 +80,48 @@ class RecoveredDiffusion(Diffusion):
     A diffusion strategy for the lowest order spaces, allowing us to recover to a
     higher order space, where diffusion will be performed.
 
-    :arg diffusion_scheme: an existing Diffusion class.
+    :arg diffusion_scheme: an existing Diffusion class. For a vector field this could be a list.
     :arg recovered_options: a RecoveredOptions object.
     """
 
-    def __init__(self, state, diffusion_scheme, V0, recovered_options):
+    def __init__(self, state, diffusion_scheme, V0, recovered_options, vector=False):
         super(RecoveredDiffusion, self).__init__(state)
 
+        DG1 = FunctionSpace(state.mesh, "DG", 1)
+
         self.diffusion = diffusion_scheme
+        self.vector = vector
+        dim = recovered_options.recovered_space.value_size
+
+        if self.vector and len(self.diffusion) != dim:
+            raise ValueError('If you want to use a vector method, you must supply a list of diffusion schemes.')
 
         self.x_in = Function(V0)
         x_rec = Function(recovered_options.recovered_space)
-        self.x_dg = Function(recovered_options.embedding_space)
         x_brok = Function(recovered_options.broken_space)
+
+        if self.vector:
+            self.x_dg_list = [Function(DG1) for i in range(dim)]
+            self.xdg_interpolator_list = [Interpolator(self.x_in[i] + x_rec[i] - x_brok[i], self.x_dg_list[i]) for i in range(dim)]
+            self.project_back = Projector(as_vector(self.x_dg_list), self.x_in)
+        else:
+            self.x_dg = Function(recovered_options.embedding_space)
+            self.xdg_interpolator = Interpolator(self.x_in + x_rec - x_brok, self.x_dg)
+            self.project_back = Projector(self.x_dg, self.x_in)
 
         self.x_rec_projector = Recoverer(self.x_in, x_rec, VDG=recovered_options.embedding_space, boundary_method=recovered_options.boundary_method)
         self.x_brok_projector = Projector(x_rec, x_brok)
-        self.xdg_interpolator = Interpolator(self.x_in + x_rec - x_brok, self.x_dg)
-        self.project_back = Projector(self.x_dg, self.x_in)
 
     def apply(self, x_in, x_out):
         self.x_in.assign(x_in)
         self.x_rec_projector.project()
         self.x_brok_projector.project()
-        self.xdg_interpolator.interpolate()
-        self.diffusion.apply(self.x_dg, self.x_dg)
+        if self.vector:
+            for i, (xdg_interpolator, diffusion_scheme) in enumerate(zip(self.xdg_interpolator_list, self.diffusion)):
+                xdg_interpolator.interpolate()
+                diffusion_scheme.apply(self.x_dg_list[i], self.x_dg_list[i])
+        else:
+            self.xdg_interpolator.interpolate()
+            self.diffusion.apply(self.x_dg, self.x_dg)
         self.project_back.project()
         x_out.assign(self.x_in)
