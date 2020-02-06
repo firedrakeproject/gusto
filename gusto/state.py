8692e0f771ad7df64644ab701c5d3b1ca2260410
Tom Bendall <thomas.bendall@metoffice.gov.uk>
2020-02-06 16:41:10 +0000

    update kernels for latlon coords to work with quads and in 3D

--- gusto/state.py
+++ gusto/state.py
@@ -571,22 +571,28 @@ class State(object):
 
 def get_latlon_mesh(mesh):
     coords_orig = mesh.coordinates
-    cell = mesh._base_mesh.ufl_cell().cellname()
-
-    DG1_hori_elt = FiniteElement("DG", cell, 1, variant="equispaced")
-    DG1_vert_elt = FiniteElement("DG", interval, 1, variant="equispaced")
-    DG1_elt = TensorProductElement(DG1_hori_elt, DG1_vert_elt)
+    
+    if coords_orig.function_space().extruded:
+        cell = mesh._base_mesh.ufl_cell().cellname()
+        DG1_hori_elt = FiniteElement("DG", cell, 1, variant="equispaced")
+        DG1_vert_elt = FiniteElement("DG", interval, 1, variant="equispaced")
+        DG1_elt = TensorProductElement(DG1_hori_elt, DG1_vert_elt)
+    else:
+        cell = mesh.ufl_cell().cellname()
+        DG1_elt = FiniteElement("DG", cell, 1, variant="equispaced")
 
     mesh_dg_fs = VectorFunctionSpace(mesh, DG1_elt)
     coords_dg = Function(mesh_dg_fs)
     coords_latlon = Function(mesh_dg_fs)
 
-    shapes = {"nDOFs": mesh_dg_fs.finat_element.space_dimension()}
+    shapes = {"nDOFs": mesh_dg_fs.finat_element.space_dimension(),
+              'dim': 3}
+    print(shapes['nDOFs'])
 
     par_loop("""
 for (int i=0; i<{nDOFs}; i++) {{
-    for (int j=0; j<3; j++) {{
-        dg[i*3 + j] = cg[i*3 + j];
+    for (int j=0; j<{dim}; j++) {{
+        dg[i*{dim} + j] = cg[i*{dim} + j];
     }}
 }}
 """.format(**shapes), dx, {'dg': (coords_dg, WRITE),
@@ -598,34 +604,38 @@ for (int i=0; i<{nDOFs}; i++) {{
     # lat-lon 'y' = asin(z/sqrt(x^2 + y^2 + z^2))
     coords_latlon.dat.data[:, 1] = np.arcsin(coords_dg.dat.data[:, 2]/np.sqrt(coords_dg.dat.data[:, 0]**2 + coords_dg.dat.data[:, 1]**2 + coords_dg.dat.data[:, 2]**2))
     coords_latlon.dat.data[:, 2] = np.sqrt(coords_dg.dat.data[:, 0]**2 + coords_dg.dat.data[:, 1]**2 + coords_dg.dat.data[:, 2]**2) - radius
+
+# We need to ensure that all points in a cell are on the same side of the branch cut in longitude coords
+# This kernel amends the longitude coords so that all longitudes in one cell are close together
     kernel = op2.Kernel("""
 #define PI 3.141592653589793
 #define TWO_PI 6.283185307179586
-void splat_coords(double *coords) {
-    double diff0 = (coords[0] - coords[3]);
-    double diff1 = (coords[0] - coords[6]);
-    double diff2 = (coords[3] - coords[6]);
+void splat_coords(double *coords) {{
+    double max_diff = 0.0;
+    double diff = 0.0;
+    
+    for (int i=0; i<{nDOFs}; i++) {{
+        for (int j=0; j<{nDOFs}; j++) {{
+            diff = coords[i*{dim}] - coords[j*{dim}];
+            if (fabs(diff) > max_diff) {{
+                max_diff = diff;
+            }}
+        }}
+    }}
 
-    if (fabs(diff0) > PI || fabs(diff1) > PI || fabs(diff2) > PI) {
-        const int sign0 = coords[0] < 0 ? -1 : 1;
-        const int sign1 = coords[3] < 0 ? -1 : 1;
-        const int sign2 = coords[6] < 0 ? -1 : 1;
-        if (sign0 < 0) {
-            coords[0] += TWO_PI;
-        }
-        if (sign1 < 0) {
-            coords[3] += TWO_PI;
-        }
-        if (sign2 < 0) {
-            coords[6] += TWO_PI;
-        }
-    }
-}
-""", "splat_coords")
+    if (max_diff > PI) {{
+        for (int i=0; i<{nDOFs}; i++) {{
+            if (coords[i*{dim}] < 0) {{
+                coords[i*{dim}] += TWO_PI;
+            }}
+        }}
+    }}
+}}
+""".format(**shapes), "splat_coords")
 
     op2.par_loop(kernel, coords_latlon.cell_set,
                  coords_latlon.dat(op2.RW, coords_latlon.cell_node_map()))
-    return Mesh(coords_latlon)
+    return Mesh(coords_dg)
 
 
 def topo_sort(field_deps):
