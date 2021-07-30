86f07df094e775085d0e4a89b07c33e6148eda16
jshipton <j.shipton@exeter.ac.uk>
2021-07-30 14:57:06 +0100

    slightly easier to read initial conditions

--- examples/forced_advection.py
+++ examples/forced_advection.py
@@ -16,7 +16,7 @@ mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
 
 timestepping = TimesteppingParameters(dt=15)
 dirname = 'forced_advection'
-output = OutputParameters(dirname=dirname)
+output = OutputParameters(dirname=dirname, dumpfreq=60)
 fieldlist = ["u", "rho", "theta"]
 
 diagnostic_fields = [CourantNumber()]
@@ -45,30 +45,24 @@ m1 = state.fields("m1", space=Vth)
 m2 = state.fields("m2", space=Vth)
 m3 = state.fields("m3", space=Vth)
 
-x, z = SpatialCoordinate(state.mesh)
-x = x/L # rescaled to domain
-z = z/H
-
 # set up constants and temperature and saturation profile
 Gamma = Constant(-6.5e-3)   # lapse rate
 H = Constant(15000)   # depth of mesh
 T0 = Constant(293)   # temperature at surface
 e1 = Constant(0.98) # level of saturation
 e2 = Constant(2/3) # dimensionless height above which m1 is zero
-T = Gamma * H * z + T0   # temperature profile 
+T = Gamma * z + T0   # temperature profile 
 ms = 3.8e-3 * exp((18 * T - 4824)/(T - 30))   # saturation profile
 
 # initialise m1 as in Zerroukat and Allen 2020
-m1.interpolate(conditional(1 > z/e2, 1 - z/e2, 0) * e1 * ms)
+m1.interpolate(conditional(z < e2*H, (1 - z/(e2*H)) * e1 * ms, 0))
 
 m1eqn = AdvectionEquation(state, Vth, equation_form="advective")
 m2eqn = AdvectionEquation(state, Vth, equation_form="advective")
-m3eqn = AdvectionEquation(state, Vth, equation_form="advective")
 
 advected_fields = []
 advected_fields.append(("m1", SSPRK3(state, m1, m1eqn)))
 advected_fields.append(("m2", SSPRK3(state, m2, m2eqn)))
-advected_fields.append(("m3", SSPRK3(state, m3, m3eqn)))
 
 class Moisture(Physics):
     def __init__(self, state, ms):
