5f5a74c1451a0329d4d9b55f82f42194c76b450e
nhartney <nh491@exeter.ac.uk>
2021-08-02 19:15:29 +0100

    set up W1 test case

---
+++ examples/sw_williamson1.py
@@ -0,0 +1,85 @@
+from gusto import *
+from firedrake import (IcosahedralSphereMesh, SpatialCoordinate,
+                       as_vector, FunctionSpace, cos, sin, acos, conditional,
+                       Constant)
+from math import pi
+
+# shallow water parameters
+a = 6371220.
+H = 5960.
+day = 24.*60.*60.
+
+# set up mesh
+mesh = IcosahedralSphereMesh(radius=a,
+                                 refinement_level=3, degree=3)
+x = SpatialCoordinate(mesh)
+global_normal = x
+mesh.init_cell_orientations(x)
+
+# spherical co-ordinates
+theta, lamda = latlon_coords(mesh)
+
+timestepping = TimesteppingParameters(dt=1)
+dirname = 'williamson1'
+fieldlist = ['u', 'D']
+parameters = ShallowWaterParameters(H=H)
+
+output = OutputParameters(dirname=dirname, steady_state_error_fields=['D', 'u'])
+
+diagnostic_fields = [RelativeVorticity(), PotentialVorticity(),
+                         ShallowWaterKineticEnergy(),
+                         ShallowWaterPotentialEnergy(),
+                         ShallowWaterPotentialEnstrophy()]
+
+state = State(mesh, horizontal_degree=1,
+              family="BDM",
+              timestepping=timestepping,
+              output=output,
+              parameters=parameters,
+              diagnostic_fields=diagnostic_fields,
+              fieldlist=fieldlist)
+
+# interpolate initial conditions
+u0 = state.fields("u")
+D0 = state.fields("D")
+u_max = 2*pi*a/(12*day)
+alpha = 0.
+h_max = 1000
+lamda_c = 3*pi/2
+theta_c = 0
+R = a/3
+r = a * acos(sin(theta_c)*sin(theta) + cos(theta_c)*cos(theta)*cos(lamda - lamda_c))
+uexpr = as_vector(
+    [u_max*(cos(theta)*cos(alpha) + sin(theta)*cos(lamda)*sin(alpha)),
+     -u_max*sin(lamda)*sin(alpha), 0.0])
+Dexpr = (h_max/2)*(1 + cos(pi*r/R))
+
+V = FunctionSpace(mesh, "CG", 1)
+f = state.fields("coriolis", V) # think it is looking for Coriolis
+f.interpolate(Constant(0))
+
+u0.project(uexpr)
+D0.interpolate(conditional(r < R, Dexpr, 0))
+state.initialise([('u', u0),
+                  ('D', D0)])
+
+
+### I took this part from W2 case code and I don't really know what's going on
+
+ueqn = VectorInvariant(state, u0.function_space())
+Deqn = AdvectionEquation(state, D0.function_space(), equation_form="continuity")
+
+advected_fields = []
+advected_fields.append(("u", ThetaMethod(state, u0, ueqn)))
+advected_fields.append(("D", SSPRK3(state, D0, Deqn, subcycles=2)))
+
+linear_solver = ShallowWaterSolver(state)
+
+# Set up forcing
+sw_forcing = ShallowWaterForcing(state)
+
+# build time stepper
+stepper = CrankNicolson(state, advected_fields, linear_solver,
+                            sw_forcing)
+
+stepper.run(t=0, tmax=10)
