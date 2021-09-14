d527ea56ca8b8413e7d058dd903a7e40c8733cfd
nhartney <nh491@exeter.ac.uk>
2021-09-14 12:00:01 +0100

    fix some lint mistakes

--- examples/deformational_advection.py
+++ examples/deformational_advection.py
@@ -1,8 +1,8 @@
+from math import pi
 from gusto import *
 from firedrake import (IcosahedralSphereMesh, SpatialCoordinate,
                        as_vector, FunctionSpace, cos, sin, acos, conditional,
                        Constant)
-from math import pi 
 
 nondivergent = True
 
@@ -42,13 +42,13 @@ u0 = state.fields("u")
 D0 = state.fields("D")
 
 # parameters for initial conditions from Lauritzen
-u_max = 2*pi*R/(12*day) 
+u_max = 2*pi*R/(12*day)
 alpha = 0.
 h_max = 1
 b = 0.1
 c = 0.9
 lamda_1 = 2*pi/2 - pi/6
-lamda_2 = 2*pi/2 + pi/6 
+lamda_2 = 2*pi/2 + pi/6
 theta_c = 0
 br = R/2
 r1 = R * acos(sin(theta_c)*sin(theta) + cos(theta_c)*cos(theta)*cos(lamda - lamda_1))
@@ -77,10 +77,10 @@ else:
         )
         u_merid = 5/2*R/T * sin(lamda_p) * (cos(theta))**3 * cos(pi*t/T)
         return sphere_to_cartesian(mesh, u_zonal, u_merid)
-    
+
 u0.project(uexpr(0))
 
-# set up advected variable in the same space as the height field 
+# set up advected variable in the same space as the height field
 VD = D0.function_space()
 m1 = state.fields("m1", space=VD)
 
