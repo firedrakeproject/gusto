e0140765235d5d3e39874503534f2b423f8c2c79
nhartney <nh491@exeter.ac.uk>
2021-09-20 10:17:53 +0100

    Change wind to blow over the poles rather than around the equator, and the temperature profile so that it varies with latitude. Initialise m1 like in w1, and add a temperature field.

--- examples/sw_williamson1_moisture.py
+++ examples/sw_williamson1_moisture.py
@@ -1,8 +1,8 @@
+from math import pi
 from gusto import *
 from firedrake import (IcosahedralSphereMesh, SpatialCoordinate,
-                       as_vector, FunctionSpace, cos, sin, acos, conditional,
+                       as_vector, cos, sin, acos, conditional,
                        Constant, exp, Function)
-from math import pi
 
 # parameters
 R = 6371220.
@@ -10,7 +10,7 @@ day = 24.*60.*60.
 
 # set up mesh
 mesh = IcosahedralSphereMesh(radius=R,
-                                 refinement_level=3, degree=3)
+                             refinement_level=3, degree=3)
 x = SpatialCoordinate(mesh)
 global_normal = x
 mesh.init_cell_orientations(x)
@@ -33,7 +33,7 @@ state = State(mesh, horizontal_degree=1,
 # interpolate initial conditions
 u0 = state.fields("u")
 D0 = state.fields("D")
-u_max = 2*pi*R/(12*day) # Maximum amplitude of the zonal wind (m/s) - they use a
+u_max = 2*pi*R/(12*day)  # Maximum amplitude of the zonal wind (m/s)
 alpha = pi/2
 h_max = 1000
 lamda_c = 3*pi/2
@@ -41,17 +41,21 @@ theta_c = 0
 a = R * 3
 r = a * acos(sin(theta_c)*sin(theta) + cos(theta_c)*cos(theta)*cos(lamda - lamda_c))
 
-uexpr = as_vector([-u_max*x[1]/R, u_max*x[0]/R, 0.0])
+# uexpr = as_vector([-u_max*x[1]/R, u_max*x[0]/R, 0.0])
+def uexpr():
+    u_zonal = u_max*(cos(theta)*cos(alpha) + sin(theta)*cos(lamda)*sin(alpha))
+    u_merid = -u_max*sin(lamda)*sin(alpha)
+    return sphere_to_cartesian(mesh, u_zonal, u_merid)
+
 #uexpr = as_vector(
-   # [u_max*(cos(theta)*cos(alpha) + sin(theta)*cos(lamda)*sin(alpha)),
-   #  -u_max*sin(lamda)*sin(alpha), 0.0])
+#    [u_max*(cos(theta)*cos(alpha) + sin(theta)*cos(lamda)*sin(alpha)),
+#     -u_max*sin(lamda)*sin(alpha), 0.0])
 
 m1expr = (h_max/2)*(1 + cos(pi*r/R))
 
+u0.project(uexpr())
 
-u0.project(uexpr)
-
-# set up advected variable in the same space as the height field 
+# set up advected variable in the same space as the height field
 VD = D0.function_space()
 m1 = state.fields("m1", space=VD)
 m2 = state.fields("m2", space=VD)
@@ -59,16 +63,21 @@ m2 = state.fields("m2", space=VD)
 # set up constants and temperature and saturation profile
 Gamma = Constant(-6.5e-3)   # lapse rate
 T0 = Constant(293)   # temperature at equator
-T = Gamma * lamda + T0   # temperature profile 
+T = Gamma * abs(theta - pi/2) + T0   # temperature profile
 H = pi
-e1 = Constant(0.98) # level of saturation
-e2 = Constant(2/3) # dimensionless longitude beyond which m1 is zero
+e1 = Constant(0.98)  # level of saturation
+e2 = Constant(2/3)  # dimensionless latitude beyond which m1 is zero
 ms = 3.8e-3 * exp((18 * T - 4824)/(T - 30))   # saturation profile
 
+# set up a temperature field for viewing the temperature profile
+temp_field = state.fields("temp", space=VD)
+temp_field.interpolate(T)
+
 # initialise m1 as the height field in W1
-#m1.interpolate(conditional(r < R, m1expr, 0))
+m1.interpolate(conditional(r < R, m1expr, 0))
 # initialise m1 as in Zerroukat and Allen 2020
-m1.interpolate(conditional(lamda < e2*H, (1 - lamda/(e2*H)) * e1 * ms, 0))
+# m1.interpolate(conditional(lamda < e2*H, (1 - lamda/(e2*H)) * e1 * ms, 0))
+
 
 m1eqn = AdvectionEquation(state, VD, equation_form="advective")
 m2eqn = AdvectionEquation(state, VD, equation_form="advective")
@@ -76,6 +85,7 @@ m2eqn = AdvectionEquation(state, VD, equation_form="advective")
 advected_fields = []
 advected_fields.append(("m1", SSPRK3(state, m1, m1eqn)))
 advected_fields.append(("m2", SSPRK3(state, m2, m1eqn)))
+
 
 class Moisture(Physics):
     def __init__(self, state, ms):
@@ -90,7 +100,6 @@ class Moisture(Physics):
         m1 = state.fields("m1")
         m2 = state.fields("m2")
         gamma1 = Constant(0.9)
-        gamma2 = Constant(0.5)
         dt = state.timestepping.dt
         self.dm1.interpolate(conditional(m1 - ms > 0, gamma1 * (m1 - ms), 0))
         self.dm2.interpolate(
@@ -98,9 +107,9 @@ class Moisture(Physics):
                         conditional(ms - m1 < m2, gamma1 * (ms - m1), m2),
                         0))
         m1 += self.dm2 - self.dm1
-        m2 += self.dm1 - self.dm2 
+        m2 += self.dm1 - self.dm2
 
-        
+
 moisture = Moisture(state, ms)
 
 # build time stepper
