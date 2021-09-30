4693dc8996fee99518d1b2142f63d7319d90cc73
nhartney <nh491@exeter.ac.uk>
2021-09-30 12:20:07 +0100

    Add checks for which saturation field and initial condition to use.

--- examples/sw_williamson1_moisture.py
+++ examples/sw_williamson1_moisture.py
@@ -3,6 +3,8 @@ from gusto import *
 from firedrake import (IcosahedralSphereMesh, SpatialCoordinate,
                        as_vector, cos, sin, acos, conditional,
                        Constant, exp, Function, File)
+SSHS = True
+lat_only = False
 
 # parameters
 R = 6371220.
@@ -38,20 +40,25 @@ u0 = state.fields("u")
 D0 = state.fields("D")
 u_max = 2*pi*R/(12*day)  # Maximum amplitude of the zonal wind (m/s)
 alpha = pi/2
-h_max = 0.032
 lamda_c = 3*pi/2
 theta_c = 0
 a = R * 3
 r = a * acos(sin(theta_c)*sin(theta) + cos(theta_c)*cos(theta)*cos(lamda - lamda_c))
+# initial height depends on the choice of saturation field
+if SSHS:
+    if lat_only:
+        h_max = 1
+    else:
+        h_max = 0.087
+else:
+    h_max = 0.032
 
-# uexpr = as_vector([-u_max*x[1]/R, u_max*x[0]/R, 0.0])
 def uexpr():
     u_zonal = u_max*(cos(theta)*cos(alpha) + sin(theta)*cos(lamda)*sin(alpha))
     u_merid = -u_max*sin(lamda)*sin(alpha)
     return sphere_to_cartesian(mesh, u_zonal, u_merid)
 
-
-m1expr = (h_max/2)*(1 + cos(pi*r/R))
+mexpr = (h_max/2)*(1 + cos(pi*r/R))
 
 u0.project(uexpr())
 
@@ -60,32 +67,47 @@ VD = D0.function_space()
 m1 = state.fields("m1", space=VD)
 m2 = state.fields("m2", space=VD)
 
-# set up constants and temperature and saturation profile
-Gamma = Constant(-37.5604) # lapse rate - this could be lat-dependent
-T0 = Constant(300)   # temperature at equator
-T = Gamma * abs(theta) + T0   # temperature profile
-e1 = Constant(0.98)  # level of saturation
-ms = 3.8e-3 * exp((18 * T - 4824)/(T - 30))   # saturation profile
+# choose saturation profile: SSHS 2021 paper OR varying in latitude
+if SSHS:
+    if lat_only:
+        alpha_0 = 0
+    else:
+        alpha_0 = 1
+    ms = exp(-(theta**2/(pi/3)**2) - alpha_0 * (lamda - pi)**2/(2*pi/3)**2)
+
+else: # varying in latitude
+    Gamma = Constant(-37.5604) # lapse rate - this could be lat-dependent
+    T0 = Constant(300)   # temperature at equator
+    T = Gamma * abs(theta) + T0   # temperature profile
+    e1 = Constant(0.98)  # level of saturation
+    ms = 3.8e-3 * exp((18 * T - 4824)/(T - 30))   # saturation profile
+    # set up a temperature field for viewing the temperature profile
+    temp_field = state.fields("temp", space=VD)
+    temp_field.interpolate(T)
+
+# write moisture out to a file
 msout = Function(VD).interpolate(ms)
 outfile = File('ms.pvd')
 outfile.write(msout)
 
-# set up a temperature field for viewing the temperature profile
-temp_field = state.fields("temp", space=VD)
-temp_field.interpolate(T)
-
-# set up a field to visualise latitude
+# set up fields to visualise latitude and longitude
 lat_field = state.fields("lat", space=VD)
 lat_field.interpolate(theta)
+lon_field = state.fields("lon", space=VD)
+lon_field.interpolate(lamda)
 
 mtot = state.fields("mtot", space=VD)
 
-# initialise m1 as the height field in W1
-m1.interpolate(conditional(r < R, m1expr, 0))
+# initialise m1 or m2 as the height field in W1
+if SSHS:
+    m2.interpolate(conditional(r < R, mexpr, 0))
+else:
+    m1.interpolate(conditional(r < R, mexpr, 0))
+
 # initialise m1 as in Zerroukat and Allen 2020
 # m1.interpolate(conditional(1 > theta/e2, (1 - theta/e2) * e1 * ms, 0))
-mtot.interpolate(m1 + m2)
 
+mtot.interpolate(m1 + m2)
 
 m1eqn = AdvectionEquation(state, VD, equation_form="advective")
 m2eqn = AdvectionEquation(state, VD, equation_form="advective")
@@ -107,7 +129,7 @@ class Moisture(Physics):
         ms = self.ms
         m1 = state.fields("m1")
         m2 = state.fields("m2")
-        gamma1 = Constant(1.0)
+        gamma1 = Constant(0.9)
         dt = state.timestepping.dt
         self.dm1.interpolate(conditional(m1 - ms > 0, gamma1 * (m1 - ms), 0))
         self.dm2.interpolate(
