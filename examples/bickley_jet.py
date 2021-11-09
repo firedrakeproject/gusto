37ae3aa37f67fe44174903aac7567fa4f437e65a
nhartney <nh491@exeter.ac.uk>
2021-11-09 10:18:11 +0000

    initialise with the same scales as given in the Zeitlin book

--- examples/bickley_jet.py
+++ examples/bickley_jet.py
@@ -1,26 +1,29 @@
 from gusto import *
-from firedrake import PeriodicRectangleMesh
-from math import pi
+from firedrake import PeriodicRectangleMesh, UnitSquareMesh
+from math import pi, sin, cos
+from mpmath import sech
 
 # set up mesh
-Lx = 3000e3
-Ly = 1000e3
-delta_x = 5e3
+Lx = 3000
+Ly = 1000
+delta_x = 5
 nx = int(Lx/delta_x)
 
 mesh = PeriodicRectangleMesh(nx, nx, Lx, Ly, direction='x')
 
 # set up parameters
-H = 5960.
-angle = 56 * pi/180
+H = 5960. # making this choice
+angle = 60 * pi/180 # making this choice
 parameters = ShallowWaterParameters(H=H)
 g = parameters.g
 Omega = parameters.Omega
 f = 2 * Omega * sin(angle)
+L = 6
+dh = 59.6
 dt = 10.
 
 dirname="bickley_jet"
-x = SpatialCoordinate(mesh)
+x, y = SpatialCoordinate(mesh)
 
 output = OutputParameters(dirname=dirname)
 
@@ -30,3 +33,7 @@ eqns = ShallowWaterEquations(state, "BDM", 1)
 
 u0 = state.fields("u")
 D0 = state.fields("D")
+
+uexpr = Constant(0)
+vexpr = (-g*dh/f*L) * sech(x/L)**2
+#test = -0.5*pi*pi*(4*cos(pi*x) - 5*cos(pi*x*0.5) + 2)*sin(pi*y)
