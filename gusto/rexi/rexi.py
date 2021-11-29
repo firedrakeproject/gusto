22f795a3165b622d1ed53f806d0c7feaf85190e6
jshipton <j.shipton@exeter.ac.uk>
2021-11-29 09:30:01 +0000

    start to integrate rexi with gusto

---
+++ gusto/rexi/rexi.py
@@ -0,0 +1,140 @@
+from rexi import *
+from firedrake import Function, TrialFunctions, Constant, \
+    LinearVariationalProblem, LinearVariationalSolver
+
+
+class Rexi(object):
+    """
+    Class defining the solver for the system
+
+    (A_n + tau L)V_n = U
+
+    required for computing the matrix exponential as described in notes.pdf
+
+    :arg equation: :class:`.Equation` object defining the equation set to
+    be solved
+    :arg rexi_parameters: :class:`.Equation` object
+    :arg solver_parameters: dictionary of solver parameters. Default None,
+    which results in the default solver parameters defined in the equation
+    class being used.
+    :arg manager: :class:`.Ensemble` object containing the space and ensemble
+    subcommunicators
+
+    """
+    def __init__(self, equation, rexi_parameters, *, solver_parameters=None,
+                 manager=None):
+
+        residual = equation.residual
+
+        # Get the Rexi Coefficients, given the values of h and M in
+        # rexi_parameters
+        self.alpha, self.beta, self.beta2 = RexiCoefficients(rexi_parameters)
+
+        self.manager = manager
+
+        # define the start point of the solver loop (idx) and the
+        # number of solvers (N) for this process depending on the
+        # total number of solvers (nsolvers) and how many ensemble
+        # processes (neprocs) there are.
+        nsolvers = len(self.alpha)
+        if manager is None:
+            # if running in serial we loop over all the solvers, from
+            # 0: nsolvers
+            self.N = nsolvers
+            self.idx = 0
+        else:
+            rank = manager.ensemble_comm.rank
+            neprocs = manager.ensemble_comm.size
+            m = int(nsolvers/neprocs)
+            p = nsolvers - m*neprocs
+            if rank < p:
+                self.N = m+1
+                self.idx = rank*(m+1)
+            else:
+                self.N = m
+                self.idx = rank*m + p
+
+        # set dummy constants for tau and A_i
+        self.ar = Constant(1.)
+        self.ai = Constant(1.)
+        self.tau = Constant(1.)
+
+        # set up functions, problem and solver
+        W_ = equation.function_space
+        W = MixedFunctionSpace(W_[0], W_[0], W_[1], W[1])
+        self.U0 = Function(W)
+        self.w_sum = Function(W)
+        self.w = Function(W)
+        self.w_ = Function(W)
+        tests = TestFunctions(W)
+        trials = TrialFunctions(W)
+        tests_r = tests[::2]
+        tests_i = tests[1::2]
+        trials_r = trials[::2]
+        trials_i = trials[1::2]
+
+        mass_form = residual.label_map(
+            lambda t: t.has_label(time_derivative),
+            map_if_true=replace_subject(trial),
+            map_if_false=drop)
+        
+        a = (
+            self.ar*equation.mass_term(trial, i=0, j=0)
+            + self.tau*equation.L(trial, 0)
+            - self.ai*equation.mass_term(trial, i=0, j=1)
+            + self.ar*equation.mass_term(trial, i=1, j=1)
+            + self.tau*equation.L(trial, 1)
+            + self.ai*equation.mass_term(trial, i=1, j=0)
+        )
+        L = equation.mass_term(self.U0)
+
+        if hasattr(equation, "aP"):
+            aP = equation.aP(trial, self.ai, self.tau)
+        else:
+            aP = None
+
+        rexi_prob = LinearVariationalProblem(a, L, self.w, aP=aP,
+                                             constant_jacobian=False)
+
+        if solver_parameters is None:
+            solver_parameters = equation.solver_parameters
+
+        self.solver = LinearVariationalSolver(
+            rexi_prob, solver_parameters=solver_parameters)
+
+    def solve(self, U0, dt):
+        """
+        Solve method for approximating the matrix exponential by a
+        rational sum. Solves
+
+        (A_n + tau L)V_n = U
+
+        multiplies by the corresponding B_n and sums over n.
+
+        :arg U0: the mixed function on the rhs.
+        :arg dt: the value of tau
+
+        """
+
+        # assign tau and U0 and initialise solution to 0.
+        self.tau.assign(dt)
+        self.U0.assign(U0)
+        self.w_.assign(0.)
+
+        # loop over solvers, assigning a_i, solving and accumulating the sum
+        for i in range(self.N):
+            j = self.idx + i
+            self.ar.assign(self.alpha[j].real)
+            self.ai.assign(self.alpha[j].imag)
+            self.solver.solve()
+            self.w_ += (
+                + Constant(self.beta[j].real)*self.w
+            )
+
+        # in parallel we have to accumulate the sum over all processes
+        if self.manager is not None:
+            self.manager.allreduce(self.w_, self.w_sum)
+        else:
+            self.w_sum.assign(self.w_)
+
+        return self.w_sum
