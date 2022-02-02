efa034c84eb9dddc330e6cb99ec1e23119195e6f
Matthew Griffith <mjg41@bath.ac.uk>
2022-02-02 11:26:12 +0000

    New shallow water example using cubedsphere to be modified to use different solver configurations.

---
+++ examples/sw_williamson2_cubedsphere.py
@@ -0,0 +1,88 @@
+from gusto import *
+from firedrake import (CubedSphereMesh, SpatialCoordinate,
+                       as_vector, FunctionSpace)
+from math import pi
+import sys
+
+day = 24.*60.*60.
+if '--running-tests' in sys.argv:
+    ref_dt = {3: 3000.}
+    tmax = 3000.
+else:
+    # setup resolution and timestepping parameters for convergence test
+    ref_dt = {3: 4000., 4: 2000., 5: 1000., 6: 500.}
+    tmax = 5*day
+
+# setup shallow water parameters
+R = 6371220.
+H = 5960.
+
+# setup input that doesn't change with ref level or dt
+fieldlist = ['u', 'D']
+parameters = ShallowWaterParameters(H=H)
+
+for ref_level, dt in ref_dt.items():
+
+    dirname = "sw_W2_ref%s_dt%s" % (ref_level, dt)
+    mesh = CubedSphereMesh(radius=R,
+                                 refinement_level=ref_level, degree=3)
+    x = SpatialCoordinate(mesh)
+    global_normal = x
+    mesh.init_cell_orientations(x)
+
+    timestepping = TimesteppingParameters(dt=dt)
+
+    output = OutputParameters(dirname=dirname,
+                              dumplist_latlon=['D', 'D_error'],
+                              steady_state_error_fields=['D', 'u'],
+                              log_level='INFO')
+
+    diagnostic_fields = [RelativeVorticity(), PotentialVorticity(),
+                         ShallowWaterKineticEnergy(),
+                         ShallowWaterPotentialEnergy(),
+                         ShallowWaterPotentialEnstrophy()]
+
+    state = State(mesh, horizontal_degree=1,
+                  family="RTCF",
+                  timestepping=timestepping,
+                  output=output,
+                  parameters=parameters,
+                  diagnostic_fields=diagnostic_fields,
+                  fieldlist=fieldlist)
+
+    # interpolate initial conditions
+    u0 = state.fields("u")
+    D0 = state.fields("D")
+    x = SpatialCoordinate(mesh)
+    u_max = 2*pi*R/(12*day)  # Maximum amplitude of the zonal wind (m/s)
+    uexpr = as_vector([-u_max*x[1]/R, u_max*x[0]/R, 0.0])
+    Omega = parameters.Omega
+    g = parameters.g
+    Dexpr = H - ((R * Omega * u_max + u_max*u_max/2.0)*(x[2]*x[2]/(R*R)))/g
+    # Coriolis expression
+    fexpr = 2*Omega*x[2]/R
+    V = FunctionSpace(mesh, "CG", 1)
+    f = state.fields("coriolis", V)
+    f.interpolate(fexpr)  # Coriolis frequency (1/s)
+
+    u0.project(uexpr)
+    D0.interpolate(Dexpr)
+    state.initialise([('u', u0),
+                      ('D', D0)])
+
+    ueqn = VectorInvariant(state, u0.function_space())
+    Deqn = AdvectionEquation(state, D0.function_space(), equation_form="continuity")
+    advected_fields = []
+    advected_fields.append(("u", ThetaMethod(state, u0, ueqn)))
+    advected_fields.append(("D", SSPRK3(state, D0, Deqn, subcycles=2)))
+
+    linear_solver = ShallowWaterSolver(state)
+
+    # Set up forcing
+    sw_forcing = ShallowWaterForcing(state)
+
+    # build time stepper
+    stepper = CrankNicolson(state, advected_fields, linear_solver,
+                            sw_forcing)
+
+    stepper.run(t=0, tmax=tmax)
