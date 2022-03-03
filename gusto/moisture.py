33799fdf33a30cdbc258a4922145f5455b93790e
nhartney <nh491@exeter.ac.uk>
2022-03-03 14:08:33 +0000

    file to define condensation and evaporation functions

---
+++ gusto/moisture.py
@@ -0,0 +1,18 @@
+from firedrake import exp, dx, conditional
+
+
+def condensation(test, Q, D, parameters):
+    alpha = parameters.alpha
+    q_0 = parameters.q_0
+    H = parameters.H
+    tau = parameters.tau
+    q_s = q_0 * exp(-alpha*D/H)
+    expr = conditional(Q > q_s, (Q - q_s) * (Q - q_s)/tau, 0)
+    return test * expr * dx
+
+
+def evaporation(test, Q, D, parameters):
+    q_g = parameters.q_g
+    tau_e = parameters.tau_e
+    expr = conditional(q_g > Q, (q_g - Q) * (q_g - Q)/tau_e, 0)
+    return test * expr * dx
