afb543d8c3846e04b95b6479406752713178da02
jshipton <j.shipton@exeter.ac.uk>
2022-03-28 10:43:49 +0100

    remove local definition of compressible Euler equations

--- examples/sk_sdc.py
+++ examples/sk_sdc.py
@@ -7,65 +7,8 @@ from firedrake import (as_vector, SpatialCoordinate, PeriodicIntervalMesh,
 import numpy as np
 import sys
 
-class MyCompressibleEulerEquations(PrognosticEquation):
-
-    field_names = ['u', 'rho', 'theta']
-
-    def __init__(self, state):
-
-        spaces = state.spaces.build_compatible_spaces("CG", 1)
-        W = MixedFunctionSpace(spaces)
-
-        field_name = "_".join(self.field_names)
-        super().__init__(state, W, field_name)
-
-        Vu = W.sub(0)
-        self.bcs['u'].append(DirichletBC(Vu, 0.0, "bottom"))
-        self.bcs['u'].append(DirichletBC(Vu, 0.0, "top"))
-
-        g = state.parameters.g
-        cp = state.parameters.cp
-
-        w, phi, gamma = TestFunctions(W)
-        X = Function(W)
-        u, rho, theta = X.split()
-        rhobar = state.fields("rhobar", space=rho.function_space(), dump=False)
-        thetabar = state.fields("thetabar", space=theta.function_space(), dump=False)
-        pi = Pi(state.parameters, rho, theta)
-        n = FacetNormal(state.mesh)
-
-        u_mass = subject(prognostic(inner(u, w)*dx, "u"), X)
-
-        rho_mass = subject(prognostic(inner(rho, phi)*dx, "rho"), X)
-
-        theta_mass = subject(prognostic(inner(theta, gamma)*dx, "theta"), X)
-
-        mass_form = time_derivative(u_mass + rho_mass + theta_mass)
-
-        u_adv = prognostic(vector_invariant_form(state, w, u), "u")
-
-        rho_adv = prognostic(continuity_form(state, phi, rho), "rho")
-
-        theta_adv = prognostic(advection_form(state, gamma, theta, ibp=IntegrateByParts.TWICE), "theta")
-
-        adv_form = subject(u_adv + rho_adv + theta_adv, X)
-
-        pressure_gradient_form = name(subject(prognostic(
-            cp*(-div(theta*w)*pi*dx
-                + jump(theta*w, n)*avg(pi)*dS_v), "u"), X), "pressure_gradient")
-
-        gravity_form = subject(prognostic(Term(g*inner(state.k, w)*dx), "u"), X)
-
-        self.residual = (mass_form + adv_form
-                         + pressure_gradient_form + gravity_form)
-
-
 dt = 2.
-if '--running-tests' in sys.argv:
-    tmax = dt
-else:
-    tmax = 3600.
-
+tmax = 3600.
 
 nlayers = 10  # horizontal layers
 columns = 150  # number of columns
