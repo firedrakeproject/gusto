78f32ae02e9d88c95d94acf93f26ce51d03dd496
nh491 <nh491@pileus.ex.ac.uk>
2022-04-04 14:41:39 +0100

    correct height variable in the saturation function

--- gusto/moisture.py
+++ gusto/moisture.py
@@ -6,12 +6,12 @@ def condensation(test, Q, D, parameters):
     q_0 = parameters.q_0
     H = parameters.H
     tau = parameters.tau
-    q_s = q_0 * exp(-alpha*D/H)
+    q_s = 1 * exp(-alpha*(D-H)/H)
     expr = conditional(Q > q_s, (Q - q_s) * (Q - q_s)/tau, 0)
     return test * expr * dx
 
 
-def evaporation(test, Q, D, parameters):
+def evaporation(test, Q, parameters):
     q_g = parameters.q_g
     tau_e = parameters.tau_e
     expr = conditional(q_g > Q, (q_g - Q) * (q_g - Q)/tau_e, 0)
