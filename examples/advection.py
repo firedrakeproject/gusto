b87a2a5f1cee0447960b8f14aef4efa414c58dc3
nh491 <nh491@pileus.ex.ac.uk>
2022-04-13 16:56:24 +0100

    add tests for RK4 and Heun's method to test_dg_advection, and an advection equation example

---
+++ examples/advection.py
@@ -0,0 +1,33 @@
+from firedrake import (IcosahedralSphereMesh, PeriodicIntervalMesh,
+                       ExtrudedMesh, SpatialCoordinate, as_vector,
+                       sin, exp, pi)
+
+from gusto import *
+
+mesh = IcosahedralSphereMesh(radius=1,
+                             refinement_level=3,
+                             degree=1)
+x = SpatialCoordinate(mesh)
+mesh.init_cell_orientations(x)
+
+dirname="advection_Heun"
+
+dt = pi/3. * 0.02
+output = OutputParameters(dirname=dirname, dumpfreq=1)
+diagnostic_fields = [CourantNumber()]
+state = State(mesh, dt=dt, output=output, diagnostic_fields=diagnostic_fields)
+
+uexpr = as_vector([-x[1], x[0], 0.0])
+
+tmax = pi/2
+f_init = exp(-x[2]**2 - x[0]**2)
+
+V = state.spaces("DG", "DG", 1)
+eqn = AdvectionEquation(state, V, "f", "BDM", 1)
+
+state.fields("f").interpolate(f_init)
+state.fields("u").project(uexpr)
+
+advection_scheme = [(eqn, Heun(state))]
+timestepper = PrescribedAdvection(state, advection_scheme)
+timestepper.run(0, tmax)
--- tests/test_dg_advection.py
+++ tests/test_dg_advection.py
@@ -11,7 +11,7 @@ def run(state, advection_scheme, tmax, f_end):
 
 @pytest.mark.parametrize("geometry", ["slice", "sphere"])
 @pytest.mark.parametrize("equation_form", ["advective", "continuity"])
-@pytest.mark.parametrize("scheme", ["ssprk", "implicit_midpoint"])
+@pytest.mark.parametrize("scheme", ["ssprk", "implicit_midpoint", "RK4", "Heun"])
 def test_dg_advection_scalar(tmpdir, geometry, equation_form, scheme,
                              tracer_setup):
     setup = tracer_setup(tmpdir, geometry)
@@ -30,12 +30,16 @@ def test_dg_advection_scalar(tmpdir, geometry, equation_form, scheme,
         advection_scheme = [(eqn, SSPRK3(state))]
     elif scheme == "implicit_midpoint":
         advection_scheme = [(eqn, ImplicitMidpoint(state))]
+    elif scheme == "RK4":
+         advection_scheme = [(eqn, RK4(state))]
+    elif scheme == "Heun":
+         advection_scheme = [(eqn, Heun(state))]
     assert run(state, advection_scheme, setup.tmax, setup.f_end) < setup.tol
 
 
 @pytest.mark.parametrize("geometry", ["slice", "sphere"])
 @pytest.mark.parametrize("equation_form", ["advective", "continuity"])
-@pytest.mark.parametrize("scheme", ["ssprk", "implicit_midpoint"])
+@pytest.mark.parametrize("scheme", ["ssprk", "implicit_midpoint", "RK4", "Heun"])
 def test_dg_advection_vector(tmpdir, geometry, equation_form, scheme,
                              tracer_setup):
     setup = tracer_setup(tmpdir, geometry)
@@ -55,5 +59,9 @@ def test_dg_advection_vector(tmpdir, geometry, equation_form, scheme,
         advection_schemes = [(eqn, SSPRK3(state))]
     elif scheme == "implicit_midpoint":
         advection_schemes = [(eqn, ImplicitMidpoint(state))]
+    elif scheme == "RK4":
+         advection_scheme = [(eqn, RK4(state))]
+    elif scheme == "Heun":
+         advection_scheme = [(eqn, Heun(state))]
     f_end = as_vector((setup.f_end, *[0.]*(gdim-1)))
     assert run(state, advection_schemes, setup.tmax, f_end) < setup.tol
