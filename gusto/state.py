213d22435de14182628587caf139033ba11f288a
Tom Bendall <t.bendall15@imperial.ac.uk>
2022-07-30 16:48:30 +0100

    update checkpointing to use CheckpointFile instead of DumbCheckpoint

--- gusto/state.py
+++ gusto/state.py
@@ -8,7 +8,7 @@ from firedrake import (FiniteElement, TensorProductElement, HDiv,
                        FunctionSpace, VectorFunctionSpace,
                        interval, Function, Mesh, functionspaceimpl,
                        File, SpatialCoordinate, sqrt, Constant, inner,
-                       op2, DumbCheckpoint, FILE_CREATE, FILE_READ, interpolate,
+                       op2, CheckpointFile, FILE_CREATE, FILE_READ, interpolate,
                        CellNormal, cross, as_vector)
 import numpy as np
 from gusto.configuration import logger, set_log_handler
@@ -483,10 +483,9 @@ class State(object):
                 self.output.pddumpfreq = self.output.dumpfreq
 
         # if we want to checkpoint and are not picking up from a previous
-        # checkpoint file, setup the dumb checkpointing
+        # checkpoint file, setup the checkpointing
         if self.output.checkpoint and not pickup:
-            self.chkpt = DumbCheckpoint(path.join(self.dumpdir, "chkpt"),
-                                        mode=FILE_CREATE)
+            self.chkpt = CheckpointFile(path.join(self.dumpdir, "chkpt"), 'w')
             # make list of fields to pickup (this doesn't include
             # diagnostic fields)
             self.to_pickup = [f for f in self.fields if f.name() in self.fields.to_pickup]
@@ -505,14 +504,14 @@ class State(object):
         if self.output.checkpoint:
             # Open the checkpointing file for writing
             chkfile = path.join(self.dumpdir, "chkpt")
-            with DumbCheckpoint(chkfile, mode=FILE_READ) as chk:
+            with CheckpointFile(chkfile, 'r') as chk:
                 # Recover all the fields from the checkpoint
                 for field in self.to_pickup:
-                    chk.load(field)
-                t = chk.read_attribute("/", "time")
+                    chk.load_function(field)
+                t = chk.get_attr("/", "time")
                 next(self.dumpcount)
             # Setup new checkpoint
-            self.chkpt = DumbCheckpoint(path.join(self.dumpdir, "chkpt"), mode=FILE_CREATE)
+            self.chkpt = CheckpointFile(path.join(self.dumpdir, "chkpt"), 'w')
         else:
             raise ValueError("Must set checkpoint True if pickup")
 
@@ -540,8 +539,8 @@ class State(object):
         # Dump all the fields to the checkpointing file (backup version)
         if output.checkpoint and (next(self.chkptcount) % output.chkptfreq) == 0:
             for field in self.to_pickup:
-                self.chkpt.store(field)
-            self.chkpt.write_attribute("/", "time", t)
+                self.chkpt.save_function(field)
+            self.chkpt.set_attr("/", "time", t)
 
         if output.dump_vtus and (next(self.dumpcount) % output.dumpfreq) == 0:
             # dump fields
