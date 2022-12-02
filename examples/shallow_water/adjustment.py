ea523505bcb20ea978bbed10702a45ef64f7a69b
nhartney <nh491@exeter.ac.uk>
2022-12-02 15:14:15 +0000

    change parameters for consistent scaling and add no normal flow boundary conditions in y

--- examples/shallow_water/adjustment.py
+++ examples/shallow_water/adjustment.py
@@ -5,27 +5,27 @@ from firedrake import PeriodicRectangleMesh, exp, Constant
 # Lx = 200
 # Ly = 20
 # nx = ny = 1000
-Lx = 200
+Lx = 20
 Ly = 20
-delta = 1
+delta = 0.2
 nx = int(Lx/delta)
 ny = int(Ly/delta)
 
-mesh = PeriodicRectangleMesh(nx, ny, Lx, Ly)
+mesh = PeriodicRectangleMesh(nx, ny, Lx, Ly, direction='x')
 x, y = SpatialCoordinate(mesh)
 
 # set up parameters
-dt = 0.01
-H = 1.
-g = 10
-beta = 1 #2.286e-11
+dt = 0.002
+H = 1
+g = 1
+beta = 1 # 2.286e-11
 fexpr = beta*(y-Ly/2)
 
 parameters = ShallowWaterParameters(H=H, g=g)
 
-dirname = "height_adjustment_gaussian_with_rotation"
+dirname = "height_adjustment_gaussian"
 
-output = OutputParameters(dirname=dirname, dumpfreq=10)
+output = OutputParameters(dirname=dirname, dumpfreq=1)
 
 diagnostic_fields = [CourantNumber()]
 
@@ -35,14 +35,15 @@ state = State(mesh,
               diagnostic_fields=diagnostic_fields,
               parameters=parameters)
 
-eqns = ShallowWaterEquations(state, "BDM", 1, fexpr=fexpr, thermal=True)
+eqns = ShallowWaterEquations(state, "BDM", 1, fexpr=fexpr,
+                             no_normal_flow_bc_ids=[1,2], thermal=True)
 
 # initial conditions
 u0 = state.fields("u")
 D0 = state.fields("D")
 b0 = state.fields("b")
-epsilon = 0.2
-a = 5
+epsilon = 0.3
+a = 0.3
 Dexpr = 1 - epsilon*exp(-((x-Lx/2)**2/(2*a**2) + (y-Ly/2)**2/(2*a**2)))
 D0.interpolate(Dexpr)
 b0.interpolate(Constant(1))
