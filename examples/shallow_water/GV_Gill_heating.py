f078f720594f1fa3c6c9d9dbebeb10b849ab5b7d
nhartney <nh491@exeter.ac.uk>
2022-12-15 12:18:38 +0000

    start of Gill heating problem following Geoff's example

---
+++ examples/shallow_water/GV_Gill_heating.py
@@ -0,0 +1,58 @@
+from gusto import *
+from firedrake import (PeriodicRectangleMesh, exp, Constant, sqrt, cos,
+                       conditional)
+
+# set up mesh
+Lx = 40
+Ly = 16
+delta = 0.2
+nx = int(Lx/delta)
+ny = int(Ly/delta)
+
+mesh = PeriodicRectangleMesh(nx, ny, Lx, Ly, direction='x')
+x, y = SpatialCoordinate(mesh)
+
+# set up parameters
+dt = 0.02
+beta = 0.5
+L = 2
+k = pi/(2*L)
+alpha = 0.15
+H = 0  # maybe??
+tmax = 10000
+fexpr = beta*(y-(Ly/2))
+
+dirname = "GV_Gill_heating"
+
+parameters = ShallowWaterParameters(H=H)
+
+output = OutputParameters(dirname=dirname, dumpfreq=1)
+
+diagnostic_fields = [CourantNumber(), RelativeVorticity()]
+
+state = State(mesh,
+              dt=dt,
+              output=output,
+              diagnostic_fields=diagnostic_fields,
+              parameters=parameters)
+
+expy = exp(0.25*(y-(Ly/2))**2)
+# forcing = cos(k*(x-(Lx/2)))*exp(0.25*(y-(Ly/2))**2)
+forcing = -((y-(Ly/2)) + 1)*(cos(k*(x-(Lx/2)))*expy)
+forcing_expr = conditional(x>((Lx/2)-L), conditional(x<((Lx/2)+L), forcing, 0), 0)
+
+# need to add damping terms too
+
+eqns = LinearShallowWaterEquations(state, "BDM", 1, fexpr=fexpr,
+                             no_normal_flow_bc_ids=[1,2])
+
+# initial conditions
+u0 = state.fields("u")
+D0 = state.fields("D")
+
+D0.interpolate(0.1*forcing_expr)
+
+# timestepper
+stepper = Timestepper(eqns, ForwardEuler(state), state)
+
+stepper.run(t=0, tmax = 2*dt)
