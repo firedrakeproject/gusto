afd367aadd63cde746b53a6149a2afa3369062ed
Dan Witt <danielwitt1@hotmail.co.uk>
2023-01-24 10:41:27 +0000

    adjusted data output points

--- examples/compressible/solid_body_rotation.py
+++ examples/compressible/solid_body_rotation.py
@@ -8,8 +8,8 @@ from gusto import *                                              #
 # Test case Parameters
 # -------------------------------------------------------------- #
 dt = 1800
-days = 1
-ndumps = 20
+days = 30
+ndumps = days
 tmax = days * 24 * 60 * 60
 deltaz = 2.0e3
 
@@ -34,6 +34,7 @@ f0 = 2 * omega * sin(phi0)
 Omega = as_vector((0, 0, f0))
 
 eqn = CompressibleEulerEquations(domain, params, Omega=Omega, u_transport_option='vector_advection_form')
+
 dirname = 'sbr_quadratic_%i_day_dt_%i_degree%i_solveforrho' % (days, dt, 2)
 output = OutputParameters(dirname=dirname,
                           dumpfreq=int(tmax / (ndumps * dt)),
