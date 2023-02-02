4bdb46ab6773a4de867e473181d9295353cc3c8a
Dan Witt <danielwitt1@hotmail.co.uk>
2023-02-02 15:05:52 +0000

    Added plotting for invariants

--- examples/shallow_water/williamson_6.py
+++ examples/shallow_water/williamson_6.py
@@ -7,10 +7,10 @@ from firedrake import IcosahedralSphereMesh, SpatialCoordinate, cos, grad
 
 # Set up timestepping variables
 day = 24. * 60. * 60.
-ref = 3
-dt = 1000
-tmax = 14*day
-ndumps = 14
+ref = [3, 4, 5]
+dt_val = [1000, 500, 250]
+tmax = 5*day
+ndumps = 5
 
 # Shallow Water Parameters
 a = 6371220.
@@ -21,75 +21,77 @@ parameters = ShallowWaterParameters(H=H)
 # ------------------------------------------------------------------------ #
 # Set up model objects
 # ------------------------------------------------------------------------ #
+for index, ref in enumerate(ref):
 
-# Mesh and domain
-mesh = IcosahedralSphereMesh(radius=a,
-                             refinement_level=ref, degree=1)
-x = SpatialCoordinate(mesh)
-global_normal = x
-mesh.init_cell_orientations(x)
-domain = Domain(mesh, dt, "BDM", 1)
+    dt = dt_val[index]
+    # Mesh and domain
+    mesh = IcosahedralSphereMesh(radius=a,
+                                refinement_level=ref, degree=1)
+    x = SpatialCoordinate(mesh)
+    global_normal = x
+    mesh.init_cell_orientations(x)
+    domain = Domain(mesh, dt, "BDM", 1)
 
-# Equations
-lat, lon = latlon_coords(mesh)
-Omega = parameters.Omega
-fexpr = 2*Omega * x[2] / a
-eqns = ShallowWaterEquations(domain, parameters, fexpr=fexpr, u_transport_option='vector_advection_form')
+    # Equations
+    lat, lon = latlon_coords(mesh)
+    Omega = parameters.Omega
+    fexpr = 2*Omega * x[2] / a
+    eqns = ShallowWaterEquations(domain, parameters, fexpr=fexpr, u_transport_option='vector_advection_form')
 
-# Output and IO
-dirname = 'Rossby-Haurwitx_Wave_Invariants'
-dumpfreq = int(tmax / (ndumps*dt))
-output = OutputParameters(dirname=dirname,
-                          dumpfreq=dumpfreq,
-                          dumplist_latlon=['D', 'D_error'],
-                          log_level='INFO')
-diagnostic_fields = [CourantNumber(), SteadyStateError('u'), SteadyStateError('D'), RelativeVorticity(), PotentialVorticity(),
-                     ShallowWaterKineticEnergy(), ShallowWaterPotentialEnergy(parameters), Divergence(), ShallowWaterPotentialEnstrophy()]
-io = IO(domain, output, diagnostic_fields=diagnostic_fields)
+    # Output and IO
+    dirname = 'Rossby_Haurwitx_Wave_Invariant_Ref%s' % ref
+    dumpfreq = int(tmax / (ndumps*dt))
+    output = OutputParameters(dirname=dirname,
+                            dumpfreq=dumpfreq,
+                            dumplist_latlon=['D', 'D_error'],
+                            log_level='INFO')
+    diagnostic_fields = [CourantNumber(), SteadyStateError('u'), SteadyStateError('D'), RelativeVorticity(), PotentialVorticity(), AbsoluteVorticity
+                        ShallowWaterKineticEnergy(), ShallowWaterPotentialEnergy(parameters), Divergence(), ShallowWaterPotentialEnstrophy()]
+    io = IO(domain, output, diagnostic_fields=diagnostic_fields)
 
-# Transport Fields and time stepper
-transported_fields = [SSPRK3(domain, "u"),
-                      SSPRK3(domain, "D")]
+    # Transport Fields and time stepper
+    transported_fields = [SSPRK3(domain, "u"),
+                        SSPRK3(domain, "D")]
 
-stepper = SemiImplicitQuasiNewton(eqns, io, transported_fields)
+    stepper = SemiImplicitQuasiNewton(eqns, io, transported_fields)
 
-# ------------------------------------------------------------------------ #
-# Initial Conditions
-# ------------------------------------------------------------------------ #
+    # ------------------------------------------------------------------------ #
+    # Initial Conditions
+    # ------------------------------------------------------------------------ #
 
-u0 = stepper.fields('u')
-D0 = stepper.fields('D')
+    u0 = stepper.fields('u')
+    D0 = stepper.fields('D')
 
-Vu = domain.spaces("HDiv")
-K = Constant(7.847e-6)
-w = K
-R = 4.
-h0 = 8000.
-g = parameters.g
+    Vu = domain.spaces("HDiv")
+    K = Constant(7.847e-6)
+    w = K
+    R = 4.
+    h0 = 8000.
+    g = parameters.g
 
-# Intilising the velocity field
-# CG2 = domain.spaces("CG", 2)
-CG2 = FunctionSpace(mesh, 'CG', 2)
-psi = Function(CG2)
-psiexpr = -a**2 * w * sin(lat) + a**2 * K * cos(lat)**R * sin(lat) * cos(R*lon)
-psi.interpolate(psiexpr)
-uexpr = domain.perp(grad(psi))
+    # Intilising the velocity field
+    # CG2 = domain.spaces("CG", 2)
+    CG2 = FunctionSpace(mesh, 'CG', 2)
+    psi = Function(CG2)
+    psiexpr = -a**2 * w * sin(lat) + a**2 * K * cos(lat)**R * sin(lat) * cos(R*lon)
+    psi.interpolate(psiexpr)
+    uexpr = domain.perp(grad(psi))
 
-# Initilising the depth field
-A = (w / 2) * (2 * Omega + w) * cos(lat)**2 + 0.25 * K**2 * cos(lat)**(2 * R) * ((R + 1) * cos(lat)**2 + (2 * R**2 - R - 2) - 2 * R**2 * cos(lat)**(-2))
-B_frac = (2 * (Omega + w) * K) / ((R + 1) * (R + 2))
-B = B_frac * cos(lat)**R * ((R**2 + 2 * R + 2) - (R + 1)**2 * cos(lat)**2)
-C = (1 / 4) * K**2 * cos(lat)**(2 * R) * ((R + 1)*cos(lat)**2 - (R + 2))
-Dexpr = h0 * g + a**2 * (A + B*cos(lon*R) + C * cos(2 * R * lon))
+    # Initilising the depth field
+    A = (w / 2) * (2 * Omega + w) * cos(lat)**2 + 0.25 * K**2 * cos(lat)**(2 * R) * ((R + 1) * cos(lat)**2 + (2 * R**2 - R - 2) - 2 * R**2 * cos(lat)**(-2))
+    B_frac = (2 * (Omega + w) * K) / ((R + 1) * (R + 2))
+    B = B_frac * cos(lat)**R * ((R**2 + 2 * R + 2) - (R + 1)**2 * cos(lat)**2)
+    C = (1 / 4) * K**2 * cos(lat)**(2 * R) * ((R + 1)*cos(lat)**2 - (R + 2))
+    Dexpr = h0 * g + a**2 * (A + B*cos(lon*R) + C * cos(2 * R * lon))
 
-# Finalizing fields and setting reference profiles
-u0.project(uexpr)
-D0.interpolate(Dexpr / g)
-# Dbar is a background field for diagnostics
-Dbar = Function(D0.function_space()).assign(H)
-stepper.set_reference_profiles([('D', Dbar)])
+    # Finalizing fields and setting reference profiles
+    u0.project(uexpr)
+    D0.interpolate(Dexpr / g)
+    # Dbar is a background field for diagnostics
+    Dbar = Function(D0.function_space()).assign(H)
+    stepper.set_reference_profiles([('D', Dbar)])
 
-# ------------------------------------------------------------------------ #
-# Run!
-# ------------------------------------------------------------------------ #
-stepper.run(t=0, tmax=tmax)
+    # ------------------------------------------------------------------------ #
+    # Run!
+    # ------------------------------------------------------------------------ #
+    stepper.run(t=0, tmax=tmax)
--- plotting/Williams6-Invariants.py
+++ plotting/Williams6-Invariants.py
@@ -4,4 +4,47 @@ Plotting the invariants of the William 6 test case
 import matplotlib.pyplot as plt
 import netCDF4 as nc
 
+
 data = nc.Dataset("results/Rossby-Haurwitx_Wave_Invariants/diagnostics.nc")
+time = data.variables['time'][:]
+Enstrophy = data.groups['SWPotentialEnstrophy_from_PotentialVorticity']['total'][:] / data.groups['SWPotentialEnstrophy_from_PotentialVorticity']['l2'][0]
+
+Kinetic_Energy = data.groups['ShallowWaterKineticEnergy']['l2'][:] / data.groups['ShallowWaterKineticEnergy']['l2'][0]
+Potential_Energy = data.groups['ShallowWaterPotentialEnergy']['l2'][:] / data.groups['ShallowWaterPotentialEnergy']['l2'][0]
+
+Divergence = data.groups['u_divergence']['l2'][:]
+P_vorticity = data.groups['PotentialVorticity']['l2'][:] 
+R_vorticity = data.groups['RelativeVorticity']['l2'][:] 
+Total_Energy = (data.groups['ShallowWaterKineticEnergy']['l2'][:] + data.groups['ShallowWaterPotentialEnergy']['l2'][:]) / (data.groups['ShallowWaterKineticEnergy']['l2'][0] + data.groups['ShallowWaterPotentialEnergy']['l2'][0])
+
+
+
+# Energy Plot
+plt.ylabel('Energy (Joules')
+plt.xlabel('Time (seconds)')
+name = ['Kinetic Energy', 'Potential Energy', 'Total Energy (KE + V)']
+for index, value in enumerate([Kinetic_Energy, Potential_Energy, Total_Energy]): 
+    plt.plot(time, value)
+    plt.title('%s Invariant' % name[index])
+    plt.show()
+
+# Vorticity plot 
+plt.ylabel('Vorticity')
+plt.xlabel('Time (seconds)')
+name = ['Relative', 'Potential']
+for index, value in enumerate([R_vorticity, P_vorticity]): 
+    plt.plot(time, value)
+    plt.title('%s Vorticity' % name[index])
+    plt.show()
+
+# Enstrophy Plt
+plt.ylabel('Enstrophy')
+plt.xlabel('Time')
+plt.plot(time, Enstrophy)
+plt.show()
+
+# Divergence Plt
+plt.ylabel('Divergence')
+plt.xlabel('Time')
+plt.plot(time, Divergence)
+plt.show()
