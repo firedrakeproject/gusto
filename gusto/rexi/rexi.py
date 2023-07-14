d293eed774e98252161e397e98e9ab7e00c2be9a
nhartney <nh491@exeter.ac.uk>
2023-07-14 11:34:28 +0100

    M and h should have a default

--- gusto/rexi/rexi.py
+++ gusto/rexi/rexi.py
@@ -10,8 +10,8 @@ class RexiParameters(Configuration):
     """
     Parameters for the REXI coefficients
     """
-    h = None
-    M = None
+    h = 0.2
+    M = 64
     reduce_to_half = False
 
 
