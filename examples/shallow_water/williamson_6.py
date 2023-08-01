43d934cd24ee6d24ea696b4c3d593349e0d2f3ae
dw603@exeter.ac.uk <dw603@exeter.ac.uk>
2023-08-01 11:52:02 +0100

    output to tomplotdata

--- examples/shallow_water/williamson_6.py
+++ examples/shallow_water/williamson_6.py
@@ -3,7 +3,7 @@ Rossby-Haurwitz wave
 """
 
 from gusto import *
-from firedrake import IcosahedralSphereMesh, SpatialCoordinate, cos, grad, curl, div, TensorProductElement
+from firedrake import IcosahedralSphereMesh, SpatialCoordinate, cos, grad, curl, div, TensorProductElement, CubedSphereMesh
 
 # Set up timestepping variables
 day = 24. * 60. * 60.
@@ -21,77 +21,83 @@ parameters = ShallowWaterParameters(H=H)
 # ------------------------------------------------------------------------ #
 # Set up model objects
 # ------------------------------------------------------------------------ #
-
+meshs = [IcosahedralSphereMesh, CubedSphereMesh]
+family = ["BDM", "RTCF"]
 dt = 250
-# Mesh and domain
-mesh = IcosahedralSphereMesh(radius=a,
-                            refinement_level=ref, degree=2)
-x = SpatialCoordinate(mesh)
-domain = Domain(mesh, dt, "BDM", 1)
 
-# Equations
-lat, lon = latlon_coords(mesh)
-Omega = parameters.Omega
-fexpr =  2*Omega * x[2] / a
-eqns = ShallowWaterEquations(domain, parameters, fexpr=fexpr, u_transport_option='vector_invariant_form')
+for index, Mesh in enumerate(meshs):
+        
+    # Mesh and domain
+    mesh = Mesh(radius=a, refinement_level=ref, degree=2)
+    x = SpatialCoordinate(mesh)
+    domain = Domain(mesh, dt, family[index], 1)
 
-# Output and IO
-dirname = 'W6_Red=6_dt=500_long'
-dumpfreq = int(tmax / (ndumps*dt))
-output = OutputParameters(dirname=dirname,
-                        dumpfreq=dumpfreq,
-                        dumplist_latlon=['D', 'D_error'],
-                        log_level='INFO')
+    # Equations
+    lat, lon = latlon_coords(mesh)
+    Omega = parameters.Omega
+    fexpr =  2*Omega * x[2] / a
+    eqns = ShallowWaterEquations(domain, parameters, fexpr=fexpr, u_transport_option='vector_invariant_form')
+    print(eqns.X.function_space().dim())
+    # Output and IO
+    dirname = f'Williamson6_mesh={mesh}'
+    dumpfreq = int(tmax / (ndumps*dt))
 
-diagnostic_fields = [CourantNumber(), SteadyStateError('u'), SteadyStateError('D'), RelativeVorticity(), PotentialVorticity(), AbsoluteVorticity(),
-                    ShallowWaterKineticEnergy(), ShallowWaterPotentialEnergy(parameters), Divergence(), ShallowWaterPotentialEnstrophy()]
-io = IO(domain, output, diagnostic_fields=diagnostic_fields)
+    output = OutputParameters(dirname=dirname,
+                          dumpfreq=int(tmax / (ndumps * dt)),
+                          dump_nc=True,
+                          dump_vtus=False,
+                          log_level=('INFO'))
+    diagnostic_fields = [MeridionalComponent('u'), ZonalComponent('u'), 
+                     CourantNumber(), SteadyStateError('u'), SteadyStateError('D'),
+                     RelativeVorticity, PotentialVorticity, AbsoluteVorticity,
+                     ShallowWaterKineticEnergy, ShallowWaterPotentialEnergy(parameters),
+                       Divergence(), ShallowWaterPotentialEnstrophy()]
 
-# Transport Fields and time stepper
-transported_fields = [SSPRK3(domain, "u"),
-                    SSPRK3(domain, "D")]
-transport_methods = [DGUpwind(eqns, field) for field in ["u", "D"]]
+    # Transport Fields and time stepper
+    transported_fields = [SSPRK3(domain, "u"),
+                        SSPRK3(domain, "D")]
+    transport_methods = [DGUpwind(eqns, field) for field in ["u", "D"]]
 
-stepper = SemiImplicitQuasiNewton(eqns, io, transported_fields, transport_methods)
+    stepper = SemiImplicitQuasiNewton(eqns, io, transported_fields, transport_methods)
 
-# ------------------------------------------------------------------------ #
-# Initial Conditions
-# ------------------------------------------------------------------------ #
+    # ------------------------------------------------------------------------ #
+    # Initial Conditions
+    # ------------------------------------------------------------------------ #
 
-u0 = stepper.fields('u')
-D0 = stepper.fields('D')
+    u0 = stepper.fields('u')
+    D0 = stepper.fields('D')
 
-Vu = domain.spaces("HDiv")
-K = Constant(7.847e-6)
-w = K
-R = 4.
-h0 = 8000.
-g = parameters.g
+    Vu = domain.spaces("HDiv")
+    K = Constant(7.847e-6)
+    w = K
+    R = 4.
+    h0 = 8000.
+    g = parameters.g
 
-# Intilising the velocity field
-# CG2 = domain.spaces("CG", 2)
-CG2 = FunctionSpace(mesh, 'CG', 2)
-psi = Function(CG2)
-psiexpr = -a**2 * w * sin(lat) + a**2 * K * cos(lat)**R * sin(lat) * cos(R*lon)
-psi.interpolate(psiexpr)
-uexpr = domain.perp(grad(psi))
-#Dexpr = domain.perp(div(curl(grad(psi))))
+    # Intilising the velocity field
+    # CG2 = domain.spaces("CG", 2)
+    CG2 = FunctionSpace(mesh, 'CG', 2)
+    psi = Function(CG2)
+    psiexpr = -a**2 * w * sin(lat) + a**2 * K * cos(lat)**R * sin(lat) * cos(R*lon)
+    psi.interpolate(psiexpr)
+    uexpr = domain.perp(grad(psi))
+    #Dexpr = domain.perp(div(curl(grad(psi))))
 
-# Initilising the depth field
-A = (w / 2) * (2 * Omega + w) * cos(lat)**2 + 0.25 * K**2 * cos(lat)**(2 * R) * ((R + 1) * cos(lat)**2 + (2 * R**2 - R - 2) - 2 * R**2 * cos(lat)**(-2))
-B_frac = (2 * (Omega + w) * K) / ((R + 1) * (R + 2))
-B = B_frac * cos(lat)**R * ((R**2 + 2 * R + 2) - (R + 1)**2 * cos(lat)**2)
-C = (1 / 4) * K**2 * cos(lat)**(2 * R) * ((R + 1)*cos(lat)**2 - (R + 2))
-Dexpr = h0 * g + a**2 * (A + B*cos(lon*R) + C * cos(2 * R * lon))
+    # Initilising the depth field
+    A = (w / 2) * (2 * Omega + w) * cos(lat)**2 + 0.25 * K**2 * cos(lat)**(2 * R) * ((R + 1) * cos(lat)**2 + (2 * R**2 - R - 2) - 2 * R**2 * cos(lat)**(-2))
+    B_frac = (2 * (Omega + w) * K) / ((R + 1) * (R + 2))
+    B = B_frac * cos(lat)**R * ((R**2 + 2 * R + 2) - (R + 1)**2 * cos(lat)**2)
+    C = (1 / 4) * K**2 * cos(lat)**(2 * R) * ((R + 1)*cos(lat)**2 - (R + 2))
+    Dexpr = h0 * g + a**2 * (A + B*cos(lon*R) + C * cos(2 * R * lon))
 
-# Finalizing fields and setting reference profiles
-u0.project(uexpr)
-D0.interpolate(Dexpr / g)
-# Dbar is a background field for diagnostics
-Dbar = Function(D0.function_space()).assign(H)
-stepper.set_reference_profiles([('D', Dbar)])
+    # Finalizing fields and setting reference profiles
+    u0.project(uexpr)
+    D0.interpolate(Dexpr / g)
+    # Dbar is a background field for diagnostics
+    Dbar = Function(D0.function_space()).assign(H)
+    stepper.set_reference_profiles([('D', Dbar)])
 
-# ------------------------------------------------------------------------ #
-# Run!
-# ------------------------------------------------------------------------ #
-stepper.run(t=0, tmax=tmax)
+    # ------------------------------------------------------------------------ #
+    # Run!
+    # ------------------------------------------------------------------------ #
+    stepper.run(t=0, tmax=tmax)
