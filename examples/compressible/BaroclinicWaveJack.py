057f78cf046a126a698acc308d422a925c055a65
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2023-08-09 11:40:04 +0100

    Lint and format WaveJack

--- examples/compressible/BaroclinicWaveJack.py
+++ examples/compressible/BaroclinicWaveJack.py
@@ -1,18 +1,21 @@
 from firedrake import (ExtrudedMesh, SpatialCoordinate, cos, sin, pi, sqrt,
                        exp, Constant, Function, as_vector, acos,
                        errornorm, norm, min_value, max_value, le, ge)
-from gusto import *                                            # 
-# -------------------------------------------------------------- #
-# Test case Parameters
-# -------------------------------------------------------------- #
+from gusto import *
+from gusto.logging import info
+
+
+# ----------------------- #
+#  Test case Parameters   #
+# ----------------------- #
 dt = 270.
 days = 10.
 tmax = days * 24. * 60. * 60.
-deltaz = 2e3 # 15 layers, as we are in a higher space this matches the paper better
+deltaz = 2e3  # 15 layers, as we are in a higher space this matches the paper better
 
-# -------------------------------------------------------------- #
-# Set up Model
-# -------------------------------------------------------------- #
+# --------------- #
+#  Set up Model   #
+# --------------- #
 
 # Domain
 a = 6.371229e6  # radius of earth
@@ -29,18 +32,20 @@ omega = Constant(7.292e-5)
 Omega = as_vector((0, 0, omega))
 
 eqn = CompressibleEulerEquations(domain, params, Omega=Omega, u_transport_option='vector_invariant_form')
-print(eqn.X.function_space().dim())
+info(f'Number of degrees of freedom {eqn.X.function_space().dim()}')
 
 dirname = 'baroclinicPerturbation_thetalimiter'
 output = OutputParameters(dirname=dirname,
                           dumpfreq=40,
                           dump_nc=True,
                           dump_vtus=False)
-diagnostic_fields = [MeridionalComponent('u'), ZonalComponent('u'), 
-                     RadialComponent('u'), CourantNumber(), ZonalComponent('u_pert'),
-                     MeridionalComponent('u_pert'), Temperature(eqn), Pressure(eqn), 
-                     SteadyStateError('Temperature'), SteadyStateError('Pressure_Vt')]
-          
+diagnostic_fields = [
+    MeridionalComponent('u'), ZonalComponent('u'),
+    RadialComponent('u'), CourantNumber(), ZonalComponent('u_pert'),
+    MeridionalComponent('u_pert'), Temperature(eqn), Pressure(eqn),
+    SteadyStateError('Temperature'), SteadyStateError('Pressure_Vt')
+]
+
 io = IO(domain, output, diagnostic_fields=diagnostic_fields)
 Vtheta = domain.spaces("theta")
 limiter = ThetaLimiter(Vtheta)
@@ -60,9 +65,9 @@ stepper = SemiImplicitQuasiNewton(eqn, io, transported_fields,
                                   transport_methods,
                                   linear_solver=linear_solver)
 
-# -------------------------------------------------------------- #
-# Initial Conditions
-# -------------------------------------------------------------- #
+# --------------------- #
+#  Initial Conditions   #
+# --------------------- #
 
 x, y, z = SpatialCoordinate(mesh)
 lat, lon = latlon_coords(mesh)
@@ -80,12 +85,17 @@ g = params.g
 p0 = Constant(100000)
 
 lapse = 0.005
-T0e = 310 # Equatorial temp
-T0p = 240 # Polar surface temp
+# Equatorial temp
+T0e = 310
+# Polar surface temp
+T0p = 240
 T0 = 0.5 * (T0e + T0p)
-H = Rd * T0 / g # scale height of atmosphere
-k = 3 # power of temp field
-b = 2 # half width parameter
+# scale height of atmosphere
+H = Rd * T0 / g
+# power of temp field
+k = 3
+# half width parameter
+b = 2
 
 u = stepper.fields("u")
 rho0 = stepper.fields("rho")
@@ -110,43 +120,67 @@ tao1 = A * lapse / T0 * exp((r - a)*lapse / T0) + B * (1 - 2*((r-a)/(b*H))**2)*e
 tao2 = C * (1 - 2*((r-a)/(b*H))**2)*exp(-((r - a) / (b*H))**2)
 
 tao1_int = A * (exp(lapse * (r - a) / T0) - 1) + B * (r - a) * exp(-((r-a) / (b*H))**2)
-tao2_int = C * (r - a)  * exp(-((r-a) / (b*H))**2)
+tao2_int = C * (r - a) * exp(-((r-a) / (b*H))**2)
 
 # Variable fields
-Temp = (a / r)**2 * (tao1 - tao2 * ( s**k - (k / (k+2)) *s**(k+2)))**(-1)
-P_expr = p0 * exp(-g / Rd * tao1_int + g / Rd * tao2_int * (s**k - (k / (k+2)) *s**(k+2)))
+Temp = (a / r)**2 * (tao1 - tao2 * (s**k - (k / (k+2)) * s**(k+2)))**(-1)
+P_expr = p0 * exp(-g / Rd * tao1_int + g / Rd * tao2_int * (s**k - (k / (k+2)) * s**(k+2)))
 wind = ((g*k) / (2 * omega * a)) * (cos(lat)**(k-1) - cos(lat)**(k+1))*tao2_int*Temp
 
-theta_expr = Temp * (P_expr / p0) ** (-params.kappa) 
+theta_expr = Temp * (P_expr / p0) ** (-params.kappa)
 pie_expr = Temp / theta_expr
 rho_expr = P_expr / (Rd * Temp)
-# -------------------------------------------------------------- #
-# Perturbation
-# -------------------------------------------------------------- #
 
-zt = 1.5e4     # top of perturbation
-d0 = a / 6     # horizontal radius of perturbation
-Vp = 1         # Perturbed wind amplitude  
-lon_c , lat_c = pi/9,  2*pi/9 # location of perturbation centre   
+
+# -------------- #
+# Perturbation   #
+# -------------- #
+
+# top of perturbation
+zt = 1.5e4
+# horizontal radius of perturbation
+d0 = a / 6
+# Perturbed wind amplitude
+Vp = 1
+# location of perturbation centre
+lon_c, lat_c = pi/9, 2*pi/9
 err_tol = 1e-12
 
-d = a * acos(sin(lat_c)*sin(lat) + cos(lat_c)*cos(lat)*cos(lon - lon_c)) # distance from centre of perturbation
+# distance from centre of perturbation
+d = a * acos(sin(lat_c)*sin(lat) + cos(lat_c)*cos(lat)*cos(lon - lon_c))
 
-depth = r - a # The distance from origin subtracted from earth radius
-zeta = conditional(ge(depth,zt-err_tol), 0, 1 - 3*(depth / zt)**2 + 2*(depth / zt)**3) # peturbation vertical taper
+# The distance from origin subtracted from earth radius
+depth = r - a
+# peturbation vertical taper
+zeta = conditional(
+    ge(depth, zt-err_tol), 0,
+    1 - 3*(depth / zt)**2 + 2*(depth / zt)**3
+)
 
 perturb_magnitude = (16*Vp/(3*sqrt(3))) * zeta * sin((pi * d) / (2 * d0)) * cos((pi * d) / (2 * d0)) ** 3
 
 
-zonal_pert = conditional(le(d,err_tol), 0, 
-                         conditional(ge(d,(d0-err_tol)), 0, -perturb_magnitude * (-sin(lat_c)*cos(lat) + cos(lat_c)*sin(lat)*cos(lon - lon_c)) / sin(d / a)))
-meridional_pert = conditional(le(d,err_tol), 0, 
-                              conditional(ge(d,d0-err_tol), 0, perturb_magnitude * cos(lat_c)*sin(lon - lon_c) / sin(d / a)))
+zonal_pert = conditional(
+    le(d, err_tol), 0,
+    conditional(
+        ge(d, (d0-err_tol)), 0,
+        -perturb_magnitude * (-sin(lat_c)*cos(lat) + cos(lat_c)*sin(lat)*cos(lon - lon_c)) / sin(d / a)
+    )
+)
+meridional_pert = conditional(
+    le(d, err_tol), 0,
+    conditional(
+        ge(d, d0-err_tol), 0,
+        perturb_magnitude * cos(lat_c)*sin(lon - lon_c) / sin(d / a)
+    )
+)
 
 rho_expr = P_expr / (Rd * Temp)
-# -------------------------------------------------------------- #
-# Configuring fields
-# -------------------------------------------------------------- #
+
+
+# --------------------#
+# Configuring fields  #
+# --------------------#
 # get components of u in spherical polar coordinates
 zonal_u = wind + zonal_pert
 merid_u = Constant(0.0) + meridional_pert
@@ -160,28 +194,28 @@ wind_pert = stepper.fields('u_pert', space=Vu)
 wind_pert.project(as_vector([u_pert, v_pert, w_pert]))
 
 # obtain initial conditions
-print('Set up initial conditions')
-print('project u')
+info('Set up initial conditions')
+info('project u')
 u.project(as_vector([u_expr, v_expr, w_expr]))
-print('interpolate theta')
+info('interpolate theta')
 theta0.interpolate(theta_expr)
-print('find pi')
+info('find pi')
 pie = Function(Vr).interpolate(pie_expr)
-print('find rho')
+info('find rho')
 rho0.interpolate(rho_expr)
 compressible_hydrostatic_balance(eqn, theta0, rho0, exner_boundary=pie, solve_for_rho=True)
 
-print('make analytic rho')
+info('make analytic rho')
 rho_analytic = Function(Vr).interpolate(rho_expr)
-print('Normalised rho error is:', errornorm(rho_analytic, rho0) / norm(rho_analytic))
+info('Normalised rho error is:', errornorm(rho_analytic, rho0) / norm(rho_analytic))
 
 # make mean fields
-print('make mean fields')
+info('make mean fields')
 rho_b = Function(Vr).assign(rho0)
 theta_b = Function(Vt).assign(theta0)
 
 # assign reference profiles
 stepper.set_reference_profiles([('rho', rho_b),
                                 ('theta', theta_b)])
-print('Intialise Windy Boi')
+info('Intialise Windy Boi')
 stepper.run(t=0, tmax=tmax)
