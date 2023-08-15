4a21a43122b225f8fb699a1f4fb4bd814971afcc
jshipton <j.shipton@exeter.ac.uk>
2023-08-15 14:33:50 +0100

    rexi implementation

---
+++ gusto/rexi/rexi.py
@@ -0,0 +1,236 @@
+from gusto.rexi.rexi_coefficients import *
+from firedrake import Function, TrialFunctions, TestFunctions, \
+    Constant, DirichletBC, \
+    LinearVariationalProblem, LinearVariationalSolver, MixedFunctionSpace
+from gusto import Configuration, replace_subject, drop, time_derivative, all_terms, replace_test_function, prognostic, Term, perp, NullTerm, linearisation, subject, replace_trial_function
+from firedrake.formmanipulation import split_form
+
+
+class RexiParameters(Configuration):
+    """
+    Parameters for the REXI coefficients
+    """
+    h = 0.2
+    M = 64
+    reduce_to_half = False
+
+
+class Rexi(object):
+    """
+    Class defining the solver for the system
+
+    (A_n + tau L)V_n = U
+
+    required for computing the matrix exponential as described in notes.pdf
+
+    :arg equation: :class:`.Equation` object defining the equation set to
+    be solved
+    :arg rexi_parameters: :class:`.Equation` object
+    :arg solver_parameters: dictionary of solver parameters. Default None,
+    which results in the default solver parameters defined in the equation
+    class being used.
+    :arg manager: :class:`.Ensemble` object containing the space and ensemble
+    subcommunicators
+
+    """
+    def __init__(self, equation, rexi_parameters, *, solver_parameters=None,
+                 manager=None):
+
+        residual = equation.residual.label_map(
+            lambda t: t.has_label(linearisation),
+            map_if_true=lambda t: Term(t.get(linearisation).form, t.labels),
+            map_if_false=drop)
+        residual = residual.label_map(
+            all_terms,
+            lambda t: replace_trial_function(t.get(subject))(t))
+
+        # Get the Rexi Coefficients, given the values of h and M in
+        # rexi_parameters
+        self.alpha, self.beta, self.beta2 = RexiCoefficients(rexi_parameters)
+
+        self.manager = manager
+
+        # define the start point of the solver loop (idx) and the
+        # number of solvers (N) for this process depending on the
+        # total number of solvers (nsolvers) and how many ensemble
+        # processes (neprocs) there are.
+        nsolvers = len(self.alpha)
+        if manager is None:
+            # if running in serial we loop over all the solvers, from
+            # 0: nsolvers
+            self.N = nsolvers
+            self.idx = 0
+        else:
+            rank = manager.ensemble_comm.rank
+            neprocs = manager.ensemble_comm.size
+            m = int(nsolvers/neprocs)
+            p = nsolvers - m*neprocs
+            if rank < p:
+                self.N = m+1
+                self.idx = rank*(m+1)
+            else:
+                self.N = m
+                self.idx = rank*m + p
+
+        # set dummy constants for tau and A_i
+        self.ar = Constant(1.)
+        self.ai = Constant(1.)
+        self.tau = Constant(1.)
+
+        # set up functions, problem and solver
+        W_ = equation.function_space
+        self.w_out = Function(W_)
+        spaces = []
+        for i in range(len(W_)):
+            spaces.append(W_[i])
+            spaces.append(W_[i])
+        W = MixedFunctionSpace(spaces)
+        self.U0 = Function(W)
+        self.w_sum = Function(W)
+        self.w = Function(W)
+        self.w_ = Function(W)
+        tests = TestFunctions(W)
+        trials = TrialFunctions(W)
+        tests_r = tests[::2]
+        tests_i = tests[1::2]
+        trials_r = trials[::2]
+        trials_i = trials[1::2]
+
+        ar, ai = self.ar, self.ai
+        a = NullTerm
+        L = NullTerm
+        for i in range(len(W_)):
+            ith_res = residual.label_map(
+                lambda t: t.get(prognostic) == equation.field_names[i],
+                lambda t: Term(
+                    split_form(t.form)[i].form,
+                    t.labels),
+                drop)
+
+            mass_form = ith_res.label_map(
+                lambda t: t.has_label(time_derivative),
+                map_if_false=drop)
+
+            m = mass_form.label_map(
+                all_terms,
+                replace_test_function(tests_r[i], i))
+            a += (
+                (ar + ai) * m.label_map(all_terms,
+                                        replace_subject(trials_r[i], i))
+                + (ar - ai) * m.label_map(all_terms,
+                                        replace_subject(trials_i[i], i))
+                )
+
+            L += (
+                m.label_map(all_terms, replace_subject(self.U0.split()[2*i], i))
+                + m.label_map(all_terms, replace_subject(self.U0.split()[2*i+1], i))
+            )
+
+            m = mass_form.label_map(
+                all_terms,
+                replace_test_function(tests_i[i], i))
+            a += (
+                (ar - ai) * m.label_map(all_terms,
+                                        replace_subject(trials_r[i], i))
+                +(-ar - ai) * m.label_map(all_terms,
+                                        replace_subject(trials_i[i], i))
+                )
+
+            L += (
+                m.label_map(all_terms, replace_subject(self.U0.split()[2*i], i))
+                - m.label_map(all_terms, replace_subject(self.U0.split()[2*i+1], i))
+            )
+
+            L_form = ith_res.label_map(
+                lambda t: t.has_label(time_derivative),
+                drop)
+
+            Lr = L_form.label_map(
+                all_terms,
+                replace_test_function(tests_r[i], i))
+            a -= self.tau * Lr.label_map(all_terms,
+                                         replace_subject(trials_r))
+            a -= self.tau * Lr.label_map(all_terms,
+                                        replace_subject(trials_i))
+
+            Li = L_form.label_map(
+                all_terms,
+                replace_test_function(tests_i[i], i))
+            a -= self.tau * Li.label_map(all_terms,
+                                        replace_subject(trials_r))
+            a += self.tau * Li.label_map(all_terms,
+                                        replace_subject(trials_i))
+
+
+        a = a.label_map(lambda t: t is NullTerm, drop)
+        L = L.label_map(lambda t: t is NullTerm, drop)
+
+        if hasattr(equation, "aP"):
+            aP = equation.aP(trial, self.ai, self.tau)
+        else:
+            aP = None
+
+        # Boundary conditions (assumes extruded mesh)
+        # BCs are declared for the plain velocity space. As we need them in
+        # extended mixed problem, we replicate the BCs but for subspace of W
+        bcs = []
+        for bc in equation.bcs['u']:
+            bcs.append(DirichletBC(W.sub(0), bc.function_arg, bc.sub_domain))
+            bcs.append(DirichletBC(W.sub(1), bc.function_arg, bc.sub_domain))
+
+        rexi_prob = LinearVariationalProblem(a.form, L.form, self.w, aP=aP,
+                                             bcs=bcs,
+                                             constant_jacobian=False)
+
+        #if solver_parameters is None:
+        #    solver_parameters = equation.solver_parameters
+
+        self.solver = LinearVariationalSolver(
+            rexi_prob, solver_parameters=solver_parameters)
+
+    def solve(self, x_out, x_in, dt):
+        """
+        Solve method for approximating the matrix exponential by a
+        rational sum. Solves
+
+        (A_n + tau L)V_n = U
+
+        multiplies by the corresponding B_n and sums over n.
+
+        :arg U0: the mixed function on the rhs.
+        :arg dt: the value of tau
+
+        """
+
+        # assign tau and U0 and initialise solution to 0.
+        self.tau.assign(dt)
+        Uin = x_in.split()
+        U0 = self.U0.split()
+        for i in range(len(Uin)):
+            U0[2*i].assign(Uin[i])
+        self.w_.assign(0.)
+        w_ = self.w_.split()
+        w = self.w.split()
+
+        # loop over solvers, assigning a_i, solving and accumulating the sum
+        for i in range(self.N):
+            j = self.idx + i
+            self.ar.assign(self.alpha[j].real)
+            self.ai.assign(self.alpha[j].imag)
+            self.solver.solve()
+            for k in range(len(Uin)):
+                wk = w_[2*k]
+                wk += Constant(self.beta[j].real)*w[2*k] - Constant(self.beta[j].imag)*w[2*k+1]
+
+        # in parallel we have to accumulate the sum over all processes
+        if self.manager is not None:
+            self.manager.allreduce(self.w_, self.w_sum)
+        else:
+            self.w_sum.assign(self.w_)
+
+        w_sum = self.w_sum.split()
+        w_out = self.w_out.split()
+        for i in range(len(w_out)):
+            w_out[i].assign(w_sum[2*i])
+
+        x_out.assign(self.w_out)
---
+++ gusto/rexi/rexi_coefficients.py
@@ -0,0 +1,137 @@
+import numpy
+
+
+class REXIParameters(object):
+    """
+    mu and a coefficients from
+    "A high-order time-parallel scheme for solving wave propagation problems
+    via the direct construction of an approximate time-evolution operator",
+    Haut et.al.
+    """
+
+    mu = -4.315321510875024 + 1j*0
+    L = 11
+    a = [
+        -1.0845749544592896e-7 + 1j*2.77075431662228e-8,
+        1.858753344202957e-8 + 1j*-9.105375434750162e-7,
+        3.6743713227243024e-6 + 1j*7.073284346322969e-7,
+        -2.7990058083347696e-6 + 1j*0.0000112564827639346,
+        0.000014918577548849352 + 1j*-0.0000316278486761932,
+        -0.0010751767283285608 + 1j*-0.00047282220513073084,
+        0.003816465653840016 + 1j*0.017839810396560574,
+        0.12124105653274578 + 1j*-0.12327042473830248,
+        -0.9774980792734348 + 1j*-0.1877130220537587,
+        1.3432866123333178 + 1j*3.2034715228495942,
+        4.072408546157305 + 1j*-6.123755543580666,
+        -9.442699917778205 + 1j*0.,
+        4.072408620272648 + 1j*6.123755841848161,
+        1.3432860877712938 + 1j*-3.2034712658530275,
+        -0.9774985292598916 + 1j*0.18771238018072134,
+        0.1212417070363373 + 1j*0.12326987628935386,
+        0.0038169724770333343 + 1j*-0.017839242222443888,
+        -0.0010756025812659208 + 1j*0.0004731874917343858,
+        0.000014713754789095218 + 1j*0.000031358475831136815,
+        -2.659323898804944e-6 + 1j*-0.000011341571201752273,
+        3.6970377676364553e-6 + 1j*-6.517457477594937e-7,
+        3.883933649142257e-9 + 1j*9.128496023863376e-7,
+        -1.0816457995911385e-7 + 1j*-2.954309729192276e-8
+    ]
+
+
+def b_coefficients(h, M):
+    """
+    Compute the b coefficients where
+    b_m = h^2 exp(imh)
+    """
+    m = numpy.arange(-M, M+1)
+    return numpy.exp(h*h)*numpy.exp(-1j*m*h)
+
+
+def RexiCoefficients(rexi_parameters):
+    """
+    Compute the A_n and B_n coefficients in the REXI sum
+
+    exp(ix) \approx sum_{n=0}^{P} B_n (ix + A_n)
+
+    where P = 4N+1 if rexi_parameters.reduce_to_half is False else
+    2N+1 if True.
+
+    Returns 3 numpy arrays:
+    alpha contains the A_n coefficients
+    beta contains the B_n coefficients
+    beta2 contains zeros if rexi_parameters.reduce_to_half is False else
+    it contains the coefficients that multiply the conjugate terms
+
+    :arg rexi_parameters: class containing the parameters (h, M and
+    reduce_to_half) necessary to compute the coefficients
+
+    """
+
+    h = float(rexi_parameters.h)
+    M = int(float(rexi_parameters.M))
+
+    # get L, mu and the a coefficients
+    params = REXIParameters()
+    L = params.L
+    mu = params.mu
+    a = params.a
+
+    # calculate the b coefficients
+    b = b_coefficients(h, M)
+
+    # allocate arrays for alpha, beta_re and beta_im
+    N = M + L
+    alpha = numpy.zeros((2*N+1,), dtype=numpy.complex128)
+    beta_re = numpy.zeros((2*N+1,), dtype=numpy.complex128)
+    beta_im = numpy.zeros((2*N+1,), dtype=numpy.complex128)
+
+    # compute alpha, beta_re and beta_im
+    for l in range(-L, L+1):
+        for m in range(-M, M+1):
+            n = l+m
+            alpha[n+N] = h*(mu + 1j*n)
+            beta_re[n+N] += b[m+M].real*h*a[l+L]
+            beta_im[n+N] += b[m+M].imag*h*a[l+L]
+
+    # calculate conj(beta_re) and conj(beta_im), used to define A_n and B_n
+    beta_conj_re = numpy.conjugate(beta_re)
+    beta_conj_im = numpy.conjugate(beta_im)
+
+    if rexi_parameters.reduce_to_half:
+        # If reducing the number of solvers to (nearly) half, as
+        # described in notes.pdf, we only need alpha_n for n \in [N,
+        # 2N]. We need to retain all the betas (due to the lack of
+        # symmetry coming from the numerical calculation of the a
+        # coefficients) but in order not to special case the Nth
+        # solver (where the solution is real hence equal to its
+        # conjugate) we must divide the Nth value of the betas by 2.
+        alpha = alpha[N:]
+        beta_re[N] /= 2.
+        beta_im[N] /= 2.
+        beta_conj_re[N] /= 2.
+        beta_conj_im[N] /= 2.
+        beta = numpy.concatenate(
+            (beta_re[N:] + 1j*beta_im[N:],
+             -beta_conj_re[N::-1] - 1j*beta_conj_im[N::-1])
+        )/2
+        # beta2 is the coefficient that multiplies the conjugate of
+        # the solution
+        beta2 = numpy.concatenate(
+            (beta_re[N::-1] + 1j*beta_im[N::-1],
+             -beta_conj_re[N:] - 1j*beta_conj_im[N:])
+        )/2
+    else:
+        beta = numpy.concatenate(
+            (beta_re + 1j*beta_im,
+             -beta_conj_re[::-1] - 1j*beta_conj_im[::-1])
+        )/2
+        # when not reducing the number of solvers we return zero here
+        # in order not to special case this option (i.e. we still
+        # calculate the conjugate of the solution but then multiply it
+        # by zero - you're doing (nearly) twice the amount of work necessary
+        # anyway!)
+        beta2 = numpy.zeros(len(beta))
+
+    alpha = numpy.concatenate((alpha, -alpha))
+
+    return alpha, beta, beta2
