1a56e1b819bb684035340bb24beb045aca620094
atb1995 <alex.brown1995@hotmail.co.uk>
2023-09-07 14:49:24 +0100

    add unit test

--- integration-tests/model/test_time_discretisation.py
+++ integration-tests/model/test_time_discretisation.py
@@ -8,7 +8,7 @@ def run(timestepper, tmax, f_end):
     return norm(timestepper.fields("f") - f_end) / norm(f_end)
 
 
-@pytest.mark.parametrize("scheme", ["ssprk", "implicit_midpoint",
+@pytest.mark.parametrize("scheme", ["ssprk", "TrapeziumRule", "ImplicitMidpoint",
                                     "RK4", "Heun", "BDF2", "TR_BDF2", "AdamsBashforth", "Leapfrog", "AdamsMoulton"])
 def test_time_discretisation(tmpdir, scheme, tracer_setup):
     if (scheme == "AdamsBashforth"):
@@ -27,8 +27,10 @@ def test_time_discretisation(tmpdir, scheme, tracer_setup):
 
     if scheme == "ssprk":
         transport_scheme = SSPRK3(domain)
-    elif scheme == "implicit_midpoint":
+    elif scheme == "TrapeziumRule":
         transport_scheme = TrapeziumRule(domain)
+    elif scheme == "ImplicitMidpoint":
+        transport_scheme = ImplicitMidpoint(domain)
     elif scheme == "RK4":
         transport_scheme = RK4(domain)
     elif scheme == "Heun":
