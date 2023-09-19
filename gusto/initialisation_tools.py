a766a30ece5a18349543b169f6ea6661550687d6
dw603@exeter.ac.uk <dw603@exeter.ac.uk>
2023-09-19 12:09:50 +0100

    replaced atan2 with atan

--- gusto/initialisation_tools.py
+++ gusto/initialisation_tools.py
@@ -6,7 +6,7 @@ from firedrake import MixedFunctionSpace, TrialFunctions, TestFunctions, \
     Function, Constant, \
     LinearVariationalProblem, LinearVariationalSolver, \
     NonlinearVariationalProblem, NonlinearVariationalSolver, split, solve, \
-    sin, cos, sqrt, asin, atan2, as_vector, min_value, max_value, FunctionSpace, \
+    sin, cos, sqrt, asin, atan, atan2, as_vector, min_value, max_value, FunctionSpace, \
     errornorm, zero
 from gusto import thermodynamics
 from gusto.logging import logger
@@ -34,7 +34,7 @@ def latlon_coords(mesh):
     unsafe = z0/sqrt(x0*x0 + y0*y0 + z0*z0)
     safe = min_value(max_value(unsafe, -1.0), 1.0)  # avoid silly roundoff errors
     theta = asin(safe)  # latitude
-    lamda = atan2(y0, x0)  # longitude
+    lamda = atan(y0 / x0)  # longitude
     return theta, lamda
 
 
