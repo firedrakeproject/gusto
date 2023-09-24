7e19757a492c026bb19579f16c40031b00001d5f
Tom Bendall <thomas.bendall@metoffice.gov.uk>
2023-09-24 22:01:09 +0100

    fix final issues to get spaces being created

--- gusto/function_spaces.py
+++ gusto/function_spaces.py
@@ -188,6 +188,8 @@ class Spaces(object):
         setattr(self, "L2", de_rham_complex.L2)
         # Register L2 space as DG also
         setattr(self, "DG", de_rham_complex.L2)
+        if hasattr(de_rham_complex, 'theta'):
+         setattr(self, "theta", de_rham_complex.theta)
 
     def build_dg1_equispaced(self):
         """
