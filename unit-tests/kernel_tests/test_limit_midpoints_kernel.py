248c46e3c5386edffb14ec06b0818760ba4e96cd
Tom Bendall <thomas.bendall@metoffice.gov.uk>
2023-11-17 17:46:17 +0000

    fix Vtheta limiter for new DoF ordering

--- unit-tests/kernel_tests/test_limit_midpoints_kernel.py
+++ unit-tests/kernel_tests/test_limit_midpoints_kernel.py
@@ -62,6 +62,8 @@ def test_limit_midpoints(profile):
     init_field.interpolate(init_expr)
     DG1_field.interpolate(DG_expr)
 
+    import pdb; pdb.set_trace()
+
     # ------------------------------------------------------------------------ #
     # Apply kernel
     # ------------------------------------------------------------------------ #
