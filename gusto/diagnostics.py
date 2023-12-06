3a7942d89d2ab365045e9f32f081d97d6f83c8ec
Tim Andrews <ta440@github.com>
2023-12-06 21:36:31 +0000

    revert diagnostics file

--- gusto/diagnostics.py
+++ gusto/diagnostics.py
@@ -73,7 +73,7 @@ class Diagnostics(object):
         fmin = op2.Global(1, np.finfo(float).max, dtype=float, comm=f._comm)
         op2.par_loop(op2.Kernel("""
 static void minify(double *a, double *b) {
-    a[0] = a[0] > b[0] ? b[0] : a[0];
+    a[0] = a[0] > fabs(b[0]) ? fabs(b[0]) : a[0];
 }
 """, "minify"), f.dof_dset.set, fmin(op2.MIN), f.dat(op2.READ))
         return fmin.data[0]
@@ -91,7 +91,7 @@ static void minify(double *a, double *b) {
         fmax = op2.Global(1, np.finfo(float).min, dtype=float, comm=f._comm)
         op2.par_loop(op2.Kernel("""
 static void maxify(double *a, double *b) {
-    a[0] = a[0] < b[0] ? b[0] : a[0];
+    a[0] = a[0] < fabs(b[0]) ? fabs(b[0]) : a[0];
 }
 """, "maxify"), f.dof_dset.set, fmax(op2.MAX), f.dat(op2.READ))
         return fmax.data[0]
