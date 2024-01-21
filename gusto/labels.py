ef4582ff4cf910223e8fa7a70864189033ce72b3
Tom Bendall <thomas.bendall@metoffice.gov.uk>
2024-01-21 15:50:23 +0000

    remove mass weighted label

--- gusto/labels.py
+++ gusto/labels.py
@@ -90,7 +90,6 @@ class PhysicsLabel(Label):
 # ---------------------------------------------------------------------------- #
 
 time_derivative = Label("time_derivative")
-mass_weighted = Label("mass_weighted", validator=lambda value: type(value) == str)
 implicit = Label("implicit")
 explicit = Label("explicit")
 transport = Label("transport", validator=lambda value: type(value) == TransportEquationType)
