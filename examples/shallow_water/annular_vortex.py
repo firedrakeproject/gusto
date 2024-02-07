5ff9619ffb9395c78cbe5a7aab412ed02f42327b
12shughes <sh1293@exeter.ac.uk>
2024-02-07 14:30:20 +0000

    start including the initial condition solving code

--- examples/shallow_water/annular_vortex.py
+++ examples/shallow_water/annular_vortex.py
@@ -4,7 +4,7 @@ Set up Martian annular vortex experiment!
 
 from gusto import *
 from firedrake import (IcosahedralSphereMesh, SpatialCoordinate,
-                       as_vector, pi, sqrt, min_value)
+                       as_vector, pi, sqrt, min_value, sin, cos)
 
 # ---------------------------------------------------------------------------- #
 # Test case parameters
@@ -18,12 +18,13 @@ tmax = day
 dt = 450.
 
 # setup shallow water parameters
-R = 6371220.    ### radius of Earth - change for Mars!
-H = 5960.       ### probably this changes too!
+#R = 6371220.    ### radius of Earth - change for Mars!
+R = 3389500.    # Mars value (3389500)
+#H = 5960.      ### probably this changes too!
+H = 17000.      # Will's Mars value
 
 ### setup shallow water parameters - can also change g and Omega as required
 parameters = ShallowWaterParameters(H=H)
-
 # ------------------------------------------------------------------------ #
 # Set up model objects
 # ------------------------------------------------------------------------ #
@@ -35,7 +36,8 @@ x = SpatialCoordinate(mesh)
 domain = Domain(mesh, dt, 'BDM', 1)
 
 # Equation, including mountain given by bexpr
-Omega = parameters.Omega
+#Omega = parameters.Omega
+Omega = 2*pi/88774
 fexpr = 2*Omega*x[2]/R
 lamda, theta, _ = lonlatr_from_xyz(x[0], x[1], x[2])
 R0 = pi/9.
@@ -47,10 +49,11 @@ thsq = (theta - theta_c)**2
 rsq = min_value(R0sq, lsq+thsq)
 r = sqrt(rsq)
 bexpr = 2000 * (1 - r/R0)
+#bexpr = 0.
 eqns = ShallowWaterEquations(domain, parameters, fexpr=fexpr, bexpr=bexpr)
 
 # I/O (input/output)
-dirname = "annular_vortex"
+dirname = "annular_vortex_mars_no_u"
 output = OutputParameters(dirname=dirname)
 diagnostic_fields = [Sum('D', 'topography')]
 io = IO(domain, output, diagnostic_fields=diagnostic_fields)
@@ -69,11 +72,14 @@ stepper = SemiImplicitQuasiNewton(eqns, io, transported_fields, transport_method
 
 u0 = stepper.fields('u')
 D0 = stepper.fields('D')
-u_max = 20.   # Maximum amplitude of the zonal wind (m/s)
+#u_max = 20.   # Maximum amplitude of the zonal wind (m/s)
+u_max = 0.1
 uexpr = as_vector([-u_max*x[1]/R, u_max*x[0]/R, 0.0])
-g = parameters.g
+g = 3.71
+#g = parameters.g
 Rsq = R**2
 Dexpr = H - ((R * Omega * u_max + 0.5*u_max**2)*x[2]**2/Rsq)/g - bexpr
+#Dexpr = 0.
 
 u0.project(uexpr)
 D0.interpolate(Dexpr)
@@ -86,3 +92,71 @@ stepper.set_reference_profiles([('D', Dbar)])
 # ------------------------------------------------------------------------ #
 
 stepper.run(t=0, tmax=tmax)
+
+
+# this bit is solving for the initial conditions
+
+# need to setup totintegral to be an integral in latitude from -pi/2 to +pi/2
+# and cumulintegral to be an integral in latitude from -pi/2 to current latitude
+
+twomega = 2 * Omega
+twomegarad = twomega * radius
+
+alpha = 0.5
+
+hbart = 1
+phibar = H
+
+lat = theta
+sinlat = sin(lat)
+coslat = cos(lat)
+
+# need to find the correct weighting function for the integrals as lat not necessarily equally spaced
+da = coslat * pi
+
+
+
+f = twomega * sinlat
+
+# not sure if this works in gusto?
+hn = [1] * len(lat)
+
+rlat1 = pi / 180. * 45.
+rlat2 = pi / 180. * 50.
+qp = twomega / hbart
+qt0 = twomega * sinlat / hbart
+qt = qt0
+
+
+# not really sure what this conditional bit is meant to be doing
+qt = condit(lat > 0., 0.3*qp,
+        condit(lat > rlat1, 1.6*qp,
+            condit(lat > rlat2, qp, qt0)))
+
+# iteration loop
+k=0
+while k<50:
+    ctop = totintegral(qt*hn*da)
+    cbot = totintegral(hn*da)
+
+    coff = -ctop/cbot
+
+    if k >= 1:
+        zn0 = zn
+    zn = (coff + qt) * hn - f
+    if k >= 1:
+        zn = alpha * zn + (1 - alpha) * zn0
+    
+    un = -cumulintegral(zn * da) * R / coslat
+
+    dhdmu = -(un / coslat + twomegarad) * un * sinlat / (coslat * phibar)
+
+    hn = cumulintegral(dhdmu * da)
+
+    havgn = totintegral(hn * da)/totintegral(da)
+    hn = hn - havgn + hbart
+
+    k += 1
+
+    qn = (f + zn)/hn -coff
+    error = totintegral(sqrt((qn - qt)**2))
