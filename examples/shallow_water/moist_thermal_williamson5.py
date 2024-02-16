50fbb956ad70e693c10ab93f094240c42924ddb8
nhartney <nh491@exeter.ac.uk>
2024-02-16 11:51:13 +0000

    include missing spatial discretisation in moist thermal example

--- examples/shallow_water/moist_thermal_williamson5.py
+++ examples/shallow_water/moist_thermal_williamson5.py
@@ -104,8 +104,10 @@ SWSaturationAdjustment(eqns, sat_func, time_varying_saturation=True,
 InstantRain(eqns, qprecip, vapour_name="cloud_water", rain_name="rain",
             gamma_r=gamma_r)
 
+transport_methods = [DGUpwind(eqns, field_name) for field_name in eqns.field_names]
+
 # Timestepper
-stepper = Timestepper(eqns, RK4(domain), io)
+stepper = Timestepper(eqns, RK4(domain), io, spatial_methods=transport_methods)
 
 # ----------------------------------------------------------------- #
 # Initial conditions
