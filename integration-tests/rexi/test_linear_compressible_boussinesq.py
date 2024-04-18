39a6db6699b0d00d40adc1559c783d9ba5fda0b1
jshipton <j.shipton@exeter.ac.uk>
2024-04-18 14:11:08 +0100

    fix rexi compressible boussinesq test

--- integration-tests/rexi/test_linear_compressible_boussinesq.py
+++ integration-tests/rexi/test_linear_compressible_boussinesq.py
@@ -7,7 +7,7 @@ REXI applied to the compressible Boussinesq equations.
 from os.path import join, abspath, dirname
 from gusto import *
 from firedrake import (PeriodicIntervalMesh, ExtrudedMesh,
-                       sin, SpatialCoordinate, Function, pi)
+                       exp, SpatialCoordinate, Function, pi)
 from firedrake.output import VTKFile
 
 
@@ -17,16 +17,16 @@ def run_rexi_linear_boussinesq(tmpdir):
     # Set up model objects
     # ---------------------------------------------------------------------- #
     tmax = 1000
-    L = 1.0e3  # Domain length
-    H = 1.0e3  # Height position of the model top
+    Lx = 1.0e3  # Domain length
+    Lz = 1.0e3  # Height position of the model top
 
     columns = 10  # number of columns
     nlayers = 10  # horizontal layers
 
     # Domain
     mesh_name = 'linear_boussinesq_mesh'
-    m = PeriodicIntervalMesh(columns, L)
-    mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers,
+    m = PeriodicIntervalMesh(columns, Lx)
+    mesh = ExtrudedMesh(m, layers=nlayers, layer_height=Lz/nlayers,
                         name=mesh_name)
     domain = Domain(mesh, tmax, 'CG', 1)
 
@@ -40,7 +40,7 @@ def run_rexi_linear_boussinesq(tmpdir):
 
     U_in = Function(eqns.function_space, name="U_in")
     U_expl = Function(eqns.function_space, name="Uexpl")
-    u, p, b = U_in.subfunctions
+    _, p, b = U_in.subfunctions
 
     # spaces
     Vb = b.function_space()
@@ -55,11 +55,9 @@ def run_rexi_linear_boussinesq(tmpdir):
     # interpolate the expression to the function
     b_b = Function(Vb).interpolate(bref)
 
-    # setup constants
-    a = 5.0e3
-    deltab = 1.0e-2
-    b_pert = deltab*sin(pi*z/H)/(1 + (x - L/2)**2/a**2)
-    # interpolate the expression to the function
+    # Add perturbation
+    r = sqrt((x-Lx/2)**2 + (z-Lz/2)**2)
+    b_pert = 0.1*exp(-(r/(Lx/5)**2))
     b.interpolate(b_b + b_pert)
 
     p_b = Function(Vp)
@@ -77,15 +75,12 @@ def run_rexi_linear_boussinesq(tmpdir):
     # REXI output
     rexi_output = VTKFile(str(tmpdir)+"/sk_wave/rexi.pvd")
     u1, p1, b1 = U_expl.subfunctions
-    u1.assign(u1)
-    p1.assign(p1-p_b)
-    b1.assign(b1-b_b)
+    p1.assign(p)
+    b1.assign(b)
     rexi_output.write(u1, p1, b1)
     rexi = Rexi(eqns, RexiParameters())
     rexi.solve(U_expl, U_in, tmax)
     u1, p1, b1 = U_expl.subfunctions
-    p1 -= p_b
-    b1 -= b_b
     rexi_output.write(u1, p1, b1)
 
     # Checkpointing
@@ -96,9 +91,8 @@ def run_rexi_linear_boussinesq(tmpdir):
                                     checkpoint=True)
     check_mesh = pick_up_mesh(check_output, mesh_name)
     check_domain = Domain(check_mesh, tmax, 'CG', 1)
-    check_eqn = LinearBoussinesqEquations(domain, parameters)
-    check_io = IO(check_domain, output=check_output,
-                  diagnostic_fields=[Perturbation('b')])
+    check_eqn = LinearBoussinesqEquations(check_domain, parameters)
+    check_io = IO(check_domain, output=check_output)
     check_stepper = Timestepper(check_eqn, RK4(check_domain), check_io)
     check_stepper.io.pick_up_from_checkpoint(check_stepper.fields)
     usoln = check_stepper.fields("u")
@@ -113,19 +107,18 @@ def test_rexi_linear_boussinesq(tmpdir):
     dirname = str(tmpdir)
     u, p, b, uexpl, pexpl, bexpl = run_rexi_linear_boussinesq(dirname)
 
-    udiff_arr = uexpl.dat.data - usoln.dat.data
-    pdiff_arr = pexpl.dat.data - psoln.dat.data
-    bdiff_arr = bexpl.dat.data - bsoln.dat.data
+    udiff_arr = uexpl.dat.data - u.dat.data
+    pdiff_arr = pexpl.dat.data - p.dat.data
+    bdiff_arr = bexpl.dat.data - b.dat.data
 
-    uerror = np.linalg.norm(udiff_arr) / np.linalg.norm(usoln.dat.data)
-    perror = np.linalg.norm(pdiff_arr) / np.linalg.norm(psoln.dat.data)
-    berror = np.linalg.norm(bdiff_arr) / np.linalg.norm(bsoln.dat.data)
+    uerror = np.linalg.norm(udiff_arr) / np.linalg.norm(u.dat.data)
+    perror = np.linalg.norm(pdiff_arr) / np.linalg.norm(p.dat.data)
+    berror = np.linalg.norm(bdiff_arr) / np.linalg.norm(b.dat.data)
 
-    # Slack values chosen to be robust to different platforms
-    assert uerror < 1e-10, \
+    assert uerror < 1e-14, \
         'u values in REXI compressible boussinesq test do not match KGO values'
-    assert perror < 1e-10, \
+    assert perror < 1e-14, \
         'p values in REXI compressible boussinesq test do not match KGO values'
-    assert berror < 1e-10, \
+    assert berror < 1e-14, \
         'b values in REXI compressible boussinesq test do not match KGO values'
 
