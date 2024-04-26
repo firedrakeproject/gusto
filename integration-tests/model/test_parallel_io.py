80f903bac8bf765cccc360855abb6d05f3adeb7d
jshipton <j.shipton@exeter.ac.uk>
2024-04-26 16:56:35 +0100

    minor test fix

--- integration-tests/model/test_parallel_io.py
+++ integration-tests/model/test_parallel_io.py
@@ -5,14 +5,14 @@ import pytest
 
 @pytest.mark.parallel(nprocs=4)
 @pytest.mark.parametrize("spatial_parallelism", [True, False])
-def test_parallel_io(tmpdir):
+def test_parallel_io(tmpdir, spatial_parallelism):
 
     if spatial_parallelism:
         ensemble = Ensemble(COMM_WORLD, 2)
     else:
         ensemble = Ensemble(COMM_WORLD, 1)
 
-    mesh = PeriodicUnitSquareMesh(10, comm=ensemble.comm)
+    mesh = PeriodicUnitSquareMesh(10, 10, comm=ensemble.comm)
     dt = 0.1
     domain = Domain(mesh, dt, "BDM", 1)
 
