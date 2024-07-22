6d7324d4f6c0362dd6932a745091f71ce9251b2b
Tim Andrews <ta440@github.com>
2024-07-22 17:34:31 +0100

    determine if it is a scalar or vector velocity by function space shape

--- gusto/diagnostics.py
+++ gusto/diagnostics.py
@@ -333,7 +333,7 @@ class CourantNumber(DiagnosticField):
         n = FacetNormal(domain.mesh)
 
         # Check if the velocity is a vector or scalar field
-        if domain.mesh.geometric_dimension() == 1:
+        if len(u.function_space().shape) == 0:
             un = 0.5*(-u_expr * n[0] + abs(-u_expr * n[0]))
         else:
             un = 0.5*(inner(-u_expr, n) + abs(inner(-u_expr, n)))
