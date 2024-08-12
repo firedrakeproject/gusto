ac0a778578bb98f55a8e4314e4be1958365c293d
nhartney <nh491@exeter.ac.uk>
2024-08-12 13:14:47 +0100

    nu does not have a default

--- gusto/saturation_function.py
+++ gusto/saturation_function.py
@@ -1,7 +1,7 @@
 from firedrake import exp
 _all_ = ["compute_saturation"]
 
-def compute_saturation(q0, H, g, D, b, B=None, nu=20):
+def compute_saturation(q0, nu, H, g, D, b, B=None):
     if B is None:
         sat_expr = q0*H/(D) * exp(nu*(1-b/g))
     else:
