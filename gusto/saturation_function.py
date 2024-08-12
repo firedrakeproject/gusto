c58b1cf43413ac8c924feb4747ed039bcc95c0f6
nhartney <nh491@exeter.ac.uk>
2024-08-12 12:35:40 +0100

    make nu a parameter in the saturation function (20 is default)

--- gusto/saturation_function.py
+++ gusto/saturation_function.py
@@ -1,9 +1,9 @@
 from firedrake import exp
 _all_ = ["compute_saturation"]
 
-def compute_saturation(q0, H, g, D, b, B=None):
+def compute_saturation(q0, H, g, D, b, B=None, nu=20):
     if B is None:
-        sat_expr = q0*H/(D) * exp(20*(1-b/g))
+        sat_expr = q0*H/(D) * exp(nu*(1-b/g))
     else:
-        sat_expr = q0*H/(D+B) * exp(20*(1-b/g))
+        sat_expr = q0*H/(D+B) * exp(nu*(1-b/g))
     return sat_expr
