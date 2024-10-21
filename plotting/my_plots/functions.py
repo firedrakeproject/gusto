cba88cbb50de7ede67f554a500b3b10515bcdf6c
12shughes <sh1293@exeter.ac.uk>
2024-10-21 14:15:16 +0100

    add code using regrid files to pileus

---
+++ plotting/my_plots/functions.py
@@ -0,0 +1,51 @@
+import numpy as np
+import xarray as xr
+
+def scaled2_eddy_enstrophy(q, **kwargs):
+    '''
+    Calculate the eddy enstrophy from dataarray q
+    '''
+    latmin = kwargs.pop('latmin', 50)
+
+    q = q.where(q.lat >= latmin, drop = True)
+    # q = q.where(q.lon < 179.5, drop = True)
+    qbar = q.mean(dim = 'lon')
+    qbar = qbar.expand_dims({'lon':q.lon})
+
+    qprime = (q - qbar)
+
+    cos = np.cos(np.deg2rad(q.lat))
+
+    qp = qprime **2 * cos
+
+    Z = qp.sum(dim = 'lat').sum(dim = 'lon') / (cos.sum(dim = 'lat') * 2 * np.pi * q.mean(dim='lon').mean(dim='lat')**2)
+    
+    return Z
+
+
+def condensing_area(q):
+    q = q.where(q.lat >= 50, drop=True)
+    qbar = q.mean(dim='lat').mean(dim='lon')
+    frac = qbar.D_minus_H_rel_flag_less
+    return frac
+
+
+def delta_q(da, tmin, tmax):
+    da = da.where(da.lat >= 50, drop=True)
+    dabar = da.mean(dim='lon')
+    daave = dabar.where(dabar.time>=tmin, drop=True).where(dabar.time<=tmax, drop=True).mean(dim='time')
+    PVmax = np.max(daave.values)
+    latmax = np.max(daave.lat.values)
+    PVpole = daave.where(daave.lat==latmax, drop=True).values.item()
+    dq = PVmax - PVpole
+    return dq
+
+
+def delta_q_inst(da):
+    da = da.where(da.lat >= 50, drop=True)
+    daave = da.mean(dim='lon')
+    PVmax = daave.max(dim='lat')
+    latmax = np.max(daave.lat.values)
+    PVpole = daave.where(daave.lat==latmax, drop=True).mean(dim='lat')
+    dq = PVmax - PVpole
+    return dq
---
+++ plotting/my_plots/late_pv_profile.py
@@ -0,0 +1,29 @@
+import os
+import matplotlib.pyplot as plt
+import xarray as xr
+
+file = 'Relax_to_annulus/annular_vortex_mars_60-70_PVmax--1-8_PVpole--0-8_tau_r--2sol_A0-0-norel_len-300sols_tracer_tophat'
+
+sol_early = 100
+sol_late = 300
+
+path = f'/data/home/sh1293/results/{file}'
+
+if not os.path.exists(f'{path}/Plots/'):
+    os.makedirs(f'{path}/Plots/')
+
+ds = xr.open_dataset(f'{path}/regrid_output.nc')
+
+ds['sol'] = ds.time/88774
+
+ds_late = ds.where(ds.sol>=sol_early, drop=True).where(ds.sol<=sol_late, drop=True)
+
+ds_late_time_mean = ds_late.mean(dim='time')
+
+ds_late_time_zonal_mean = ds_late_time_mean.mean(dim='lon')
+
+fig, ax = plt.subplots(1, 1, figsize=(8,8))
+
+plot = ds_late_time_zonal_mean.PotentialVorticity.plot(ax=ax)
+
+plt.savefig(f'{path}/Plots/late_time_pv_profile_{sol_early}-{sol_late}sol.pdf')
