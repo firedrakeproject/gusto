9b2a5457933ab6e18f3b9a3f0e92eb55f11a1d3b
JHopeCollins <joshua.hope-collins13@imperial.ac.uk>
2024-11-04 16:01:35 +0000

    set constant_jacobian for mass matrices in SIQN forcing solvers

--- gusto/timestepping/semi_implicit_quasi_newton.py
+++ gusto/timestepping/semi_implicit_quasi_newton.py
@@ -531,11 +531,13 @@ class Forcing(object):
 
         # now we can set up the explicit and implicit problems
         explicit_forcing_problem = LinearVariationalProblem(
-            a.form, L_explicit.form, self.xF, bcs=bcs
+            a.form, L_explicit.form, self.xF, bcs=bcs,
+            constant_jacobian=True
         )
 
         implicit_forcing_problem = LinearVariationalProblem(
-            a.form, L_implicit.form, self.xF, bcs=bcs
+            a.form, L_implicit.form, self.xF, bcs=bcs,
+            constant_jacobian=True
         )
 
         self.solvers = {}
