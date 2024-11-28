eeab6e9d3c3a0c42cde39f169331cc6e38598ed1
atb1995 <alex.brown@metoffice.gov.uk>
2024-11-28 11:12:49 +0000

    Undo changes to IMEX_RK

--- gusto/time_discretisation/imex_runge_kutta.py
+++ gusto/time_discretisation/imex_runge_kutta.py
@@ -4,7 +4,7 @@ from firedrake import (Function, Constant, NonlinearVariationalProblem,
                        NonlinearVariationalSolver)
 from firedrake.fml import replace_subject, all_terms, drop
 from firedrake.utils import cached_property
-from gusto.core.labels import time_derivative, implicit, explicit, prognostic
+from gusto.core.labels import time_derivative, implicit, explicitW
 from gusto.time_discretisation.time_discretisation import (
     TimeDiscretisation, wrapper_apply
 )
@@ -236,42 +236,18 @@ class IMEXRungeKutta(TimeDiscretisation):
         self.x1.assign(x_in)
         self.x_out.assign(x_in)
         solver_list = self.solvers
-        u_form = self.residual.label_map(lambda t: t.get(prognostic) =='u',
-                                         map_if_false=drop)
-        rho_form = self.residual.label_map(lambda t: t.get(prognostic) =='rho',
-                                         map_if_false=drop)
-        theta_form = self.residual.label_map(lambda t: t.get(prognostic) =='theta',
-                                         map_if_false=drop)
-        water_vapour_form = self.residual.label_map(lambda t: t.get(prognostic) =='water_vapour',
-                                         map_if_false=drop)
-        cloud_water_form = self.residual.label_map(lambda t: t.get(prognostic) =='cloud_water',
-                                         map_if_false=drop)
-        # print("u_form")
-        # print(u_form.form.__str__())
-        # print("rho_form")
-        # print(rho_form.form.__str__())
-        # print("theta_form")
-        # print(theta_form.form.__str__())
-        # print("water_vapour_form")
-        # print(water_vapour_form.form.__str__())
-        # print("cloud_water_form")
-        # print(cloud_water_form.form.__str__())
 
         for stage in range(self.nStages):
             self.solver = solver_list[stage]
             # Set initial solver guess
             if (stage > 0):
                 self.x_out.assign(self.xs[stage-1])
-            for evaluate in self.evaluate_source:
-                evaluate(self.xs[stage-1], self.dt)
             self.solver.solve()
 
             # Apply limiter
             if self.limiter is not None:
                 self.limiter.apply(self.x_out)
             self.xs[stage].assign(self.x_out)
-        for evaluate in self.evaluate_source:
-                evaluate(self.xs[self.nStages-1], self.dt)
         self.final_solver.solve()
 
         # Apply limiter
