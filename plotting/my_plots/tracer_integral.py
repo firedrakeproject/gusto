correct the tracer integrals to include r**2 factor and be in radians

plotting/my_plots/tracer_integral.py
@@ -5,27 +5,38 @@ import os
 import numpy as np
 
 # file = 'Free_run/annular_vortex_mars_60-70_free_A0-0-norel_len-15sols_tracer_tophat'
-file = 'Relax_to_pole_and_CO2/annular_vortex_mars_60-70_tau_r--2sol_tau_c--0.01sol_beta--4-0_A0-0-norel_len-300sols_tracer_tophat-80'
+# file = 'Free_run/annular_vortex_mars_60-70_free_A0-0-norel_len-30sols_tracer_tophat-80_ref-5'
+# file = 'Relax_to_pole_and_CO2/annular_vortex_mars_60-70_tau_r--2sol_tau_c--0.01sol_beta--4-0_A0-0-norel_len-300sols_tracer_tophat-80'
+file = 'Relax_to_pole_and_CO2/annular_vortex_mars_60-70_tau_r--2sol_tau_c--0.01sol_beta--1-0_A0-0-norel_len-30sols_tracer_tophat-80_ref-5'
 
 lat_thresh = 72
 
 path = f'/data/home/sh1293/results/{file}'
 
-ds = xr.open_dataset(f'{path}/regrid_output.nc')
-tracer = ds.tracer
-integral_pole, latpole = fcs.tracer_integral(tracer, lat_thresh, 'pole')
-integral_eq, lateq = fcs.tracer_integral(tracer, lat_thresh, 'equator')
-integral_total = integral_eq + integral_pole
+radius = 3396000
 
-tracer['coslat'] = np.cos(tracer.lat * np.pi/180.)
-integrand = tracer * tracer.coslat
-true_total = integrand.integrate('lon').integrate('lat')
+# ds = xr.open_dataset(f'{path}/regrid_output.nc')
+# tracer = ds.tracer
+# integral_pole, latpole = fcs.tracer_integral(tracer, lat_thresh, 'pole')
+# integral_eq, lateq = fcs.tracer_integral(tracer, lat_thresh, 'equator')
+# integral_total = integral_eq + integral_pole
+
+# tracer['coslat'] = np.cos(tracer.lat * np.pi/180.)
+# integrand = tracer * tracer.coslat
+# integrand['lat'] = integrand.lat * np.pi/180.
+# integrand['lon'] = integrand.lon * np.pi/180.
+# true_total = integrand.integrate('lon').integrate('lat')
+
+diag = xr.open_dataset(f'{path}/diagnostics.nc', group='tracer')
+diag = diag.assign_coords(true_time=diag.time * 450)
+
 
 fig, ax = plt.subplots(1,1, figsize=(8,8))
-integral_pole.plot(ax=ax, label=f'poleward {latpole}')
-integral_eq.plot(ax=ax, label=f'equatorward {lateq}')
-integral_total.plot(ax=ax, label='total')
-true_total.plot(ax=ax, label='true total', linestyle='--')
+# integral_pole.plot(ax=ax, label=f'poleward {latpole}')
+# integral_eq.plot(ax=ax, label=f'equatorward {lateq}')
+# integral_total.plot(ax=ax, label='total')
+# true_total.plot(ax=ax, label='true total', linestyle='--')
+(diag['total']/radius**2).plot(ax=ax, x='true_time', label='diag total')
 plt.legend()
 
 if not os.path.exists(f'{path}/Plots'):