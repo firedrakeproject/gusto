fea795e9a2faf2a1852128c468928a30bbe1b8f8
atb1995 <alex.brown@metoffice.gov.uk>
2024-12-04 11:09:33 +0000

    New test added and passes

--- integration-tests/transport/test_supg_transport.py
+++ integration-tests/transport/test_supg_transport.py
@@ -12,6 +12,54 @@ def run(timestepper, tmax, f_end):
     timestepper.run(0, tmax)
     return norm(timestepper.fields("f") - f_end) / norm(f_end)
 
+def run_coupled(timestepper, tmax, f_end):
+    timestepper.run(0, tmax)
+    norm1 = norm(timestepper.fields("f1") - f_end) / norm(f_end)
+    norm2 = norm(timestepper.fields("f2") - f_end) / norm(f_end)
+    return norm1, norm2
+
+@pytest.mark.parametrize("scheme", ["ssprk", "implicit_midpoint"])
+def test_supg_transport_mixed_scalar(tmpdir, scheme, tracer_setup):
+    setup = tracer_setup(tmpdir, geometry="slice")
+    domain = setup.domain
+
+    V = domain.spaces("theta")
+    ibp = IntegrateByParts.TWICE
+
+    opts = SUPGOptions(ibp=ibp)
+
+    tracer1 = ActiveTracer(name='f1', space="theta",
+                            variable_type=TracerVariableType.mixing_ratio,
+                            transport_eqn=TransportEquationType.advective)
+    tracer2 = ActiveTracer(name='f2', space="theta",
+                            variable_type=TracerVariableType.mixing_ratio,
+                            transport_eqn=TransportEquationType.conservative)
+    tracers = [tracer1, tracer2]
+    Vu = domain.spaces("HDiv")
+    eqn = CoupledTransportEquation(domain, active_tracers=tracers, Vu=Vu)
+    opts = SUPGOptions(field_names=["f1", "f2"], term_labels=[transport], ibp=ibp)
+    transport_method = [DGUpwind(eqn, "f1", ibp=ibp), DGUpwind(eqn, "f2", ibp=ibp)]
+
+    if scheme == "ssprk":
+        transport_scheme = SSPRK3(domain, options=opts)
+    elif scheme == "implicit_midpoint":
+        transport_scheme = TrapeziumRule(domain, options=opts)
+
+    time_varying_velocity = False
+    timestepper = PrescribedTransport(
+        eqn, transport_scheme, setup.io, time_varying_velocity, transport_method
+    )
+
+    # Initial conditions
+    timestepper.fields("f1").interpolate(setup.f_init)
+    timestepper.fields("f2").interpolate(setup.f_init)
+    timestepper.fields("u").project(setup.uexpr)
+
+    error1, error2 = run_coupled(timestepper, setup.tmax, setup.f_end)
+    assert error1 < setup.tol, \
+        'The transport error for f1 is greater than the permitted tolerance'
+    assert error2 < setup.tol, \
+        'The transport error for f2 is greater than the permitted tolerance'
 
 @pytest.mark.parametrize("equation_form", ["advective", "continuity"])
 @pytest.mark.parametrize("scheme", ["ssprk", "implicit_midpoint"])
@@ -29,34 +77,32 @@ def test_supg_transport_scalar(tmpdir, equation_form, scheme, space,
         V = domain.spaces("theta")
         ibp = IntegrateByParts.TWICE
 
-    opts = SUPGOptions(ibp=ibp)
-
     if equation_form == "advective":
         eqn = AdvectionEquation(domain, V, "f")
     else:
         eqn = ContinuityEquation(domain, V, "f")
+
+    opts = SUPGOptions(ibp=ibp)
+    transport_method = DGUpwind(eqn, "f", ibp=ibp)
 
     if scheme == "ssprk":
         transport_scheme = SSPRK3(domain, options=opts)
     elif scheme == "implicit_midpoint":
         transport_scheme = TrapeziumRule(domain, options=opts)
 
-    transport_method = DGUpwind(eqn, "f", ibp=ibp)
     time_varying_velocity = False
     timestepper = PrescribedTransport(
         eqn, transport_scheme, setup.io, time_varying_velocity, transport_method
     )
 
-    # Initial conditions
     timestepper.fields("f").interpolate(setup.f_init)
     timestepper.fields("u").project(setup.uexpr)
-
     error = run(timestepper, setup.tmax, setup.f_end)
     assert error < setup.tol, \
         'The transport error is greater than the permitted tolerance'
 
 
-@pytest.mark.parametrize("equation_form", ["advective", "continuity", "coupled_transport"])
+@pytest.mark.parametrize("equation_form", ["advective", "continuity"])
 @pytest.mark.parametrize("scheme", ["ssprk", "implicit_midpoint"])
 @pytest.mark.parametrize("space", ["CG", "HDiv"])
 def test_supg_transport_vector(tmpdir, equation_form, scheme, space,
@@ -78,24 +124,11 @@ def test_supg_transport_vector(tmpdir, equation_form, scheme, space,
 
     if equation_form == "advective":
         eqn = AdvectionEquation(domain, V, "f")
-        opts = SUPGOptions(ibp=ibp)
-        transport_method = DGUpwind(eqn, "f", ibp=ibp)
-    elif equation_form == "continuity":
-        eqn = ContinuityEquation(domain, V, "f")
-        opts = SUPGOptions(ibp=ibp)
-        transport_method = DGUpwind(eqn, "f", ibp=ibp)
     else:
-        tracer1 = ActiveTracer(name='f1', space=space,
-                               variable_type=TracerVariableType.mixing_ratio,
-                               transport_eqn=TransportEquationType.advective)
-        tracer2 = ActiveTracer(name='f2', space="CG",
-                               variable_type=TracerVariableType.mixing_ratio,
-                               transport_eqn=TransportEquationType.conservative)
-        tracers = [tracer1, tracer2]
-        Vu = domain.spaces("HDiv")
-        eqn = CoupledTransportEquation(domain, active_tracers=tracers, Vu=Vu)
-        opts = SUPGOptions(field_names=["f1", "f2"], term_labels=[transport], ibp=ibp)
-        transport_method = [DGUpwind(eqn, "f1", ibp=ibp), DGUpwind(eqn, "f2", ibp=IntegrateByParts.NEVER)]
+        eqn = ContinuityEquation(domain, V, "f")
+
+    opts = SUPGOptions(ibp=ibp)
+    transport_method = DGUpwind(eqn, "f", ibp=ibp)
 
     if scheme == "ssprk":
         transport_scheme = SSPRK3(domain, options=opts)
@@ -108,30 +141,14 @@ def test_supg_transport_vector(tmpdir, equation_form, scheme, space,
     )
 
     # Initial conditions
-    if equation_form == "coupled_transport":
-        f1 = timestepper.fields("f1")
-        f2 = timestepper.fields("f2")
-        if space == "CG":
-            f1.interpolate(f_init)
-        else:
-            f1.project(f_init)
-        f2.interpolate(f_init)
+    f = timestepper.fields("f")
+    if space == "CG":
+        f.interpolate(f_init)
     else:
-        f = timestepper.fields("f")
-        if space == "CG":
-            f.interpolate(f_init)
-        else:
-            f.project(f_init)
+        f.project(f_init)
     timestepper.fields("u").project(setup.uexpr)
 
     f_end = as_vector([setup.f_end]*gdim)
-    if equation_form == "coupled_transport":
-        error1, error2 = run(timestepper, setup.tmax, setup.f_end)
-        assert error1 < setup.tol, \
-            'The transport error for rho_d is greater than the permitted tolerance'
-        assert error2 < setup.tol, \
-            'The transport error for m_X is greater than the permitted tolerance'
-    else:
-        error = run(timestepper, setup.tmax, f_end)
-        assert error < setup.tol, \
-            'The transport error is greater than the permitted tolerance'
+    error = run(timestepper, setup.tmax, f_end)
+    assert error < setup.tol, \
+        'The transport error is greater than the permitted tolerance'
