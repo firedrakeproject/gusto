e7abfa9ee3c6ea58ab47ce74e70032260b5f03d3
atb1995 <alex.brown@metoffice.gov.uk>
2025-01-17 10:32:08 +0000

    Parallelised version of SDC added, this works for the moist_bf test case

--- gusto/time_discretisation/sdc.py
+++ gusto/time_discretisation/sdc.py
@@ -57,7 +57,7 @@ from gusto.core.labels import (time_derivative, implicit, explicit, source_label
 
 from qmat import genQCoeffs, genQDeltaCoeffs
 
-__all__ = ["SDC"]
+__all__ = ["SDC", "Parallel_SDC"]
 
 
 class SDC(object, metaclass=ABCMeta):
@@ -510,5 +510,181 @@ class SDC(object, metaclass=ABCMeta):
             else:
                 # Take value at final quadrature node dtau_M
                 x_out.assign(self.Unodes[-1])
+        else:
+            x_out.assign(self.Unodes[-1])
+
+class Parallel_SDC(SDC):
+    """Class for Spectral Deferred Correction schemes."""
+
+    def __init__(self, base_scheme, domain, M, maxk, quad_type, node_type, qdelta_imp, qdelta_exp,
+                 field_name=None,
+                 linear_solver_parameters=None, nonlinear_solver_parameters=None, final_update=True,
+                 limiter=None, options=None, initial_guess="base", communicator=None):
+        """
+        Initialise SDC object
+        Args:
+            base_scheme (:class:`TimeDiscretisation`): Base time stepping scheme to get first guess of solution on
+                quadrature nodes.
+            domain (:class:`Domain`): the model's domain object, containing the
+                mesh and the compatible function spaces.
+            M (int): Number of quadrature nodes to compute spectral integration over
+            maxk (int): Max number of correction interations
+            quad_type (str): Type of quadrature to be used. Options are
+                GAUSS, RADAU-LEFT, RADAU-RIGHT and LOBATTO
+            node_type (str): Node type to be used. Options are
+                EQUID, LEGENDRE, CHEBY-1, CHEBY-2, CHEBY-3 and CHEBY-4
+            qdelta_imp (str): Implicit Qdelta matrix to be used. Options are
+                BE, LU, TRAP, EXACT, PIC, OPT, WEIRD, MIN-SR-NS, MIN-SR-S
+            qdelta_exp (str): Explicit Qdelta matrix to be used. Options are
+                FE, EXACT, PIC
+            formulation (str, optional): Whether to use node-to-node or zero-to-node
+                formulation. Options are N2N and Z2N. Defaults to N2N
+            field_name (str, optional): name of the field to be evolved.
+                Defaults to None.
+            linear_solver_parameters (dict, optional): dictionary of parameters to
+                pass to the underlying linear solver. Defaults to None.
+            nonlinear_solver_parameters (dict, optional): dictionary of parameters to
+                pass to the underlying nonlinear solver. Defaults to None.
+            final_update (bool, optional): Whether to compute final update, or just take last
+                quadrature value. Defaults to True
+            limiter (:class:`Limiter` object, optional): a limiter to apply to
+                the evolving field to enforce monotonicity. Defaults to None.
+            options (:class:`AdvectionOptions`, optional): an object containing
+                options to either be passed to the spatial discretisation, or
+                to control the "wrapper" methods, such as Embedded DG or a
+                recovery method. Defaults to None.
+            initial_guess (str, optional): Initial guess to be base timestepper, or copy
+        """
+        super().__init__(base_scheme, domain, M, maxk, quad_type, node_type, qdelta_imp, qdelta_exp,
+                         formulation = "Z2N", field_name=field_name,
+                         linear_solver_parameters=linear_solver_parameters, nonlinear_solver_parameters= nonlinear_solver_parameters,
+                         final_update=final_update,
+                         limiter=limiter, options=options, initial_guess=initial_guess)
+        self.comm = communicator
+
+    def compute_quad(self):
+        """
+        Computes integration of F(y) on quadrature nodes
+        """
+        x = Function(self.W)
+        for j in range(self.M):
+            x.assign(float(self.Q[j, self.comm.ensemble_comm.rank])*self.fUnodes[self.comm.ensemble_comm.rank])
+            self.comm.reduce(x, self.quad[j], root=j)
+
+    def compute_quad_final(self):
+        """
+        Computes final integration of F(y) on quadrature nodes
+        """
+        x = Function(self.W)
+        x.assign(float(self.Qfin[self.comm.ensemble_comm.rank])*self.fUnodes[self.comm.ensemble_comm.rank])
+        self.comm.allreduce(x, self.quad_final)
+
+
+    @wrapper_apply
+    def apply(self, x_out, x_in):
+        self.Un.assign(x_in)
+        self.U_start.assign(self.Un)
+        solver_list = self.solvers
+
+        # Compute initial guess on quadrature nodes with low-order
+        # base timestepper
+        self.Unodes[0].assign(self.Un)
+        if (self.base_flag):
+            for m in range(self.M):
+                self.base.dt = float(self.dtau[m])
+                self.base.apply(self.Unodes[m+1], self.Unodes[m])
+        else:
+            for m in range(self.M):
+                self.Unodes[m+1].assign(self.Un)
+        for m in range(self.M+1):
+            for evaluate in self.evaluate_source:
+                evaluate(self.Unodes[m], self.base.dt, x_out=self.source_Uk[m])
+
+        # Iterate through correction sweeps
+        k = 0
+        while k < self.maxk:
+            k += 1
+
+            if self.qdelta_imp_type == "MIN-SR-FLEX":
+                # Recompute Implicit Q_delta matrix for each iteration k
+                self.Qdelta_imp = genQDeltaCoeffs(
+                    self.qdelta_imp_type,
+                    form=self.formulation,
+                    nodes=self.nodes,
+                    Q=self.Q,
+                    nNodes=self.M,
+                    nodeType=self.node_type,
+                    quadType=self.quad_type,
+                    k=k
+                )
+
+            # Compute for N2N: sum(j=1,M) (s_mj*F(y_m^k) +  s_mj*S(y_m^k))
+            # for Z2N: sum(j=1,M) (q_mj*F(y_m^k) +  q_mj*S(y_m^k))
+            self.Uin.assign(self.Unodes[self.comm.ensemble_comm.rank+1])
+            # Include source terms
+            for evaluate in self.evaluate_source:
+                evaluate(self.Uin, self.base.dt, x_out=self.source_in)
+            self.solver_rhs.solve()
+            self.fUnodes[self.comm.ensemble_comm.rank].assign(self.Urhs)
+
+            self.compute_quad()
+
+            # Loop through quadrature nodes and solve
+            self.Unodes1[0].assign(self.Unodes[0])
+            for evaluate in self.evaluate_source:
+                evaluate(self.Unodes[0], self.base.dt, x_out=self.source_Uk[0])
+
+
+            # Set Q or S matrix
+            self.Q_.assign(self.quad[self.comm.ensemble_comm.rank])
+
+            # Set initial guess for solver, and pick correct solver
+            self.solver = solver_list[self.comm.ensemble_comm.rank]
+            self.U_SDC.assign(self.Unodes[self.comm.ensemble_comm.rank+1])
+
+            # Compute
+            # for N2N:
+            # y_m^(k+1) = y_(m-1)^(k+1) + dtau_m*(F(y_(m)^(k+1)) - F(y_(m)^k)
+            #             + S(y_(m-1)^(k+1)) - S(y_(m-1)^k))
+            #             + sum(j=1,M) s_mj*(F+S)(y^k)
+            # for Z2N:
+            # y_m^(k+1) = y^n + sum(j=1,m) Qdelta_imp[m,j]*(F(y_(m)^(k+1)) - F(y_(m)^k))
+            #             + sum(j=1,M)  Q_delta_exp[m,j]*(S(y_(m-1)^(k+1)) - S(y_(m-1)^k))
+            self.solver.solve()
+            self.Unodes1[self.comm.ensemble_comm.rank+1].assign(self.U_SDC)
+
+            # Evaluate source terms
+            for evaluate in self.evaluate_source:
+                evaluate(self.Unodes1[self.comm.ensemble_comm.rank+1], self.base.dt, x_out=self.source_Ukp1[self.comm.ensemble_comm.rank+1])
+
+            # Apply limiter if required
+            if self.limiter is not None:
+                self.limiter.apply(self.Unodes1[self.comm.ensemble_comm.rank+1])
+
+            self.Unodes[self.comm.ensemble_comm.rank+1].assign(self.Unodes1[self.comm.ensemble_comm.rank+1])
+            self.source_Uk[self.comm.ensemble_comm.rank+1].assign(self.source_Ukp1[self.comm.ensemble_comm.rank+1])
+
+        if self.maxk > 0:
+            # Compute value at dt rather than final quadrature node tau_M
+            if self.final_update:
+                self.Uin.assign(self.Unodes1[self.comm.ensemble_comm.rank+1])
+                self.source_in.assign(self.source_Ukp1[self.comm.ensemble_comm.rank+1])
+                self.solver_rhs.solve()
+                self.fUnodes[self.comm.ensemble_comm.rank].assign(self.Urhs)
+                self.compute_quad_final()
+                # Compute y_(n+1) = y_n + sum(j=1,M) q_j*F(y_j)
+                if self.comm.ensemble_comm.rank == self.M-1:
+                    self.U_fin.assign(self.Unodes[-1])
+                self.comm.bcast(self.U_fin, self.M -1)
+                self.solver_fin.solve()
+                # Apply limiter if required
+                if self.limiter is not None:
+                    self.limiter.apply(self.U_fin)
+                x_out.assign(self.U_fin)
+            else:
+                # Take value at final quadrature node dtau_M
+                if self.comm.ensemble_comm.rank == self.M-1:
+                    x_out.assign(self.Unodes[-1])
+                self.comm.bcast(x_out, self.M -1)
         else:
             x_out.assign(self.Unodes[-1])
