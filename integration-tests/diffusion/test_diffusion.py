f973f849574ab05fd7a7dbddaf7f58b7607c4050
jshipton <j.shipton@exeter.ac.uk>
2025-02-20 15:58:55 +0000

    fix vector diffusion

--- integration-tests/diffusion/test_diffusion.py
+++ integration-tests/diffusion/test_diffusion.py
@@ -4,7 +4,8 @@ the resulting field to ensure that the result is reasonable.
 """
 
 from gusto import *
-from firedrake import (VectorFunctionSpace, Constant, as_vector, errornorm)
+from firedrake import (VectorFunctionSpace, Constant, as_vector, errornorm,
+                       TensorFunctionSpace)
 import pytest
 
 
@@ -52,15 +53,17 @@ def test_vector_diffusion(tmpdir, DG, tracer_setup):
     f_init = setup.f_init
     tmax = setup.tmax
     tol = 3.e-2
-    kappa = 1.
 
     f_end_expr = (1/(1+4*tmax))*f_init**(1/(1+4*tmax))
 
-    kappa = Constant([[kappa, 0.], [0., kappa]])
+    VR = TensorFunctionSpace(domain.mesh, "R", 0)
+    kappa = Function(VR, val=((1., 0.), (0., 1.)))
+
     if DG:
         V = VectorFunctionSpace(domain.mesh, "DG", 1)
     else:
         V = domain.spaces("HDiv")
+
     f_init = as_vector([f_init, 0.])
     f_end_expr = as_vector([f_end_expr, 0.])
 
