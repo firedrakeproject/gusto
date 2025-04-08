a5e045a6e16ac318eb00db9bf291595df449aacb
Alex Brown <81297297+atb1995@users.noreply.github.com>
2025-04-08 15:34:41 +0100

    Horizontal & vertical splitting of advective transport terms, and a DG_Upwind version (#616)
    Co-authored-by: Thomas Bendall <thomas.bendall@metoffice.gov.uk>
    Co-authored-by: atb1995 <alex.brown1995@hotmail.co.uk>

--- gusto/spatial_methods/spatial_methods.py
+++ gusto/spatial_methods/spatial_methods.py
@@ -4,7 +4,7 @@ spatial discretisation of some term.
 """
 
 from firedrake import split
-from firedrake.fml import Term, keep, drop
+from firedrake.fml import Term, keep, drop, all_terms
 from gusto.core.labels import prognostic
 
 __all__ = ['SpatialMethod']
@@ -15,19 +15,19 @@ class SpatialMethod(object):
     The base object for describing a spatial discretisation of some term.
     """
 
-    def __init__(self, equation, variable, term_label):
+    def __init__(self, equation, variable, *term_labels):
         """
         Args:
             equation (:class:`PrognosticEquation`): the equation, which includes
                 the original type of this term.
             variable (str): name of the variable to set the method for
-            term_label (:class:`Label`): the label specifying which type of term
-                to be discretised.
+            term_labels (:class:`Label`): One or more labels specifying which type(s)
+                of terms should be discretized.
         """
         self.equation = equation
         self.variable = variable
         self.domain = self.equation.domain
-        self.term_label = term_label
+        self.term_labels = list(term_labels)
 
         if hasattr(equation, "field_names"):
             # Equation with multiple prognostic variables
@@ -38,14 +38,37 @@ class SpatialMethod(object):
             self.field = equation.X
             self.test = equation.test
 
-        # Find the original term to be used
-        self.original_form = equation.residual.label_map(
-            lambda t: t.has_label(term_label) and t.get(prognostic) == variable,
-            map_if_true=keep, map_if_false=drop)
-
-        num_terms = len(self.original_form.terms)
-        assert num_terms == 1, f'Unable to find {term_label.label} term ' \
-            + f'for {variable}. {num_terms} found'
+        if (len(self.term_labels) == 1):
+            # Most cases only have one term to be replaced
+            self.term_label = self.term_labels[0]
+            self.original_form = equation.residual.label_map(
+                lambda t: t.has_label(self.term_label) and t.get(prognostic) == variable,
+                map_if_true=keep,
+                map_if_false=drop
+            )
+            # Check that the original form has the correct number of terms
+            num_terms = len(self.original_form.terms)
+            assert num_terms == 1, f'Unable to find {self.term_label.label} term ' \
+                + f'for {variable}. {num_terms} found'
+        else:
+            # Multiple terms to be replaced. Find the original terms to be used
+            self.term_label = self.term_labels[0]
+            self.original_form = equation.residual.label_map(
+                all_terms,
+                map_if_true=drop
+            )
+            for term in self.term_labels:
+                original_form = equation.residual.label_map(
+                    lambda t: t.has_label(term) and t.get(prognostic) == variable,
+                    map_if_true=keep,
+                    map_if_false=drop
+                )
+                # Check that the original form has the correct number of terms
+                num_terms = len(original_form.terms)
+                assert num_terms == 1, f'Unable to find {term.label} term ' \
+                    + f'for {variable}. {num_terms} found'
+                # Add the terms form to original forms
+                self.original_form += original_form
 
     def replace_form(self, equation):
         """
