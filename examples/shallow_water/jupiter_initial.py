68120109cae92ca642195500feaa3981e5875256
12shughes <sh1293@exeter.ac.uk>
2025-08-04 14:18:15 +0100

    make suitable for restarting

--- examples/shallow_water/jupiter_initial.py
+++ examples/shallow_water/jupiter_initial.py
@@ -3,6 +3,8 @@ from firedrake import IcosahedralSphereMesh, SpatialCoordinate, as_vector, pi, i
 import scipy
 import numpy as np
 import time
+import os
+import shutil
 
 ### options changed in Cheng Li 2020
 Bu = 10
@@ -14,6 +16,11 @@ nx = 256
 ny = nx
 Lx = 7e7
 Ly = Lx
+rstar = Lx/2-3*Lx/nx
+
+### setup smoothing parameters
+smooth_degree = 5
+smooth_delta = 2
 
 # setup shallow water parameters
 # Bu = 10
@@ -33,27 +40,72 @@ H = phi0/g
 t_day = 2*pi/Omega
 
 ### timing options
+dump_freq = 15    # dump frequency of output
 dt = 250.          # timestep (in seconds)
-tmax = 50*t_day       # duration of the simulation (in seconds)
-dump_freq = 10    # dump frequency of output
+tmax = 500*t_day       # duration of the simulation (in seconds)
+
+restart = True
+restart_name = 'int_Bu10b1p5Rop23_l100dt250df15'
+t0 = 100*t_day
 
 ### vortex locations
-south_lat_deg = [90., 83., 83., 83., 83., 83., 70.]
-south_lon_deg = [0., 72., 144., 216., 288., 0., 0.]
+south_lat_deg = [90., 83., 83., 83., 83., 83.]#, 70.]
+south_lon_deg = [0., 72., 144., 216., 288., 0.]#, 0.]
 
 ### name
 # folder_name = 'jupiter_initial_multi'
-setup = 'intruder'
-bint, bdec = str(b).split('.')
-Roint, Rodec = str(Ro).split('.')
-if bint == '0':
-    bint = ''
-if Roint == '0':
-    Roint = ''
-folder_name = f'{setup}_Bu{Bu}b{bint}p{bdec}Ro{Roint}p{Rodec}_l{round(tmax/t_day)}dt{int(dt)}df{dump_freq}'
+setup = 'int'
+
+
+
+
+##########################################################################
+
+tmin = np.ceil(t0/dump_freq)*dump_freq
+tmax = np.ceil(tmax/dump_freq)*dump_freq
+
+def split_number(x):
+    x = float(x)
+    xint, xdec = str(x).split('.')
+    if xint == '0':
+        xint = ''
+    if xdec == '0':
+        xdec = ''
+    else:
+        xdec = f'p{xdec}'
+    return xint, xdec
+
+bint, bdec = split_number(b)
+Roint, Rodec = split_number(Ro)
+Buint, Budec = split_number(Bu)
+
+if setup != '':
+    setup = f'{setup}_'
+folder_name = f'{setup}Bu{Buint}{Budec}b{bint}{bdec}Ro{Roint}{Rodec}_l{round(tmax/t_day)}dt{int(dt)}df{dump_freq}'
+
+dirname=f'/data/home/sh1293/results/jupiter_sw/{folder_name}'
+dirnameold=f'/data/home/sh1293/results/jupiter_sw/{restart_name}'
+
+def create_restart_nc(dirname, dirnameold):#, groups):
+    if not os.path.exists(f'{dirname}/'):
+        os.makedirs(f'{dirname}')
+    shutil.copy(f'{dirnameold}/field_output.nc', f'{dirname}/field_output.nc')
+    # Paths to the original and target files
+    input_file = f'{dirnameold}/field_output.nc'
+    output_file = f'{dirname}/field_output.nc'
+    # new_groups(input_file, output_file, groups, 'D')
 
 # Set up the mesh
-mesh = PeriodicRectangleMesh(nx=nx, ny=ny, Lx=Lx, Ly=Ly, quadrilateral=True)
+if not restart:
+    mesh = PeriodicRectangleMesh(nx=nx, ny=ny, Lx=Lx, Ly=Ly, quadrilateral=True)
+    output = OutputParameters(dirname=f'/data/home/sh1293/results/jupiter_sw/{folder_name}', dumpfreq=dump_freq, dump_nc=True, checkpoint=True)
+elif restart:
+    create_restart_nc(dirname=dirname, dirnameold=dirnameold)
+    output = OutputParameters(dirname=dirname, dump_nc=True, dumpfreq=dump_freq, checkpoint=True, checkpoint_pickup_filename=f'{dirnameold}/chkpt.h5')
+    chkpt_mesh = pick_up_mesh(output, 'firedrake_default')
+    mesh = chkpt_mesh
+
+
 x, y = SpatialCoordinate(mesh)
 # x *= Lx
 # y *= Ly
@@ -198,6 +250,50 @@ def xy_from_rtheta(r, theta, angle_units='rad'):
 
     return x_shift, y_shift
 
+def smooth_f_profile(degree, delta, rstar=rstar, Omega=parameters.Omega, R=R, Lx=Lx, nx=nx):
+    import sympy as sp
+
+    delta *= Lx/nx
+    r = sp.symbols('r')
+    fexpr = 2*Omega*(1-0.5*r**2/R**2)
+    left_val = fexpr.subs(r, rstar-delta)
+    right_val = 2*Omega
+    left_diff_val = sp.diff(fexpr, r).subs(r, rstar-delta)
+    left_diff2_val = sp.diff(fexpr, r, 2).subs(r, rstar-delta)
+
+    a = sp.symbols(f'a_0:{degree+1}')
+    P = a[0]
+    for i in range(1, degree+1):
+        P += a[i]*r**i
+
+    if degree == 3:
+        eqns = [
+            P.subs(r, rstar-delta) - left_val,
+            P.subs(r, rstar+delta) - right_val,
+            sp.diff(P, r).subs(r, rstar-delta) - left_diff_val,
+            sp.diff(P, r).subs(r, rstar+delta)
+        ]
+    elif degree == 5:
+        eqns = [
+            P.subs(r, rstar-delta) - left_val,
+            P.subs(r, rstar+delta) - right_val,
+            sp.diff(P, r).subs(r, rstar-delta) - left_diff_val,
+            sp.diff(P, r).subs(r, rstar+delta),
+            sp.diff(P, r, 2).subs(r, rstar-delta) - left_diff2_val,
+            sp.diff(P, r, 2).subs(r, rstar+delta)
+        ]
+    else:
+        print('do not have BCs for this degree')
+
+    sol = sp.solve(eqns, a)
+    coeffs = [sol[sp.Symbol(f'a_{i}')] for i in range(degree+1)]
+    # P_smooth = P.subs(sol)
+    # f_smooth = sp.Piecewise(
+    #     (fexpr, r<rstar-delta),
+    #     (P_smooth, (rstar-delta<=r) & (r<=rstar+delta)),
+    #     (right_val, rstar+delta<r)
+    # )
+    return coeffs
 
 r, theta = rtheta_from_xy(x, y)
 
@@ -206,12 +302,19 @@ r, theta = rtheta_from_xy(x, y)
 # Create a spatially varying function for the Coriolis force:
 Omega = parameters.Omega
 fexpr = 2*Omega*(1-0.5*r**2/R**2)
-rstar = Lx/2
-ftrap = conditional(r < rstar, fexpr, 2*Omega)
+# ftrap = conditional(r < rstar, fexpr, 2*Omega)
+coeffs = smooth_f_profile(degree=smooth_degree, delta=smooth_delta)
+fsmooth = float(coeffs[0]) + float(coeffs[1])*r + float(coeffs[2])*r**2 + float(coeffs[3])*r**3
+if smooth_degree == 5:
+    fsmooth += float(coeffs[4])*r**4 + float(coeffs[5])*r**5
+
+ftrap1 = conditional(r<rstar-smooth_delta*Lx/nx, fexpr, fsmooth)
+ftrap = conditional(r<rstar+smooth_delta*Lx/nx, ftrap1, 2*Omega)
+
 eqns = ShallowWaterEquations(domain, parameters, fexpr=ftrap)
 logger.info(f'Estimated number of cores = {eqns.X.function_space().dim() / 50000} \n mpiexec -n nprocs python script.py')
 
-output = OutputParameters(dirname=f'/data/home/sh1293/results/jupiter_sw/{folder_name}', dumpfreq=dump_freq, dump_nc=True, checkpoint=True)
+# output = OutputParameters(dirname=f'/data/home/sh1293/results/jupiter_sw/{folder_name}', dumpfreq=dump_freq, dump_nc=True, checkpoint=True)
 
 # diagnostic_fields = [SteadyStateError('u'), SteadyStateError('D'),
 #                      RelativeVorticity(), PotentialVorticity(),
@@ -278,58 +381,60 @@ def initialise_D(X, idx):
     # return list of D values in correct order
     return D_values
 
-logger.info('Setting initial depth field')
-# get the function space from the field we would like to initialise (D0)
-VD = D0.function_space()
-# create a vector function space for the coordinate values with the
-# same mesh and finite element as D0
-W = VectorFunctionSpace(VD.mesh(), VD.ufl_element())
-# set up a function, X, with the value of the coordinates
-# corresponding to the DOFs of D0
-X = assemble(interpolate(VD.mesh().coordinates, W))
-# set up a temporary function with the same structure as D to hold the
-# values from each vortex
-Dtmp = Function(D0.function_space())
-Dfinal = Function(D0.function_space())
-# loop over vortices
-for idx in range(len(south_lat)):
-    # calculate depth perturbation for each vortex
-    # print('beginning initialise D')
-    Dtmp.dat.data[:] = initialise_D(X.dat.data_ro, idx)
-    # print('ending initialise D')
-    # add on to D0
-    Dfinal += Dtmp
 
-u_veloc = 0.*x
-v_veloc = 0.*y
-Dfinal += H
+if not restart:
+    logger.info('Setting initial depth field')
+    # get the function space from the field we would like to initialise (D0)
+    VD = D0.function_space()
+    # create a vector function space for the coordinate values with the
+    # same mesh and finite element as D0
+    W = VectorFunctionSpace(VD.mesh(), VD.ufl_element())
+    # set up a function, X, with the value of the coordinates
+    # corresponding to the DOFs of D0
+    X = assemble(interpolate(VD.mesh().coordinates, W))
+    # set up a temporary function with the same structure as D to hold the
+    # values from each vortex
+    Dtmp = Function(D0.function_space())
+    Dfinal = Function(D0.function_space())
+    # loop over vortices
+    for idx in range(len(south_lat)):
+        # calculate depth perturbation for each vortex
+        # print('beginning initialise D')
+        Dtmp.dat.data[:] = initialise_D(X.dat.data_ro, idx)
+        # print('ending initialise D')
+        # add on to D0
+        Dfinal += Dtmp
 
-logger.info('Setting initial velocity field')
-for i in range(len(south_lat)):
-    r_c, theta_c = rtheta_from_lonlat(south_lon[i], south_lat[i])
-    # logger.info(f'r is {r_c}, theta is {theta_c}')
-    x_c, y_c = xy_from_rtheta(r_c, theta_c)
-    # logger.info(f'x is {x_c}, y is {y_c}')
-    # print(f'(x, y) is ({x_c}, {y_c})')
-    dr = sqrt((x-x_c)**2 + (y-y_c)**2)
+    u_veloc = 0.*x
+    v_veloc = 0.*y
+    Dfinal += H
 
-    # Overide u,v components in velocity field
-    # u_veloc += - vm * ( r / rm ) * exp( (1/b) * ( 1 - ( r / rm )**b ) ) * ( (y_grid-yy) / ( r + 1e-16 ) )
-    # v_veloc += vm * ( r / rm ) * exp( (1/b) * ( 1 - ( r / rm )**b ) ) * ( (x_grid-xx) / ( r + 1e-16 ) )
+    logger.info('Setting initial velocity field')
+    for i in range(len(south_lat)):
+        r_c, theta_c = rtheta_from_lonlat(south_lon[i], south_lat[i])
+        # logger.info(f'r is {r_c}, theta is {theta_c}')
+        x_c, y_c = xy_from_rtheta(r_c, theta_c)
+        # logger.info(f'x is {x_c}, y is {y_c}')
+        # print(f'(x, y) is ({x_c}, {y_c})')
+        dr = sqrt((x-x_c)**2 + (y-y_c)**2)
 
-    mag_veloc = vm * ( dr / rm ) * exp( (1/b) * ( 1 - ( dr / rm )**b ) )
-    # logger.info(f'mag_veloc is {mag_veloc}')
-    dx = x - x_c
-    dy = y - y_c
-    dl = sqrt(dx**2 + dy**2)
+        # Overide u,v components in velocity field
+        # u_veloc += - vm * ( r / rm ) * exp( (1/b) * ( 1 - ( r / rm )**b ) ) * ( (y_grid-yy) / ( r + 1e-16 ) )
+        # v_veloc += vm * ( r / rm ) * exp( (1/b) * ( 1 - ( r / rm )**b ) ) * ( (x_grid-xx) / ( r + 1e-16 ) )
 
-    u_veloc += - mag_veloc * (dy / dl)
-    v_veloc += mag_veloc * (dx / dl)
+        mag_veloc = vm * ( dr / rm ) * exp( (1/b) * ( 1 - ( dr / rm )**b ) )
+        # logger.info(f'mag_veloc is {mag_veloc}')
+        dx = x - x_c
+        dy = y - y_c
+        dl = sqrt(dx**2 + dy**2)
 
-uexpr = as_vector([u_veloc, v_veloc])
+        u_veloc += - mag_veloc * (dy / dl)
+        v_veloc += mag_veloc * (dx / dl)
 
-u0.project(uexpr)
-D0.interpolate(Dfinal)
+    uexpr = as_vector([u_veloc, v_veloc])
+
+    u0.project(uexpr)
+    D0.interpolate(Dfinal)
 
 Dbar = Function(D0.function_space()).assign(H)
 stepper.set_reference_profiles([('D', Dbar)])
@@ -337,9 +442,12 @@ stepper.set_reference_profiles([('D', Dbar)])
 start_time = time.time()
 
 # Run the timestepper and generate the output.
-stepper.run(t=0, tmax=tmax)
+if not restart:
+    stepper.run(t=0, tmax=tmax)
+elif restart:
+    stepper.run(t=tmin, tmax=tmax, pick_up=True)
 
 end_time = time.time()
 
 logger.info(f'Total time taken {(end_time-start_time):.2f} seconds')
-logger.info(f'File produced:\njupiter_sw/{folder_name}')
+logger.info(f'File produced:\n{folder_name}')
