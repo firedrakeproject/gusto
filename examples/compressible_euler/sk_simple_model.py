1f2ed7ce891d12dca87f415685b6a9a44ce56b49
jshipton <j.shipton@exeter.ac.uk>
2026-01-12 15:09:40 +0000

    SIQN model class for compressible Euler equations and simple example script

---
+++ examples/compressible_euler/sk_simple_model.py
@@ -0,0 +1,180 @@
+"""
+This example uses the non-linear compressible Euler equations to solve the
+vertical slice gravity wave test case of Skamarock and Klemp, 1994:
+``Efficiency and Accuracy of the Klemp-Wilhelmson Time-Splitting Technique'',
+MWR.
+
+The domain is smaller than the "hydrostatic" gravity wave test, so that there
+is difference between the hydrostatic and non-hydrostatic solutions. The test
+can be run with and without a hydrostatic switch.
+
+Potential temperature is transported using SUPG, and the degree 1 elements are
+used.
+"""
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+from firedrake import (
+    as_vector, SpatialCoordinate, PeriodicIntervalMesh, ExtrudedMesh, exp, sin,
+    Function, pi
+)
+from gusto import (
+    Perturbation, CompressibleParameters, OutputParameters,
+    CompressibleEulerEquations, SIQNModel,
+    compressible_hydrostatic_balance
+)
+
+skamarock_klemp_nonhydrostatic_defaults = {
+    'ncolumns': 150,
+    'nlayers': 10,
+    'dt': 6.0,
+    'tmax': 3000.,
+    'dumpfreq': 250,
+    'dirname': 'skamarock_klemp_nonhydrostatic',
+}
+
+
+def skamarock_klemp_nonhydrostatic(
+        ncolumns=skamarock_klemp_nonhydrostatic_defaults['ncolumns'],
+        nlayers=skamarock_klemp_nonhydrostatic_defaults['nlayers'],
+        dt=skamarock_klemp_nonhydrostatic_defaults['dt'],
+        tmax=skamarock_klemp_nonhydrostatic_defaults['tmax'],
+        dumpfreq=skamarock_klemp_nonhydrostatic_defaults['dumpfreq'],
+        dirname=skamarock_klemp_nonhydrostatic_defaults['dirname'],
+):
+
+    # ------------------------------------------------------------------------ #
+    # Test case parameters
+    # ------------------------------------------------------------------------ #
+
+    domain_width = 3.0e5      # Width of domain (m)
+    domain_height = 1.0e4     # Height of domain (m)
+    Tsurf = 300.              # Temperature at surface (K)
+    wind_initial = 20.        # Initial wind in x direction (m/s)
+    pert_width = 5.0e3        # Width parameter of perturbation (m)
+    deltaTheta = 1.0e-2       # Magnitude of theta perturbation (K)
+    N = 0.01                  # Brunt-Vaisala frequency (1/s)
+
+    # ------------------------------------------------------------------------ #
+    # Our settings for this set up
+    # ------------------------------------------------------------------------ #
+
+    element_order = 1
+
+    # ------------------------------------------------------------------------ #
+    # Set up model objects
+    # ------------------------------------------------------------------------ #
+
+    # Domain -- 3D volume mesh
+    base_mesh = PeriodicIntervalMesh(ncolumns, domain_width)
+    mesh = ExtrudedMesh(base_mesh, nlayers, layer_height=domain_height/nlayers)
+
+    # Equation
+    parameters = CompressibleParameters(mesh)
+    eqns = CompressibleEulerEquations
+
+    output = OutputParameters(
+        dirname=dirname, dumpfreq=dumpfreq, pddumpfreq=dumpfreq,
+        dump_vtus=False, dump_nc=True,
+    )
+
+    model = SIQNModel(mesh, dt, parameters, eqns, output, family="CG",
+                      element_order=element_order)
+
+    diagnostic_fields = [Perturbation('theta')]
+    model.setup(diagnostic_fields)
+
+    # ------------------------------------------------------------------------ #
+    # Initial conditions
+    # ------------------------------------------------------------------------ #
+
+    stepper = model.stepper
+    u0 = stepper.fields("u")
+    rho0 = stepper.fields("rho")
+    theta0 = stepper.fields("theta")
+
+    # spaces
+    domain = model.domain
+    Vt = domain.spaces("theta")
+    Vr = domain.spaces("DG")
+
+    # Thermodynamic constants required for setting initial conditions
+    # and reference profiles
+    g = parameters.g
+
+    x, z = SpatialCoordinate(mesh)
+
+    # N^2 = (g/theta)dtheta/dz => dtheta/dz = theta N^2g => theta=theta_0exp(N^2gz)
+    thetab = Tsurf*exp(N**2*z/g)
+
+    theta_b = Function(Vt).interpolate(thetab)
+    rho_b = Function(Vr)
+
+    # Calculate hydrostatic exner
+    compressible_hydrostatic_balance(model.equation, theta_b, rho_b)
+
+    theta_pert = (
+        deltaTheta * sin(pi*z/domain_height)
+        / (1 + (x - domain_width/2)**2 / pert_width**2)
+    )
+    theta0.interpolate(theta_b + theta_pert)
+    rho0.assign(rho_b)
+    u0.project(as_vector([wind_initial, 0.0]))
+
+    stepper.set_reference_profiles([('rho', rho_b), ('theta', theta_b)])
+
+    # ------------------------------------------------------------------------ #
+    # Run
+    # ------------------------------------------------------------------------ #
+
+    stepper.run(t=0, tmax=tmax)
+
+# ---------------------------------------------------------------------------- #
+# MAIN
+# ---------------------------------------------------------------------------- #
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(
+        description=__doc__,
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        '--ncolumns',
+        help="The number of columns in the vertical slice mesh.",
+        type=int,
+        default=skamarock_klemp_nonhydrostatic_defaults['ncolumns']
+    )
+    parser.add_argument(
+        '--nlayers',
+        help="The number of layers for the mesh.",
+        type=int,
+        default=skamarock_klemp_nonhydrostatic_defaults['nlayers']
+    )
+    parser.add_argument(
+        '--dt',
+        help="The time step in seconds.",
+        type=float,
+        default=skamarock_klemp_nonhydrostatic_defaults['dt']
+    )
+    parser.add_argument(
+        "--tmax",
+        help="The end time for the simulation in seconds.",
+        type=float,
+        default=skamarock_klemp_nonhydrostatic_defaults['tmax']
+    )
+    parser.add_argument(
+        '--dumpfreq',
+        help="The frequency at which to dump field output.",
+        type=int,
+        default=skamarock_klemp_nonhydrostatic_defaults['dumpfreq']
+    )
+    parser.add_argument(
+        '--dirname',
+        help="The name of the directory to write to.",
+        type=str,
+        default=skamarock_klemp_nonhydrostatic_defaults['dirname']
+    )
+    args, unknown = parser.parse_known_args()
+
+    skamarock_klemp_nonhydrostatic(**vars(args))
---
+++ gusto/model/model.py
@@ -0,0 +1,113 @@
+from abc import ABCMeta, abstractmethod
+from gusto.core import (Domain, IO, EmbeddedDGOptions)
+from gusto.spatial_methods import DGUpwind
+from gusto.time_discretisation import SSPRK3, RungeKuttaFormulation
+from gusto.timestepping import SemiImplicitQuasiNewton
+
+
+class ModelBase(object, metaclass=ABCMeta):
+    """Base model class."""
+
+    def __init__(self, mesh, dt, parameters, equation, output,
+                 family=None, element_order=None,
+                 no_normal_flow_bc_ids=None):
+        """
+        Args:
+            mesh (:class:`Mesh`): the model's mesh.
+            dt (float): the model timestep.
+            parameters (:class:`EquationParameters`): class storing the model
+                equation parameters.
+            equation (:class:`PrognosticEquationSet`): defines the model's
+                prognostic equation
+            output (:class:`OutputParameters`): provides parameters
+                controlling output
+            family (str, optional): the finite element space family used for
+                the velocity field. This determines the other finite element
+                spaces used via the de Rham complex. If not provided, an
+                appropriate choice will be made based on the cell of the mesh
+                (or the base mesh in 3D).
+            element_order (int): the element degree used for the DG
+                space. Defaults to None.
+            no_normal_flow_bc_ids (list, optional): a list of IDs of domain
+                boundaries at which no normal flow will be enforced. Defaults to
+                None.
+        """
+        # if HDiv finite element family not provided figure out a
+        # sensible default based on the cell shape of the mesh
+        if family is None:
+            extruded_mesh = hasattr(mesh, "_base_mesh")
+            if extruded_mesh:
+                cell = mesh._base_mesh.ufl_cell().cellname()
+            else:
+                cell = mesh.ufl_cell().cellname()
+            if cell == "interval":
+                family = "CG"
+            elif cell == "quadrilateral":
+                family = "RTCF"
+            elif cell == "triangle":
+                family = "BDM"
+            else:
+                raise ValueError(f"The mesh provided (or its base mesh if 3D) must have cells of type interval, quadrilateral or triangle, not {cell}.")
+
+        # create domain
+        self.domain = Domain(mesh, dt, family, element_order)
+
+        # set up prognostic equations
+        self.equation = equation(self.domain, parameters,
+                                 no_normal_flow_bc_ids=no_normal_flow_bc_ids)
+
+        # save output options for when IO is set up later
+        self.output = output
+
+    @abstractmethod
+    def setup(self):
+        # Set up the model. Must be implemented in the child class.
+        pass
+
+    def run(self, t, tmax, pick_up=False):
+        """
+        Runs the model for the specified time, from t to tmax
+
+        Args:
+            t (float): the start time of the run
+            tmax (float): the end time of the run
+            pick_up: (bool): specify whether to pick_up from a previous run
+        """
+        self.stepper.run(t=t, tmax=tmax, pick_up=pick_up)
+
+
+class SIQNModel(ModelBase):
+
+    @property
+    def transported_fields(self):
+        theta_opts = EmbeddedDGOptions()
+        transported_fields = [
+            SSPRK3(self.domain, "u"),
+            SSPRK3(
+                self.domain, "rho",
+                rk_formulation=RungeKuttaFormulation.linear
+            ),
+            SSPRK3(
+                self.domain, "theta",
+                options=theta_opts
+            )
+        ]
+        return transported_fields
+
+    @property
+    def transport_methods(self):
+        transport_methods = [
+            DGUpwind(self.equation, "u"),
+            DGUpwind(self.equation, "rho", advective_then_flux=True),
+            DGUpwind(self.equation, "theta")
+        ]
+        return transport_methods
+
+    def setup(self, diagnostic_fields):
+
+        io = IO(self.domain, self.output, diagnostic_fields=diagnostic_fields)
+        self.stepper = SemiImplicitQuasiNewton(
+            self.equation, io, self.transported_fields,
+            spatial_methods=self.transport_methods,
+            tau_values={'rho': 1.0, 'theta': 1.0}
+        )
