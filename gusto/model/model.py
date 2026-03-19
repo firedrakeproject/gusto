f84626f1530b2fec0470bc27e7beae80c533e4cd
Witt-D <91120222+Witt-D@users.noreply.github.com.>
2026-03-19 12:17:42 +0000

    initial tr-bdf2 model

--- gusto/model/model.py
+++ gusto/model/model.py
@@ -1,10 +1,11 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
+from firedrake import sqrt
 from gusto.core import (Domain, IO, EmbeddedDGOptions)
 from gusto.recovery import BoundaryMethod, RecoverySpaces
 from gusto.spatial_methods import DGUpwind, InteriorPenaltyDiffusion
 from gusto.time_discretisation import (SSPRK3, RungeKuttaFormulation,
                                        BackwardEuler)
-from gusto.timestepping import SemiImplicitQuasiNewton
+from gusto.timestepping import SemiImplicitQuasiNewton, TRBDF2QuasiNewton
 
 
 class ModelBase(object, metaclass=ABCMeta):
@@ -97,6 +98,222 @@ class ModelBase(object, metaclass=ABCMeta):
         self.stepper.run(t=t, tmax=tmax, pick_up=pick_up)
 
 
+class TRBDF2ModelBase(ModelBase):
+    """
+    Base for model classes using SIQN. Child classes should define the
+    standard transport and diffusion schemes and methods.
+    """
+
+    @abstractproperty
+    def diffusion_methods(self):
+        """
+        Needs to return a list of instances of the diffusion discretisation
+        class for each of the prognostic fields in the model equation set that
+        has diffusion options defined.
+        """
+        pass
+
+    @abstractproperty
+    def diffusion_schemes(self):
+        """
+        Need to return a list of instances of the timestepping scheme
+        class to be used for each of the prognostic fields in the model
+        equation set that has diffusion options defined.
+        """
+        pass
+
+    @abstractproperty
+    def transported_fields(self):
+        """
+        Needs to return a list of instances of the timestepping scheme class
+        to be used for each of the prognostic fields in the model equation set.
+        """
+        pass
+
+    @abstractproperty
+    def transport_methods(self):
+        """
+        Needs to return a list of instances of the transport method class
+        for each of the prognostic fields in the model equation set.
+        """
+        pass
+
+    @property
+    def tr_tau_values(self):
+        _tau = {}
+        for field_name in self.equation.field_names:
+            if field_name != "u":
+                _tau[field_name] = 2 * self.gamma
+        return _tau
+
+
+    @property
+    def bdf_tau_values(self):
+        _tau = {}
+        for field_name in self.equation.field_names:
+            if field_name != "u":
+                _tau[field_name] = 1.0
+        return _tau
+
+    def setup(self, output, **kwargs):
+        """
+        Args:
+            output (:class:`OutputParameters`): provides parameters
+                controlling output
+
+        Kwargs:
+            diagnostic_fields (list, optional): an iterable of `DiagnosticField`
+                objects. Defaults to None.
+            subcycling_options (:class:`SubcyclingOptions`, optional): an
+                object containing options for subcycling the time
+                discretisation. Defaults to None.
+
+            Remaining kwargs are passed straight through to the stepper - see
+            there for documentation.
+        """
+        self.gamma = 1 - sqrt(2) / 2
+        diagnostic_fields = kwargs.pop("diagnostic_fields", None)
+        io = IO(self.domain, output, diagnostic_fields=diagnostic_fields)
+
+        self.subcycling_options = kwargs.pop("subcycling_options", None)
+        self.limiters = kwargs.pop("limiters", {})
+
+        self.stepper = TRBDF2QuasiNewton(
+            self.equation, io, self.transported_fields,
+            spatial_methods=self.transport_methods+self.diffusion_methods,
+            diffusion_schemes=self.diffusion_schemes,
+            tau_values_tr=self.tr_tau_values,
+            tau_values_bdf=self.bdf_tau_values,
+            **kwargs
+        )
+
+class TRBDF2Model(TRBDF2ModelBase):
+    """
+    TRBDF2 model class encapsulating the best settings for next-to-lowest order 
+    methods.
+    """
+    def __init__(self, mesh, dt, parameters, equations, 
+                 family=None, **kwargs):
+         """
+        Args:
+            mesh (:class:`Mesh`): the model's mesh.
+            dt (float): the model timestep.
+            parameters (:class:`EquationParameters`): class storing the model
+                equation parameters.
+            equation (:class:`PrognosticEquationSet`): defines the model's
+                prognostic equation
+            family (str, optional): the finite element space family used for
+                the velocity field. This determines the other finite element
+                spaces used via the de Rham complex. If not provided, an
+                appropriate choice will be made based on the cell of the mesh
+                (or the base mesh in 3D). Defaults to None.
+
+        Kwargs:
+            Kwargs are passed straight through to the equation class; see
+            those classes for full documentation. Some common kwargs are
+            listed below.
+
+            u_transport_option (str, optional): specifies the transport term
+                used for the velocity equation. Supported options are:
+                'vector_invariant_form', 'vector_advection_form', and
+                'circulation_form'. Defaults to 'vector_invariant_form'.
+            diffusion_options (iterable, optional): iterable of
+                ``(field_name, diffusion_parameters)`` pairs where
+                diffusion_parameters is a :class:`DiffusionParameters`
+                object specifying the diffusion parameters to be applied
+                to the field field_name. Defaults to None.
+            no_normal_flow_bc_ids (list, optional): a list of IDs of domain
+                boundaries at which no normal flow will be enforced. Defaults to
+                None.
+            active_tracers (list, optional): a list of `ActiveTracer` objects
+                that encode the metadata for any active tracers to be included
+                in the equations. Defaults to None.
+        """
+         super().__init__(mesh , dt, parameters, equations, family=family, 
+                          element_order=1, 
+                          **kwargs)
+         
+
+    @property
+    def diffusion_methods(self):
+        """
+        Returns a list of instances of the InteriorPenaltyDiffusion
+        class for each of the prognostic fields in the model equation set that
+        has diffusion options defined.
+        """
+        _diffusion_methods = []
+        for field, params in self.diffusion_options:
+            _diffusion_methods.append(
+                InteriorPenaltyDiffusion(self.equation, field, params)
+            )
+        return _diffusion_methods
+    
+    @property
+    def diffusion_schemes(self):
+        """
+        Returns a list of instances of the Backward Euler timestepping scheme
+        class for each of the prognostic fields in the model equation set that
+        has diffusion options defined.
+        """
+        _diffusion_schemes = []
+        for field, _ in self.diffusion_options:
+            _diffusion_schemes.append(
+                BackwardEuler(self.domain, field)
+            )
+        return _diffusion_schemes
+    
+    
+    @property
+    def transported_fields(self):
+        """
+        Returns a list of instances of the SSPRK3 timestepping scheme class
+        for each of the prognostic fields in the model equation set.
+        """
+        _transported_fields = []
+        for field_name in self.equation.field_names:
+            if self.equation.space_names[field_name] == 'L2':
+                _transported_fields.append(
+                    SSPRK3(self.domain, field_name,
+                           subcycling_options=self.subcycling_options,
+                           rk_formulation=RungeKuttaFormulation.linear,
+                           limiter=self.limiters.get(field_name))
+                )
+            elif self.equation.space_names[field_name] == 'theta':
+                _transported_fields.append(
+                    SSPRK3(self.domain, field_name,
+                           subcycling_options=self.subcycling_options,
+                           options=EmbeddedDGOptions(),
+                           limiter=self.limiters.get(field_name))
+                )
+            else:
+                _transported_fields.append(
+                    SSPRK3(
+                        self.domain, field_name,
+                        subcycling_options=self.subcycling_options,
+                        limiter=self.limiters.get(field_name))
+                )
+        return _transported_fields
+    
+    
+    @property
+    def transport_methods(self):
+        """
+        Returns a list of instances of the DGUpwind transport method class
+        for each of the prognostic fields in the model equation set.
+        """
+        _transport_methods = []
+        for field_name in self.equation.field_names:
+            if self.equation.space_names[field_name] == 'L2':
+                _transport_methods.append(
+                    DGUpwind(self.equation, field_name,
+                             advective_then_flux=True)
+                )
+            else:
+                _transport_methods.append(
+                    DGUpwind(self.equation, field_name)
+                )
+        return _transport_methods
+        
 class SIQNModelBase(ModelBase):
     """
     Base for model classes using SIQN. Child classes should define the
@@ -180,7 +397,6 @@ class SIQNModelBase(ModelBase):
             tau_values=self.tau_values,
             **kwargs
         )
-
 
 class SIQNModel(SIQNModelBase):
     """
