5b51a0a9a35e9dc018838df0f330ea95bc9a6f1d
HannahDawes <114673612+HannahDawes@users.noreply.github.com>
2026-04-02 15:29:07 +0100

    complete cross-section finding script

--- jupiter_optimaltransport/jovian_vortices_updating_points.py
+++ jupiter_optimaltransport/jovian_vortices_updating_points.py
@@ -5,11 +5,13 @@ import itertools
 
 from gusto import *
 from firedrake import (PeriodicRectangleMesh, SpatialCoordinate, as_vector,
-                       interpolate, exp, sqrt, conditional)
+                       interpolate, exp, sqrt, conditional, 
+                       CheckpointFile)
 
-dt = 500.          # timestep (in seconds) [originally 250]
-tmax = 1730 * dt    # duration of the simulation (in seconds) [4320 * 500 = 25 days]
-# ^ 10 days (technically 10.01157 days, not exact)
+
+dt = 500.          # timestep (in seconds) 
+tmax = 25920 * dt    # duration of the simulation (in seconds) 
+# ^ 150 days
 
 # Set up the mesh and choose the refinement level
 Lx = 7e7
@@ -32,70 +34,13 @@ parameters = ShallowWaterParameters(mesh, H=H, g=g, R=R, Omega=Omega,
 
 domain = Domain(mesh, dt, "RTCF", 1)
 
-
-def smooth_f_profile(delta, rstar, Omega=parameters.Omega, R=R, Lx=Lx, nx=nx):
-    """
-    Returns the coefficients of a 5th order polynomial to smooth the transition from a polar gamma plane to the trap.
-
-    Args:
-      delta: half-width of smoothed region in number of cells
-      rstar: radius of edge of trap in x-y grid coordinates
-    """
-    
-    delta *= Lx/nx
-    Omega = Omega.dat.data[0]
-    r = sp.symbols('r')
-
-    fexpr = 2*Omega*(1-0.5*r**2/R**2)
-    left_val = fexpr.subs(r, rstar-delta)
-    right_val = 2*Omega
-    left_diff_val = sp.diff(fexpr, r).subs(r, rstar-delta)
-    left_diff2_val = sp.diff(fexpr, r, 2).subs(r, rstar-delta)
-
-    a = sp.symbols('a_0:6')
-    P = a[0]
-    for i in range(1, 6):
-        P += a[i]*r**i
-    eqns = [
-        P.subs(r, rstar-delta) - left_val,
-        P.subs(r, rstar+delta) - right_val,
-        sp.diff(P, r).subs(r, rstar-delta) - left_diff_val,
-        sp.diff(P, r).subs(r, rstar+delta),
-        sp.diff(P, r, 2).subs(r, rstar-delta) - left_diff2_val,
-        sp.diff(P, r, 2).subs(r, rstar+delta)
-    ]
-
-    sol = sp.solve(eqns, a)
-    coeffs = [sol[sp.Symbol(f'a_{i}')] for i in range(6)]
-    return coeffs
-
-
-r, theta = rtheta_from_xy(x, y)
-
-# define the expression for the gamma plane
-fexpr = 2*Omega*(1-0.5*r**2/R**2)
-# extract the coefficients of the smooth region. Here the trap radius is 3 cells from the edge of the domain, and the smoothing takes 2 cells either side of this trap radius
-delta = 2
-rstar = Lx/2 - 3*Lx/nx
-coeffs = smooth_f_profile(delta, rstar)
-# define a firedrake function using the polynomial coefficients
-fsmooth = float(coeffs[0]) + float(coeffs[1])*r + float(coeffs[2])*r**2 + float(coeffs[3])*r**3 + float(coeffs[4])*r**4 + float(coeffs[5])*r**5
-# use the gamma plane expression (fexpr), the smoothing (fsmooth) and the trap value (2*Omega) to build a full Coriolis expression
-ftrap1 = conditional(r < rstar-delta*Lx/nx, fexpr, fsmooth)
-ftrap = conditional(r < rstar+delta*Lx/nx, ftrap1, 2*Omega)
-
+# gusto shallow water equations setup
 eqns = ShallowWaterEquations(domain, parameters)
-
-# 15.72846 cores = use 16 cores
-#print(f'Estimated number of cores = {eqns.X.function_space().dim() / 50000} ')
-
 
 # field_creator object contains all the fields
 def update_points(fields):
 
     D = fields("D") 
-   # min_kernel = MinKernel()
-    #D_min = min_kernel.apply(D)  # value of D min 
 
     # index which has the min of the np array for D
     idx = np.argmin(D.dat.data_ro)  
@@ -104,8 +49,8 @@ def update_points(fields):
     W = VectorFunctionSpace(m, V.ufl_element())
     X = assemble(interpolate(m.coordinates, W))  # puts the mesh coords into the vector function space
 
+    # the minimum of the depth field is at index idx
     min_point = X.dat.data_ro[idx]
-  #  print(f"min_point = {min_point}")  #  [-57784, 18651534]
 
     # 1001 so there is a midpoint at index 500
     points_x = np.linspace(-1e7 + round(min_point[0]), 1e7 + round(min_point[0]), 1001)
@@ -113,25 +58,19 @@ def update_points(fields):
 
     points = np.array([p for p in itertools.product(points_x, points_y)])
     
-    return points #, min_point   - to track the vortex we could store the x_m,y_m values
+    return points  # returns the interval of coordinates to evaluate the field at
 
 
-dirname = "jv_up_D_10days"
-dumpfreq = 173  # should give 11 outputs
+dirname = "jv_up_pv_b1_150days" 
+dumpfreq = 8640  # to give 4 outputs
 pddumpfreq = dumpfreq 
 
+# output variable includes the function for finding new cross-section points
 output = OutputParameters(
     dirname=dirname, dumpfreq=dumpfreq, pddumpfreq=dumpfreq,
     dump_vtus=True, dump_nc=True,
-    #point_data=[('PotentialVorticity', points)]  # field name and list of points at which to ouput this field
-    point_data=[('D', update_points)]  # in the future want to include function that finds the new points too
+    point_data=[('PotentialVorticity', update_points)]  
 )
-
-
-
-# old output variable
-#output = OutputParameters(dirname="jovian_vortices111125", dumpfreq=96)  # dumpfreq is how many iterations it does before it saves a state 
-
 
 diagnostic_fields = [SteadyStateError('u'), SteadyStateError('D'),
                      RelativeVorticity(method = 'project'), PotentialVorticity(method = 'project'),
@@ -162,15 +101,11 @@ Ro = 0.23             # Rossby Number
 f0 = 2 * Omega        # Planetary vorticity
 rm = 1e6              # Radius of vortex (m)
 vm = Ro * f0 * rm     # Calculate speed with Ro
-#g = parameters.g.dat.data[0]
 phi0 = g*H
 Bu = phi0 / (f0 * rm)**2  # burger no. 
 
 south_lat = np.deg2rad(south_lat_deg)
 south_lon = np.deg2rad(south_lon_deg)
-
-#print(f"south_lat = {south_lat}, south_lon = {south_lon}")
-
 
 def initialise_D(X, idx):
     # computes the initial depth perturbation corresponding to vortex
@@ -222,6 +157,7 @@ Dfinal += H
 u_veloc = 0.*x
 v_veloc = 0.*y
 
+# create the initial velocity field of the vortex:
 for i in range(len(south_lat)):
     r_c, theta_c = rtheta_from_lonlat(south_lon[i], south_lat[i], R)
     x_c, y_c = xy_from_rtheta(r_c, theta_c)
@@ -245,5 +181,3 @@ stepper.set_reference_profiles([('D', Dbar)])
 
 # Run the timestepper and generate the output.
 stepper.run(t=0, tmax=tmax)
-
-
