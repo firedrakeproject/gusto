bbc2246f6f1789c0f8165c4d508d0feeb4477fb8
Thomas Bendall <14180399+tommbendall@users.noreply.github.com>
2026-07-07 11:36:52 +0100

    fix to IMEX first guess

--- gusto/time_discretisation/imex_runge_kutta.py
+++ gusto/time_discretisation/imex_runge_kutta.py
@@ -266,7 +266,9 @@ class IMEXRungeKutta(TimeDiscretisation):
 
             self.solver = solver_list[stage-self.solver_start_stage]
             # Set initial solver guess
-            self.x_out.assign(self.xs[stage-1])
+            if stage != self.solver_start_stage:
+                self.x_out.assign(self.xs[stage-1])
+
             # Evaluate source terms
             for evaluate in self.evaluate_source:
                 evaluate(self.xs[stage-1], self.dt, x_out=self.source[stage-1])
