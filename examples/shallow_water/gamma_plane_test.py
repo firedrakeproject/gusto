977059387cc5a10b3ed51e11730f38521a664afb
12shughes <sh1293@exeter.ac.uk>
2026-07-27 11:42:18 +0100

    tidy script

--- examples/shallow_water/gamma_plane_test.py
+++ examples/shallow_water/gamma_plane_test.py
@@ -6,60 +6,33 @@ from firedrake import (
     SpatialCoordinate, PeriodicRectangleMesh, conditional, FunctionSpace,
     Function
 )
-import scipy
-import numpy as np
-import time
-import os
-import shutil
 import sympy as sp
 
-def smooth_f_profile(degree, delta, style, rstar, Omega, R, Lx, nx):
+def smooth_f_profile(delta, rstar, Omega, R, Lx, nx):
     delta *= Lx/nx
     r = sp.symbols('r')
-    if style == 'polar':
-        fexpr = 2*Omega*(1-0.5*r**2/R**2)
-        left_val = fexpr.subs(r, rstar-delta)
-        right_val = 2*Omega
-        left_diff_val = sp.diff(fexpr, r).subs(r, rstar-delta)
-        left_diff2_val = sp.diff(fexpr, r, 2).subs(r, rstar-delta)
-    elif style == 'flat':
-        left_val = 2*Omega*(1-0.5*(rstar-delta)**2/R**2)
-        right_val = 2*Omega
-        left_diff_val = 0
-        left_diff2_val = 0
+    fexpr = 2*Omega*(1-0.5*r**2/R**2)
+    left_val = fexpr.subs(r, rstar-delta)
+    right_val = 2*Omega
+    left_diff_val = sp.diff(fexpr, r).subs(r, rstar-delta)
+    left_diff2_val = sp.diff(fexpr, r, 2).subs(r, rstar-delta)
 
-    a = sp.symbols(f'a_0:{degree+1}')
+    a = sp.symbols(f'a_0:{6}')
     P = a[0]
-    for i in range(1, degree+1):
+    for i in range(1, 6):
         P += a[i]*r**i
 
-    if degree == 3:
-        eqns = [
-            P.subs(r, rstar-delta) - left_val,
-            P.subs(r, rstar+delta) - right_val,
-            sp.diff(P, r).subs(r, rstar-delta) - left_diff_val,
-            sp.diff(P, r).subs(r, rstar+delta)
-        ]
-    elif degree == 5:
-        eqns = [
-            P.subs(r, rstar-delta) - left_val,
-            P.subs(r, rstar+delta) - right_val,
-            sp.diff(P, r).subs(r, rstar-delta) - left_diff_val,
-            sp.diff(P, r).subs(r, rstar+delta),
-            sp.diff(P, r, 2).subs(r, rstar-delta) - left_diff2_val,
-            sp.diff(P, r, 2).subs(r, rstar+delta)
-        ]
-    else:
-        print('do not have BCs for this degree')
+    eqns = [
+        P.subs(r, rstar-delta) - left_val,
+        P.subs(r, rstar+delta) - right_val,
+        sp.diff(P, r).subs(r, rstar-delta) - left_diff_val,
+        sp.diff(P, r).subs(r, rstar+delta),
+        sp.diff(P, r, 2).subs(r, rstar-delta) - left_diff2_val,
+        sp.diff(P, r, 2).subs(r, rstar+delta)
+    ]
 
     sol = sp.solve(eqns, a)
-    coeffs = [sol[sp.Symbol(f'a_{i}')] for i in range(degree+1)]
-    # P_smooth = P.subs(sol)
-    # f_smooth = sp.Piecewise(
-    #     (fexpr, r<rstar-delta),
-    #     (P_smooth, (rstar-delta<=r) & (r<=rstar+delta)),
-    #     (right_val, rstar+delta<r)
-    # )
+    coeffs = [sol[sp.Symbol(f'a_{i}')] for i in range(6)]
     return coeffs
 
 nx = 256
@@ -91,22 +64,17 @@ parameters = ShallowWaterParameters(mesh, H=H, Omega=Omega, R=R,
 
 domain = Domain(mesh, dt, "RTCF", 1)
 
-
 x, y = SpatialCoordinate(mesh)
 r, _ = rtheta_from_xy(x, y, Lx/2, Ly/2)
 Rsq = parameters.R**2
 
 Omega_num = Omega
 Omega = parameters.Omega
-
-# expression for gamma plane with no trap
 fexpr = 2*Omega*(1-0.5*r*2/R**2)
 
-# calculate polymonial that smooths trap edge
-coeffs = smooth_f_profile(degree=smooth_degree, delta=smooth_delta, style='polar', rstar=rstar, Omega=Omega_num, R=R, Lx=Lx, nx=nx)
+coeffs = smooth_f_profile(delta=smooth_delta, rstar=rstar, Omega=Omega_num, R=R, Lx=Lx, nx=nx)
 fsmooth = float(coeffs[0]) + float(coeffs[1])*r + float(coeffs[2])*r**2 + float(coeffs[3])*r**3
-if smooth_degree == 5:
-    fsmooth += float(coeffs[4])*r**4 + float(coeffs[5])*r**5
+fsmooth += float(coeffs[4])*r**4 + float(coeffs[5])*r**5
 
 Vcg = FunctionSpace(domain.mesh, "DG", 1)
 
@@ -118,8 +86,6 @@ if trap=='no_trap':
     eqns = ShallowWaterEquations(domain, parameters)
     gusto_field = eqns.prescribed_fields('coriolis')
     pv_gusto = Function(Vcg).interpolate(gusto_field)
-
-    breakpoint()
 
 elif trap=='step':
     ftrap_step = conditional(r<rstar, fexpr, 2*Omega)
